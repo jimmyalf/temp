b5ce6245a7a7feae9b9f6658513e8d863a7a17a2
Carl Berg <carl.berg@spinit.se>
2009-07-02 12:57:47 +0000

    Added IShopEquipmentRow interface

---
+++ Synologen Business/Interfaces/IShopEquipmentRow.cs
@@ -0,0 +1,7 @@
+namespace Spinit.Wpc.Synologen.Business.Interfaces {
+	public interface IShopEquipmentRow {
+		int Id { get; set; }
+		string Name { get; set; }
+		string Description { get; set; } 
+	}
+}

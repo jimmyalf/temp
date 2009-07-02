6619b5b0da3d1f86ea526ceb0e3363fb42b12aa4
Carl Berg <carl.berg@spinit.se>
2009-07-02 12:59:20 +0000

    Lets ShopEquipmentRow implement IShopEquipmentRow interface.

--- Synologen Data/Types/ShopEquipmentRow.cs
+++ Synologen Data/Types/ShopEquipmentRow.cs
@@ -1,47 +1,8 @@
-// ==========================================================================
-//
-//  PROGRAM
-//	
-//
-//  FILENAME
-//	$Workfile: ShopEquipmentRow.cs $
-//
-//  ARCHIVE
-//  $Archive: /Develop/WPC/Version 4.1/CustomerSpecific/Synologen/Synologen Data/Types/ShopEquipmentRow.cs $
-//
-//  VERSION
-//	$Revision: 1 $
-//
-//  DATES
-//	Last check in: $Date: 09-01-07 17:35 $
-//	Last modified: $Modtime: 09-01-07 10:43 $
-//
-//  AUTHOR(S)
-//	$Author: Cber $
-// 	
-//
-//  COPYRIGHT
-// 	Copyright (c) 2009 Spinit AB --- ALL RIGHTS RESERVED
-// 	Spinit AB, Datavägen 2, 436 32 Askim, SWEDEN
-//
-// ==========================================================================
-// 
-//  DESCRIPTION
-//  
-//
-// ==========================================================================
-//
-//	History
-//
-//	$Log: /Develop/WPC/Version 4.1/CustomerSpecific/Synologen/Synologen Data/Types/ShopEquipmentRow.cs $
-//
-//1     09-01-07 17:35 Cber
-// 
-// ==========================================================================
+using Spinit.Wpc.Synologen.Business.Interfaces;
 namespace Spinit.Wpc.Synologen.Data.Types {
-	public class ShopEquipmentRow {
-		public int Id;
-		public string Name;
-		public string Description;
+	public class ShopEquipmentRow : IShopEquipmentRow {
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
 	}
 }

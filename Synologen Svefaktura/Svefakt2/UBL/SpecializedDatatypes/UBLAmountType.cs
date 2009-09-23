80aab436259b9c23ff2b2bd8a66aebdbe588468d
Carl Berg <carl.berg@spinit.se>
2009-09-23 16:00:06 +0000

    Namespace updated

--- Synologen Svefaktura/Svefakt2/UBL/SpecializedDatatypes/UBLAmountType.cs
+++ Synologen Svefaktura/Svefakt2/UBL/SpecializedDatatypes/UBLAmountType.cs
@@ -1,19 +1,19 @@
-using Spinit.Wpc.Synologen.Svefaktura.Svefakt2.UBL.UnspecializedDatatypes;
+using Spinit.Wpc.Synologen.Svefaktura.Svefakt2.UBL.CommonBasicComponents;
 
-namespace Spinit.Wpc.Synologen.Svefaktura.Svefakt2 {
+namespace Spinit.Wpc.Synologen.Svefaktura.Svefakt2.UBL.SpecializedDatatypes {
 	/// <remarks/>
-	[System.Xml.Serialization.XmlIncludeAttribute(typeof(TotalAmountType))]
-	[System.Xml.Serialization.XmlIncludeAttribute(typeof(TaxTotalAmountType))]
-	[System.Xml.Serialization.XmlIncludeAttribute(typeof(TaxAmountType))]
-	[System.Xml.Serialization.XmlIncludeAttribute(typeof(PriceAmountType))]
-	[System.Xml.Serialization.XmlIncludeAttribute(typeof(ExtensionTotalAmountType))]
-	[System.Xml.Serialization.XmlIncludeAttribute(typeof(ExtensionAmountType))]
-	[System.Xml.Serialization.XmlIncludeAttribute(typeof(AmountType2))]
-	[System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.42")]
-	[System.SerializableAttribute()]
-	[System.Diagnostics.DebuggerStepThroughAttribute()]
-	[System.ComponentModel.DesignerCategoryAttribute("code")]
-	[System.Xml.Serialization.XmlTypeAttribute(Namespace="urn:oasis:names:tc:ubl:SpecializedDatatypes:1:0")]
-	public partial class UBLAmountType : AmountType {
+	[System.Xml.Serialization.XmlInclude(typeof(TotalAmountType))]
+	[System.Xml.Serialization.XmlInclude(typeof(TaxTotalAmountType))]
+	[System.Xml.Serialization.XmlInclude(typeof(TaxAmountType))]
+	[System.Xml.Serialization.XmlInclude(typeof(PriceAmountType))]
+	[System.Xml.Serialization.XmlInclude(typeof(ExtensionTotalAmountType))]
+	[System.Xml.Serialization.XmlInclude(typeof(ExtensionAmountType))]
+	[System.Xml.Serialization.XmlInclude(typeof(AmountType))]
+	[System.CodeDom.Compiler.GeneratedCode("xsd", "2.0.50727.42")]
+	[System.Serializable()]
+	[System.Diagnostics.DebuggerStepThrough()]
+	[System.ComponentModel.DesignerCategory("code")]
+	[System.Xml.Serialization.XmlType(Namespace="urn:oasis:names:tc:ubl:SpecializedDatatypes:1:0")]
+	public partial class UBLAmountType : UnspecializedDatatypes.AmountType {
 	}
 }

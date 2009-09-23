bc9e2da161a6db0019974ae2620235ad6ad32198
Carl Berg <carl.berg@spinit.se>
2009-09-23 15:55:29 +0000

    Updated namespace

--- Synologen Svefaktura/Svefakt2/UBL/CommonBasicComponents/BackorderQuantityType.cs
+++ Synologen Svefaktura/Svefakt2/UBL/CommonBasicComponents/BackorderQuantityType.cs
@@ -1,13 +1,13 @@
 using Spinit.Wpc.Synologen.Svefaktura.Svefakt2.UBL.UnspecializedDatatypes;
 
-namespace Spinit.Wpc.Synologen.Svefaktura.Svefakt2 {
+namespace Spinit.Wpc.Synologen.Svefaktura.Svefakt2.UBL.CommonBasicComponents {
 	/// <remarks/>
-	[System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.42")]
-	[System.SerializableAttribute()]
-	[System.Diagnostics.DebuggerStepThroughAttribute()]
-	[System.ComponentModel.DesignerCategoryAttribute("code")]
-	[System.Xml.Serialization.XmlTypeAttribute(Namespace="urn:oasis:names:tc:ubl:CommonBasicComponents:1:0")]
-	[System.Xml.Serialization.XmlRootAttribute("MaximumBackorderQuantity", Namespace="urn:oasis:names:tc:ubl:CommonBasicComponents:1:0", IsNullable=false)]
+	[System.CodeDom.Compiler.GeneratedCode("xsd", "2.0.50727.42")]
+	[System.Serializable()]
+	[System.Diagnostics.DebuggerStepThrough()]
+	[System.ComponentModel.DesignerCategory("code")]
+	[System.Xml.Serialization.XmlType(Namespace="urn:oasis:names:tc:ubl:CommonBasicComponents:1:0")]
+	[System.Xml.Serialization.XmlRoot("MaximumBackorderQuantity", Namespace="urn:oasis:names:tc:ubl:CommonBasicComponents:1:0", IsNullable=false)]
 	public partial class BackorderQuantityType : QuantityType {
 	}
 }

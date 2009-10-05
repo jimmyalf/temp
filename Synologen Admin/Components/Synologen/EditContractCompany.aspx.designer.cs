1e4f289b0dfe44438285c65215d46e88da1355c0
Carl Berg <carl.berg@spinit.se>
2009-10-05 15:17:50 +0000

    Added Validation Rule selection -functionality to EditCompany.

--- Synologen Admin/Components/Synologen/EditContractCompany.aspx.designer.cs
+++ Synologen Admin/Components/Synologen/EditContractCompany.aspx.designer.cs
@@ -158,6 +158,15 @@ namespace Spinit.Wpc.Synologen.Presentation.Components.Synologen {
         protected global::System.Web.UI.WebControls.CheckBox chkActive;
         
         /// <summary>
+        /// chkValidationRules control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBoxList chkValidationRules;
+        
+        /// <summary>
         /// txtOrganizationNumber control.
         /// </summary>
         /// <remarks>

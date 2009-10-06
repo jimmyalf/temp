f196a075117359daecd045527bcd467f2c480233
Carl Berg <carl.berg@spinit.se>
2009-10-06 12:00:29 +0000

    Updated EditOrder and CreateOrder to use a common base class with shared methods. Also updated EditOrder to use custom validation.

---
+++ Synologen Site/Code/SynologenSalesUserControl.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Spinit.Wpc.Synologen.Data.Types;
+
+namespace Spinit.Wpc.Synologen.Presentation.Site.Code {
+	public class SynologenSalesUserControl : SynologenUserControl {
+		private int _maxNumberOfItems = 15;
+		protected List<CompanyValidationRule> contractValidationRules = new List<CompanyValidationRule>();
+
+		protected void PopulateValidationRules(int selectedCompanyId, ControlCollection controls) {
+			contractValidationRules = new List<CompanyValidationRule>(Provider.GetCompanyRow(selectedCompanyId).CompanyValidationRules);
+			SetRequiredAsteriskInLabels(controls);
+		}
+
+		protected void SetRequiredAsteriskInLabels(ControlCollection controls) {
+			foreach (Control control in controls){
+				if(control.GetType() != typeof (Literal)){
+					if (control.HasControls()){
+						SetRequiredAsteriskInLabels(control.Controls);
+					}
+					else{ continue;}
+				}
+				if (control.GetType() != typeof(Literal)) continue;
+				var literalControl = (Literal) control;
+				if (!literalControl.ID.ToLower().Contains("required")) continue;
+				literalControl.DataBind();
+			}
+		}
+
+		protected void PerformCustomValidation(object source, ServerValidateEventArgs args) {
+			var validationControl = (CustomValidator) source;
+			var controlToValidateID = validationControl.ControlToValidate;
+			string errorMessage;
+			args.IsValid = CustomOrderValidation.IsValid(controlToValidateID, args.Value, contractValidationRules, out errorMessage);
+			validationControl.ErrorMessage = errorMessage;
+		}
+
+		protected void SessionValidation(object source, ServerValidateEventArgs args) {
+		    args.IsValid = (MemberId > 0 && MemberShopId > 0);
+		}
+
+		protected string GetControlIsRequiredCharacter(string controlToValidate) {
+			return contractValidationRules.Exists(x => x.ControlToValidate.Equals(controlToValidate) && CustomOrderValidation.IsValidationRuleRequired(x))  ? "*" : String.Empty;
+		}
+
+		protected static int GetNewTemporaryIdForCart(List<OrderItemRow> cart) {
+			if (cart == null || cart.Count == 0) return 1;
+			var tempId = 0;
+			foreach (var itemRow in cart){
+				if (itemRow.TemporaryId > tempId) tempId = itemRow.TemporaryId;
+			}
+			return tempId+1;
+		}
+
+		protected static float GetTotalCartPrice(IEnumerable<OrderItemRow> cart) {
+			float returnValue = 0;
+			foreach(var order in cart) {
+				returnValue += order.DisplayTotalPrice;
+			}
+			return returnValue;
+		}
+		
+		protected List<int> GetNumberOfItemsItemList() {
+			var returnList = new List<int>();
+			for (var i = 1; i <= MaxNumberOfItems; i++) {
+				returnList.Add(i);
+			}
+			return returnList;
+		}
+
+		public int MaxNumberOfItems {
+			get { return _maxNumberOfItems; }
+			set { _maxNumberOfItems = value; }
+		}
+	}
+}
--- Synologen Site/Wpc/Synologen/EditOrder.ascx.cs
+++ Synologen Site/Wpc/Synologen/EditOrder.ascx.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Web.UI.WebControls;
-using Spinit.Wpc.Member.Data;
-using Spinit.Wpc.Synologen.Business;
 using Spinit.Wpc.Synologen.Business.Enumeration;
 using Spinit.Wpc.Synologen.Data.Types;
 using Spinit.Wpc.Synologen.Presentation.Site.Code;
@@ -10,9 +8,7 @@ using Spinit.Wpc.Utility.Business;
 using Globals=Spinit.Wpc.Synologen.Business.Globals;
 
 namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
-	public partial class EditOrder : SynologenUserControl {
-		private int _maxNumberOfItems = 15;
-		private static int RequiredRSTLength = 5;
+	public partial class EditOrder : SynologenSalesUserControl {
 		private int _orderId;
 		private OrderRow _order;
 
@@ -37,6 +33,8 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 			SynologenSessionContext.OrderItemsMarkedForDeletion = new List<int>();
 			SynologenSessionContext.EditOrderItemsInCart = new List<OrderItemRow>();
 		}
+
+		#region Population Mehtods
 		private void PopulateContracts() {
 			if(MemberShopId==0) {return;}
 
@@ -45,21 +43,15 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 			drpContracts.Items.Insert(0,new ListItem("-- Välj avtal --","0"));
 		}
 		private void PopulateCompanies() {
-			int contractId = Int32.Parse(drpContracts.SelectedValue);
+			var contractId = Int32.Parse(drpContracts.SelectedValue);
 			drpCompany.DataSource = Provider.GetCompanies(0, contractId, null, ActiveFilter.Active);
 			drpCompany.DataBind();
 			drpCompany.Items.Insert(0, new ListItem("-- Välj företag --", "0"));
 			drpCompany.Enabled = true;
 		}
-		//private void PopulateRSTs() {
-		//    int companyId = Int32.Parse(drpCompany.SelectedValue);
-		//    drpRST.DataSource = Provider.GetCompanyRSTs(0, companyId, null);
-		//    drpRST.DataBind();
-		//    drpRST.Items.Insert(0, new ListItem("-- Välj Kostnadsställe --", "0"));
-		//    drpRST.Enabled = true;
-		//}
+
 		private void PopulateArticles() {
-			int contractId = Int32.Parse(drpContracts.SelectedValue);
+			var contractId = Int32.Parse(drpContracts.SelectedValue);
 			drpArticle.DataSource = Provider.GetContractArticleConnections(0, contractId, "tblSynologenContractArticleConnection.cId");
 			drpArticle.DataBind();
 			drpArticle.Items.Insert(0, new ListItem("-- Välj Artikel --", "0"));
@@ -75,12 +67,12 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 			SynologenSessionContext.EditOrderItemsInCart = Provider.GetOrderItemsList(_orderId, 0, null);
 			gvOrderItemsCart.DataSource = SynologenSessionContext.EditOrderItemsInCart;
 			gvOrderItemsCart.DataBind();
-			ltTotalPrice.Text = GetTotalCartPrice().ToString();
+			ltTotalPrice.Text = GetTotalCartPrice(SynologenSessionContext.EditOrderItemsInCart).ToString();
 		}
 		private void PopulateShoppingCart() {
 			gvOrderItemsCart.DataSource = SynologenSessionContext.EditOrderItemsInCart;
 			gvOrderItemsCart.DataBind();
-			ltTotalPrice.Text = GetTotalCartPrice().ToString();
+			ltTotalPrice.Text = GetTotalCartPrice(SynologenSessionContext.EditOrderItemsInCart).ToString();
 		}
 		private void PopulateOrder() {
 			txtCompanyUnit.Text = _order.CompanyUnit;
@@ -89,15 +81,11 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 			txtPersonalIDNumber.Text = _order.PersonalIdNumber;
 			txtEmail.Text = _order.Email;
 			txtPhone.Text = _order.Phone;
-			CompanyRow company = Provider.GetCompanyRow(_order.CompanyId);
+			var company = Provider.GetCompanyRow(_order.CompanyId);
 			TrySetContract(company.ContractId);
 			PopulateCompanies();
 			PopulateArticles();
-			//drpCompany.SelectedValue = _order.CompanyId.ToString();
 			TrySetCompany(_order.CompanyId);
-			//PopulateRSTs();
-			//drpRST.SelectedValue = _order.RSTId.ToString();
-			//TrySetRST(_order.RSTId);
 			txtRST.Text = _order.RstText;
 			ltCreatedDate.Text = _order.CreatedDate.ToShortDateString();
 			if (_order.UpdateDate != DateTime.MinValue) {
@@ -105,12 +93,11 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 			}
 			ltOrderNumber.Text = _orderId.ToString();
 			ltMarkedAsPayed.Text = _order.MarkedAsPayedByShop ? "Ja" : "Nej";
-			//MemberRow salesPerson = Provider.GetMember(_order.SalesPersonMemberId, LocationId, LanguageId);
-			MemberRow salesPerson = Provider.GetSynologenMember(_order.SalesPersonMemberId, LocationId, LanguageId);
+			var salesPerson = Provider.GetSynologenMember(_order.SalesPersonMemberId, LocationId, LanguageId);
 			ltSalesPersonName.Text = salesPerson.ContactFirst + " " + salesPerson.ContactLast;
 			CheckEnableSaveAndAbortOrder();
-			//CheckEnableMarkAsPayed();
 		}
+		#endregion
 
 		#region Events
 		protected void drpContracts_SelectedIndexChanged(object sender, EventArgs e) {
@@ -121,28 +108,20 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 			}
 			else {
 				drpCompany.Enabled = false;
-				//drpRST.Enabled = false;
 				drpArticle.Enabled = false;
 				drpNumberOfItems.Enabled = false;
 			}
 		}
 
 		protected void drpCompany_SelectedIndexChanged(object sender, EventArgs e) {
-			if(drpCompany.SelectedValue!="0"){
-				//drpRST.Enabled = true;
-				//PopulateRSTs();
-			}
-			else {
-				//drpRST.Enabled = false;
-			}
+			if (drpCompany.SelectedValue == "0") return;
+			PopulateValidationRules(Convert.ToInt32(drpCompany.SelectedValue), Controls);
 		}
 
-		protected void drpRST_SelectedIndexChanged(object sender, EventArgs e) {}
-
 		protected void gvOrderItemsCart_Deleting(object sender, GridViewDeleteEventArgs e) {
-			int index = e.RowIndex;
-			int temporaryId = (int) gvOrderItemsCart.DataKeys[index].Values["TemporaryId"];
-			int orderItemId = (int)gvOrderItemsCart.DataKeys[index].Values["Id"];
+			var index = e.RowIndex;
+			var temporaryId = (int) gvOrderItemsCart.DataKeys[index].Values["TemporaryId"];
+			var orderItemId = (int)gvOrderItemsCart.DataKeys[index].Values["Id"];
 			if (temporaryId>0){
 				RemoveTemporaryOrderItemFromCart(temporaryId);
 			}
@@ -153,24 +132,15 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 			PopulateShoppingCart();
 		}
 
-		protected void PersonalIDNumberValidation(object source, ServerValidateEventArgs args) {
-			args.IsValid = Business.Utility.General.ValidatePersonalIDNumber(args.Value);
-		}
-
-		protected void SessionValidation(object source, ServerValidateEventArgs args) {
-			args.IsValid = (MemberId > 0 && MemberShopId > 0);
-		}
-
 		protected void OrderItemsValidation(object source, ServerValidateEventArgs args) {
 			args.IsValid = (SynologenSessionContext.EditOrderItemsInCart.Count > 0);
 		}
 
 		protected void btnAdd_Click(object sender, EventArgs e) {
 			if (!reqNumberOfItems.IsValid || !reqArticle.IsValid || !reqArticle2.IsValid) return;
-			OrderItemRow item = new OrderItemRow();
-			int connectionId = Int32.Parse(drpArticle.SelectedValue);
-			//item.ContractId = Int32.Parse(drpContracts.SelectedValue);
-			ContractArticleRow contractArticle = Provider.GetContractCustomerArticleRow(connectionId);
+			var item = new OrderItemRow();
+			var connectionId = Int32.Parse(drpArticle.SelectedValue);
+			var contractArticle = Provider.GetContractCustomerArticleRow(connectionId);
 			item.ArticleDisplayName = contractArticle.ArticleName;
 			item.ArticleDisplayNumber = contractArticle.ArticleNumber;
 			item.ArticleId = contractArticle.ArticleId;
@@ -184,6 +154,10 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 		}
 
 		protected void btnSave_Click(object sender, EventArgs e) {
+			if(drpCompany.SelectedValue != null && drpCompany.SelectedValue != "0"){
+				PopulateValidationRules(Convert.ToInt32(drpCompany.SelectedValue), Controls);
+				Page.Validate(btnSave.ValidationGroup);
+			}
 			if (!Page.IsValid) return;
 			SaveOrder();
 			SaveOrderItems(_order.Id);
@@ -201,12 +175,8 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 			Response.Redirect(SynologenSessionContext.SalesListPage);
 		}
 
-		//protected void btnBack_Click(object sender, EventArgs e) {
-		//    Response.Redirect(SynologenSessionContext.SalesListPage);
-		//}
-
 		protected void btnAbort_Click(object sender, EventArgs e) {
-			int newStatusId = Globals.AbortStatusId;
+			var newStatusId = Globals.AbortStatusId;
 			Provider.ChangeOrderStatus(_orderId, newStatusId);
 			//TODO: Replace string below with resource
 			Provider.AddOrderHistory(_orderId, "Ordern avbröts av " + CurrentUser);
@@ -214,7 +184,7 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 		}
 
 		protected void btnHalt_Click(object sender, EventArgs e) {
-			int newStatusId = Globals.HaltedStatusId;
+			var newStatusId = Globals.HaltedStatusId;
 			Provider.ChangeOrderStatus(_orderId, newStatusId);
 			//TODO: Replace string below with resource
 			Provider.AddOrderHistory(_orderId, "Ordern sattes som vilande av " + CurrentUser);
@@ -222,7 +192,7 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 		}
 
 		protected void btnAbortHalt_Click(object sender, EventArgs e) {
-			int newStatusId = Globals.DefaultNewOrderStatus;
+			var newStatusId = Globals.DefaultNewOrderStatus;
 			if (_order.InvoiceNumber > 0) newStatusId = Globals.DefaultOrderStatusAfterSPCSInvoice;
 			Provider.ChangeOrderStatus(_orderId, newStatusId);
 			//TODO: Replace string below with resource
@@ -234,8 +204,8 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 		#region Helper Methods
 
 		private void CheckUserPermission() {
-			bool statusOK = Globals.EditableOrderStatusList.Contains(_order.StatusId);
-			bool userPermissionOK = MemberShopId == _order.SalesPersonShopId;
+			var statusOK = Globals.EditableOrderStatusList.Contains(_order.StatusId);
+			var userPermissionOK = MemberShopId == _order.SalesPersonShopId;
 			if (statusOK && userPermissionOK) return;
 			plEditOrder.Visible = false;
 			plNoAccessMessage.Visible = true;
@@ -246,12 +216,10 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 			_order.CompanyUnit = txtCompanyUnit.Text;
 			_order.CustomerFirstName = txtCustomerFirstName.Text;
 			_order.CustomerLastName = txtCustomerLastName.Text;
-			//_order.StatusId = Globals.DefaultNewOrderStatus; don't change status when editing
 			_order.Phone = txtPhone.Text;
 			_order.Email = txtEmail.Text;
 			_order.SalesPersonMemberId = MemberId;
 			_order.SalesPersonShopId = MemberShopId;
-			//_order.RSTId = Int32.Parse(drpRST.SelectedValue);
 			_order.RstText = txtRST.Text;
 			_order.CompanyId = Int32.Parse(drpCompany.SelectedValue);
 			Provider.AddUpdateDeleteOrder(Enumerations.Action.Update, ref _order);
@@ -259,32 +227,25 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 		}
 
 		private void SaveOrderItems(int id) {
-			foreach (OrderItemRow item in SynologenSessionContext.EditOrderItemsInCart) {
+			foreach (var item in SynologenSessionContext.EditOrderItemsInCart) {
 				if (!item.IsTemporary) continue;
-				Enumerations.Action action = Enumerations.Action.Create;
-				OrderItemRow tempOrder = item;
+				const Enumerations.Action action = Enumerations.Action.Create;
+				var tempOrder = item;
 				tempOrder.OrderId = id;
 				Provider.AddUpdateDeleteOrderItem(action, ref tempOrder);
 			}
 		}
 
 		private void DeleteOrderItemsMarkedForDeletion() {
-			foreach (int orderItemId in SynologenSessionContext.OrderItemsMarkedForDeletion) {
+			foreach (var orderItemId in SynologenSessionContext.OrderItemsMarkedForDeletion) {
 				const Enumerations.Action action = Enumerations.Action.Delete;
-				OrderItemRow item = new OrderItemRow();
-				item.Id = orderItemId;
+				var item = new OrderItemRow {Id = orderItemId};
 				Provider.AddUpdateDeleteOrderItem(action, ref item);
 			}
 			SynologenSessionContext.OrderItemsMarkedForDeletion = new List<int>();
 		}
 
-		private List<int> GetNumberOfItemsItemList() {
-			List<int> returnList = new List<int>();
-			for (int i = 1; i <= _maxNumberOfItems; i++) {
-				returnList.Add(i);
-			}
-			return returnList;
-		}
+
 
 		private void ClearItemInputControls() {
 			txtNotes.Text = "";
@@ -293,69 +254,39 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 		}
 
 		private static void AddOrderItemToCart(OrderItemRow item) {
-			item.TemporaryId = GetNewTemporaryIdForCart();
-
-			List<OrderItemRow> cart = SynologenSessionContext.EditOrderItemsInCart;
+			var cart = SynologenSessionContext.EditOrderItemsInCart;
+			item.TemporaryId = GetNewTemporaryIdForCart(cart);
 			cart.Add(item);
 			SynologenSessionContext.EditOrderItemsInCart = cart;
 		}
 
 		private static void RemoveTemporaryOrderItemFromCart(int itemTemporaryId) {
-			List<OrderItemRow> cart = SynologenSessionContext.EditOrderItemsInCart;
-			cart.RemoveAll(delegate(OrderItemRow x) { return x.TemporaryId == itemTemporaryId; });
+			var cart = SynologenSessionContext.EditOrderItemsInCart;
+			cart.RemoveAll(x => x.TemporaryId == itemTemporaryId);
 			SynologenSessionContext.EditOrderItemsInCart = cart;
 		}
 
 		private static void RemoveExistingOrderItemFromCart(int orderItemId) {
-			List<OrderItemRow> cart = SynologenSessionContext.EditOrderItemsInCart;
-			cart.RemoveAll(delegate(OrderItemRow x) { return x.Id == orderItemId; });
+			var cart = SynologenSessionContext.EditOrderItemsInCart;
+			cart.RemoveAll(x => x.Id == orderItemId);
 			SynologenSessionContext.EditOrderItemsInCart = cart;
 			//Add item to a list of already existing orderItems to be deleted at save
-			List<int> deleteOrderItems = SynologenSessionContext.OrderItemsMarkedForDeletion;
+			var deleteOrderItems = SynologenSessionContext.OrderItemsMarkedForDeletion;
 			deleteOrderItems.Add(orderItemId);
 			SynologenSessionContext.OrderItemsMarkedForDeletion = deleteOrderItems;
 		}
 
-		private static int GetNewTemporaryIdForCart() {
-			List<OrderItemRow> cart = SynologenSessionContext.EditOrderItemsInCart;
-			int tempId = 1;
-			if (cart == null || cart.Count == 0) return tempId;
-			while(cart.Exists(delegate(OrderItemRow x) {return x.TemporaryId == tempId;})) {
-				tempId++;
-				if (tempId > 50) throw new  OverflowException("Shoppingcart contains too many items.");
-			}
-			return tempId;
-				
-		}
-
-		private static float GetTotalCartPrice() {
-			List<OrderItemRow> cart = SynologenSessionContext.EditOrderItemsInCart;
-			float returnValue = 0;
-			foreach(OrderItemRow order in cart) {
-				returnValue += order.DisplayTotalPrice;
-			}
-			return returnValue;
-		}
-
 		public void CheckEnableSaveAndAbortOrder() {
 			try {
-				//bool rstOK = !drpRST.SelectedValue.Equals("0") && !drpRST.SelectedValue.Equals("");
-				bool contractOK = !drpContracts.SelectedValue.Equals("0") && !drpContracts.SelectedValue.Equals("");
-				bool companyOK = !drpCompany.SelectedValue.Equals("0") && !drpCompany.SelectedValue.Equals("");
-				bool notMarkedAsPayed = !_order.MarkedAsPayedByShop;
-				//btnSave.Enabled = (rstOK && contractOK && companyOK && notMarkedAsPayed);
+				var contractOK = !drpContracts.SelectedValue.Equals("0") && !drpContracts.SelectedValue.Equals("");
+				var companyOK = !drpCompany.SelectedValue.Equals("0") && !drpCompany.SelectedValue.Equals("");
+				var notMarkedAsPayed = !_order.MarkedAsPayedByShop;
 				btnSave.Enabled = (contractOK && companyOK && notMarkedAsPayed);
 				btnAbort.Enabled = Business.Utility.General.IsOrderEditable(_order.StatusId);
 			}
 			catch{btnSave.Enabled = false;}
 		}
 
-		//public void CheckEnableMarkAsPayed() {
-		//    btnMarkAsPayed.Enabled = !_order.MarkedAsPayedByShop;
-		//    if(btnMarkAsPayed.Enabled) {
-		//        btnMarkAsPayed.Enabled = Business.Utility.IsOrderEditable(_order.StatusId);
-		//    }
-		//}
 
 		public void CheckDisplayHaltedButtons() {
 			if (Business.Utility.General.IsOrderHalted(_order.StatusId)) {
@@ -378,32 +309,16 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Wpc.Synologen {
 		}
 
 		private void TrySetCompany(int companyId) {
-			try { drpCompany.SelectedValue = companyId.ToString(); }
+			try{
+				drpCompany.SelectedValue = companyId.ToString();
+				if (companyId <= 0) return;
+				PopulateValidationRules(companyId, Controls);
+			}
 			catch { drpCompany.SelectedValue = "0"; }
 		}
 
-		//private void TrySetRST(int rstId) {
-		//    try { drpRST.SelectedValue = rstId.ToString(); }
-		//    catch { drpRST.SelectedValue = "0"; }
-		//}
-
 		#endregion
 
-		public int MaxNumberOfItems {
-			get { return _maxNumberOfItems; }
-			set { _maxNumberOfItems = value; }
-		}
-
-		protected void ValidateRSTLength(object source, ServerValidateEventArgs args) {
-			args.IsValid = CheckRSTLength(args.Value);
-
-		}
-
-		private static bool CheckRSTLength(string rstNumber) {
-			if (String.IsNullOrEmpty(rstNumber)) return false;
-			if (rstNumber.Trim().Length != RequiredRSTLength) return false;
-			return true;
-		}
 
 	}
 }

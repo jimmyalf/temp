6a74127d95cb23fe3dc5d9c2de96a25ea3c882a5
Carl Berg <carl.berg@spinit.se>
2009-10-06 13:20:09 +0000

    Moved formatting methods into their own class. Performed general refactoring. Moved VismaException into it's own class.

---
+++ Synologen Visma/VismaException.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Spinit.Wpc.Synologen.Visma {
+	/// <summary>
+	/// Represents error that occurs during Visma operations
+	/// </summary>
+	public class VismaException : Exception {public VismaException(string message):base(message) {}}
+}

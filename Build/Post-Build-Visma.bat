9da3a00f08dd96f3e4aca85152d5293ef492ffc9
Carl Berg <carl.berg@spinit.se>
2009-10-08 08:49:28 +0000

    Updated Adk dll path (to use Synologen version)

--- Build/Post-Build-Visma.bat
+++ Build/Post-Build-Visma.bat
@@ -1,3 +1,3 @@
 set rootPath=%~dp0..
-xcopy "%rootPath%\Dependancies\Tools\Adk.dll" 	"%rootPath%\Synologen Visma\bin\Debug\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\Tools\Adk.dll" 	"%rootPath%\Synologen Visma\bin\Release\" /R /K /Y /Q
+xcopy "%rootPath%\Dependancies\Spinit ADK\Adk.dll" 	"%rootPath%\Synologen Visma\bin\Debug\" /R /K /Y /Q
+xcopy "%rootPath%\Dependancies\Spinit ADK\Adk.dll" 	"%rootPath%\Synologen Visma\bin\Release\" /R /K /Y /Q

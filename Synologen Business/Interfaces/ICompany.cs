8f50fadda02a342e79afa71059b024486630f08d
Carl Berg <carl.berg@spinit.se>
2009-10-12 16:03:21 +0000

    Refactoring change

--- Synologen Business/Interfaces/ICompany.cs
+++ Synologen Business/Interfaces/ICompany.cs
@@ -3,8 +3,8 @@ namespace Spinit.Wpc.Synologen.Business.Interfaces {
 		int Id { get; set; }
 		int ContractId { get; set; }
 		string Name { get; set; }
-		string Address1 { get; set; }
-		string Address2 { get; set; }
+		string PostBox { get; set; }
+		string StreetName { get; set; }
 		string Zip { get; set; }
 		string City { get; set; }
 		string CompanyCode { get; set; }

48c7ae76cfa3fcaee6c2b2737d6dd6ff1e43624c
Anders Axelsson <anders.axelsson@spinit.se>
2009-11-09 11:45:15 +0000

    Added properties in path.

--- Synologen OPQ/Spinit.Wpc.Synologen.OPQ.Build/Pre-Build-Spinit.Wpc.Synologen.OPQ.Data.Test.bat
+++ Synologen OPQ/Spinit.Wpc.Synologen.OPQ.Build/Pre-Build-Spinit.Wpc.Synologen.OPQ.Data.Test.bat
@@ -1,4 +1,4 @@
 xcopy %1Spinit.Wpc.Synologen.OPQ.Config\%USERNAME%\%2\*.config %1%2\ /R /K /Y /Q /S
-xcopy %1Spinit.Wpc.Synologen.OPQ.Config\%USERNAME%\%2\Properties\*.cs %1%2\ /R /K /Y /Q /S
-xcopy %1Spinit.Wpc.Synologen.OPQ.Config\%USERNAME%\%2\Properties\*.settings %1%2\ /R /K /Y /Q /S
+xcopy %1Spinit.Wpc.Synologen.OPQ.Config\%USERNAME%\%2\Properties\*.cs %1%2\Properties\ /R /K /Y /Q /S
+xcopy %1Spinit.Wpc.Synologen.OPQ.Config\%USERNAME%\%2\Properties\*.settings %1%2\Properties\ /R /K /Y /Q /S
 

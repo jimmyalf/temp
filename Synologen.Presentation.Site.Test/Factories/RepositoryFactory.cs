e7a02ddd0f463ddb1fa51169557d6b1d2597a385
Carl Berg <carl.berg@spinit.se>
2010-08-13 11:43:51 +0000

    New Test project for Synologen Site

---
+++ Synologen.Presentation.Site.Test/Factories/RepositoryFactory.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using Spinit.Wpc.Synologen.Core.Domain.Model;
+using Spinit.Wpc.Synologen.Core.Domain.Persistence;
+using Spinit.Wpc.Synologen.Core.Persistence;
+
+namespace Synologen.Presentation.Site.Test.Factories
+{
+	public static class RepositoryFactory
+	{
+		
+		public static IFrameRepository GetFrameRepository()
+		{
+			return new MockedFrameRepository();
+		}
+
+		internal class MockedFrameRepository : IFrameRepository
+		{
+			public Frame Get(int id)
+			{
+				return new Frame
+				{
+					AllowOrders = true,
+					ArticleNumber = "123456",
+					Axis = 50,
+					Brand = new FrameBrand {Id = 1, Name = "Björn Borg"},
+					Color = new FrameColor {Id = 1, Name = "Blå"},
+					Id = id,
+					Name = "Bra båge",
+				}
+					.SetInterval(x => x.Index, 1.5m, 1.6m, 0.1m)
+					.SetInterval(x => x.Cylinder, -2, 0, 0.25m)
+					.SetInterval(x => x.PupillaryDistance, 20, 40, 0.5m)
+					.SetInterval(x => x.Sphere, -6, 6, 0.25m);
+			}
+			public IEnumerable<Frame> GetAll() { return new List<Frame>(); }
+			public IEnumerable<Frame> FindBy<TActionCriteria>(TActionCriteria criteria) where TActionCriteria : IActionCriteria { throw new NotImplementedException(); }
+			public void Save(Frame entity) { throw new NotImplementedException(); }
+			public void Delete(Frame entity) { throw new NotImplementedException(); }
+		}
+	}
+}

c74bc62895e72f50089746678ff47dcf344426fd
Carl Berg <carl.berg@spinit.se>
2010-08-19 16:00:14 +0000

    Added more assertions to apply for new model.

--- Synologen.Presentation.Site.Test/Factories/RepositoryFactory.cs
+++ Synologen.Presentation.Site.Test/Factories/RepositoryFactory.cs
@@ -22,21 +22,40 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Test.Factories
 				{
 					AllowOrders = true,
 					ArticleNumber = "123456",
-					//Axis = 50,
 					Brand = new FrameBrand {Id = 1, Name = "Björn Borg"},
 					Color = new FrameColor {Id = 1, Name = "Blå"},
 					Id = id,
 					Name = "Bra båge",
-				}
-					//.SetInterval(x => x.Index, 1.5m, 1.6m, 0.1m)
-					//.SetInterval(x => x.Cylinder, -2, 0, 0.25m)
-					//.SetInterval(x => x.Sphere, -6, 6, 0.25m)
-					.SetInterval(x => x.PupillaryDistance, 20, 40, 0.5m);
+				}.SetInterval(x => x.PupillaryDistance, 20, 40, 0.5m);
 			}
 			public IEnumerable<Frame> GetAll() { return new List<Frame>(); }
 			public IEnumerable<Frame> FindBy<TActionCriteria>(TActionCriteria criteria) where TActionCriteria : IActionCriteria { throw new NotImplementedException(); }
 			public void Save(Frame entity) { throw new NotImplementedException(); }
 			public void Delete(Frame entity) { throw new NotImplementedException(); }
 		}
+
+		public static IFrameGlassTypeRepository GetFrameGlassRepository()
+		{
+			return new MockedFrameGlassTypeRepository();
+		}
+
+		internal class MockedFrameGlassTypeRepository : IFrameGlassTypeRepository {
+			public FrameGlassType Get(int id)
+			{
+				return new FrameGlassType
+				{
+					Id = id,
+					IncludeAdditionParametersInOrder = true,
+					IncludeHeightParametersInOrder = false,
+					Name = "Närprogressiva"
+				};
+			}
+			public IEnumerable<FrameGlassType> GetAll() { return new List<FrameGlassType>(); }
+			public IEnumerable<FrameGlassType> FindBy<TActionCriteria>(TActionCriteria criteria) where TActionCriteria : IActionCriteria { throw new NotImplementedException();}
+			public void Save(FrameGlassType entity) { throw new NotImplementedException(); }
+			public void Delete(FrameGlassType entity) { throw new NotImplementedException(); }
+		}
 	}
+
+	
 }

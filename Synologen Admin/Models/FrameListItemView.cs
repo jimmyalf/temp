9291d2bb7474f8c7361857c0507165651aec2658
Carl Berg <carl.berg@spinit.se>
2010-08-26 16:04:28 +0000

    Added delete-button hideable interface for glasstype-list-view-item and frame-list-view-item.

--- Synologen Admin/Models/FrameListItemView.cs
+++ Synologen Admin/Models/FrameListItemView.cs
@@ -1,6 +1,8 @@
+using Spinit.Wpc.Synologen.Presentation.Helpers;
+
 namespace Spinit.Wpc.Synologen.Presentation.Models
 {
-	public class FrameListItemView
+	public class FrameListItemView : IDeleConfigurableGridItem
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
@@ -8,6 +10,10 @@ namespace Spinit.Wpc.Synologen.Presentation.Models
 		public bool AllowOrders { get; set; }
 		public string Color { get; set; }
 		public string Brand { get; set; }
+		public int NumberOfOrdersWithThisFrame { get; set; }
+		public bool AllowDelete
+		{
+			get { return (NumberOfOrdersWithThisFrame <= 0); }
+		}
 	}
-
 }

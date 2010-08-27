a350406a785997129e0adf73453528f5442f64ab
Carl Berg <carl.berg@spinit.se>
2010-08-27 08:00:21 +0000

    Added test for ViewFrameOrderController

--- Synologen Admin/Models/EyeParameterViewModel.cs
+++ Synologen Admin/Models/EyeParameterViewModel.cs
@@ -1,9 +1,17 @@
 using System.ComponentModel;
+using Spinit.Wpc.Synologen.Core.Domain.Model.FrameOrder;
 
 namespace Spinit.Wpc.Synologen.Presentation.Models
 {
 	public class EyeParameterViewModel
 	{
+		public EyeParameterViewModel(EyeParameter parameter)
+		{
+			Left = parameter.Left;
+			Right = parameter.Right;
+		}
+		public EyeParameterViewModel(){}
+
 		[DisplayName("Vänster")]
 		public decimal Left { get; set; }
 		[DisplayName("Höger")]

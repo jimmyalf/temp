932cc216ab605eaf6f3c1fbe3ca4433e74654d74
Patrik Ivarsson <patrik.ivarsson@spinit.se>
2010-10-20 14:42:20 +0000

    New CriteriaConverter

---
+++ Synologen Data/Repositories/CriteriaConverters/LensSubscription/CustomersForShopMatchingCriteriaConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using NHibernate.Criterion;
+using Spinit.Data.NHibernate;
+using Spinit.Wpc.Synologen.Core.Domain.Model.LensSubscription;
+using Spinit.Wpc.Synologen.Core.Domain.Persistence.Criterias.LensSubscription;
+
+namespace Spinit.Wpc.Synologen.Data.Repositories.CriteriaConverters.LensSubscription
+{
+	public class CustomersForShopMatchingCriteriaConverter : NHibernateActionCriteriaConverter<CustomersForShopMatchingCriteria, Customer>
+	{
+		public CustomersForShopMatchingCriteriaConverter(ISession session) : base(session)
+		{
+		}
+
+		public override ICriteria Convert(CustomersForShopMatchingCriteria source)
+		{
+			return Criteria
+				.CreateAlias(x => x.Shop)
+				.FilterEqual(x => x.Shop.Id, source.ShopId)
+				.FilterByAny(filter =>
+				             	{
+									filter.By(x => x.FirstName);
+									filter.By(x => x.LastName);
+									filter.By(x => x.PersonalIdNumber);
+				             	}, source.SearchTerm);
+		}
+	}
+}

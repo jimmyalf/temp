6204bd0e24a1d0881d49462b75010be56ae1dc90
Patrik Ivarsson <patrik.ivarsson@spinit.se>
2010-10-20 14:46:54 +0000

    ListCustomer Criterias/LensSubscription added

---
+++ Synologen.Core/Domain/Persistence/Criterias/LensSubscription/CustomersForShopMatchingCriteria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Spinit.Data;
+
+namespace Spinit.Wpc.Synologen.Core.Domain.Persistence.Criterias.LensSubscription
+{
+	public class CustomersForShopMatchingCriteria : IActionCriteria
+	{
+		public int ShopId { get; set; }
+		public string SearchTerm { get; set; }
+	}
+}

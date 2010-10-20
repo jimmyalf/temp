1e8300b0f5a520af9bc49e0cae3c89d5b6f409de
Patrik Ivarsson <patrik.ivarsson@spinit.se>
2010-10-20 08:04:09 +0000

    Complementary checkin

---
+++ Synologen IntegrationTest/LensSubscriptionData/Factories/CountryFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Spinit.Wpc.Synologen.Core.Domain.Model.LensSubscription;
+
+namespace Spinit.Wpc.Synologen.Integration.Test.LensSubscriptionData.Factories
+{
+	public static class CountryFactory
+	{
+		public static Country Get()
+		{
+			return new Country {Name = "Sverige"};
+		}
+	}
+}
--- Synologen.Core/Domain/Persistence/IFrameOrderRepository.cs
+++
@@ -1,7 +0,0 @@
-using Spinit.Data;
-using Spinit.Wpc.Synologen.Core.Domain.Model.FrameOrder;
-
-namespace Spinit.Wpc.Synologen.Core.Domain.Persistence
-{
-	public interface IFrameOrderRepository : IRepository<FrameOrder> { }
-}

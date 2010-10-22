91eb1cb1dd687aa772e4e1ce09ab6ce347e23fea
Carl Berg <carl.berg@spinit.se>
2010-10-22 12:51:00 +0000

    Added some extensions methods for With/Return, added basic EditLensSubscriptionPresenter tests.

---
+++ Synologen Site/Logic/Views/LensSubscription/IEditLensSubscriptionView.cs
@@ -0,0 +1,7 @@
+using Spinit.Wpc.Synologen.Presentation.Site.Models.LensSubscription;
+using WebFormsMvp;
+
+namespace Spinit.Wpc.Synologen.Presentation.Site.Logic.Views.LensSubscription
+{
+	public interface IEditLensSubscriptionView : IView<EditLensSubscriptionModel> {}
+}
--- Synologen.Core/Extensions/DateTimeExtensions.cs
+++ Synologen.Core/Extensions/DateTimeExtensions.cs
@@ -8,5 +8,15 @@ namespace Spinit.Wpc.Synologen.Core.Extensions
 		{
 			return value.Year == compareValue.Year && value.Month == compareValue.Month && value.Day == compareValue.Day;
 		}
+
+		public static DateTime SubtractDays(this DateTime value, int numberOfDays)
+		{
+			return value.AddDays(numberOfDays*-1);
+		}
+
+		public static DateTime SubtractMonths(this DateTime value, int numberOfMonths)
+		{
+			return value.AddMonths(numberOfMonths*-1);
+		}
 	}
 }

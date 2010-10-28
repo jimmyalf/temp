350076779c7fab8a9b49bd68c747990c32f39b4c
Carl Berg <carl.berg@spinit.se>
2010-10-28 08:39:32 +0000

    Minor refactorings

--- Synologen Admin/Code/SynologenPage.cs
+++ Synologen Admin/Code/SynologenPage.cs
@@ -9,86 +9,122 @@ using Spinit.Wpc.Synologen.Business;
 using Spinit.Wpc.Utility.Core;
 
 
-namespace Spinit.Wpc.Synologen.Presentation.Code {
+namespace Spinit.Wpc.Synologen.Presentation.Code 
+{
 	/// <summary>
 	/// Summary description for MemeberPage
 	/// </summary>
-	public class SynologenPage : System.Web.UI.Page {
-		private ApplicationSettingsRow _settings = null;
-		private string _connectionString = null;
-		private IBaseLocationRow _defaultLocation = null;
-		private IBaseLanguageRow _defaultLanguage = null;
-		private IBaseLocationRow _location = null;
-		private IBaseLanguageRow _language = null;
+	public class SynologenPage : System.Web.UI.Page 
+	{
+		private ApplicationSettingsRow _settings;
+		private string _connectionString;
+		private IBaseLocationRow _defaultLocation;
+		private IBaseLanguageRow _defaultLanguage;
+		private IBaseLocationRow _location;
+		private IBaseLanguageRow _language;
 		private List<IBaseLocationRow> _locations = new List<IBaseLocationRow>();
 		private List<IBaseLanguageRow> _languages = new List<IBaseLanguageRow>();
-		private Data.SqlProvider _provider = null;
+		private Data.SqlProvider _provider;
 		private int _userId = -1;
 		private string _user = String.Empty;
 		protected CustomValidator CustomValidation = new CustomValidator();
 
-		protected void MemberPage_Load(object sender, EventArgs e) {
+		protected override void OnInit(EventArgs e) {
+			Load += MemberPage_Load;
+			base.OnInit(e);
+			MemberPageInit();
+		}
+
+		private void MemberPageInit() {
+			_connectionString = ConfigurationManager.ConnectionStrings["WpcServer"].ConnectionString;
+			if (Cache["MemberSettings"] != null)
+				_settings = (ApplicationSettingsRow)Cache["MemberSettings"];
+			else {
+				//TODO Read from db
+				_settings = new ApplicationSettingsRow();
+				Cache["MemberSettings"] = _settings;
+			}
+			try {
+				var wpcContext = CxUser.Current;
+				_location = wpcContext.Location;
+				_language = wpcContext.Language;
+				_locations = wpcContext.Locations;
+				_languages = wpcContext.Languages;
+				_userId = wpcContext.User.Id;
+				_defaultLanguage = wpcContext.DefaultLanguage;
+				_defaultLocation = wpcContext.DefaultLocation;
+				_user = wpcContext.User.UserName;
+			}
+			catch {
+				_user = Context.User.Identity.Name;
+			}
+			_provider = new Data.SqlProvider(_connectionString);
+		}
+
+		protected void MemberPage_Load(object sender, EventArgs e) 
+		{
 			InitCustomValidationMessage();
 		}
 
-		private void InitCustomValidationMessage() {
+		private void InitCustomValidationMessage() 
+		{
 			CustomValidation.EnableViewState = false;
 			CustomValidation.Display = ValidatorDisplay.None;
 			Form.Controls.Add(CustomValidation);
 		}
 
-		public void DisplayMessage(string message, bool isError) {
+		public void DisplayMessage(string message, bool isError) 
+		{
 			CustomValidation.ValidationGroup = isError ? "Error" : "Success";
 			CustomValidation.ErrorMessage = message;
 			CustomValidation.IsValid = false;
 		}
 
-		protected void GiveFocus(System.Web.UI.Control c) {
-			StringBuilder script = new StringBuilder();
+		protected void GiveFocus(System.Web.UI.Control c) 
+		{
+			var script = new StringBuilder();
 			script.Append("<script language=\"JavaScript\">");
 			script.Append(Environment.NewLine);
 			script.Append("function CSP_focus(id) { var o = document.getElementById(id); if (o != null) o.focus(); }");
 			script.Append(Environment.NewLine);
 			script.Append("</script>");
 			Page.ClientScript.RegisterClientScriptBlock(Page.GetType(), "CSP-focus-function", script.ToString());
-			Page.ClientScript.RegisterStartupScript(Page.GetType(), "CSP-focus",
-			                                        "<script language=\"JavaScript\">CSP_focus('" + c.ClientID + "');</script>");
+			Page.ClientScript.RegisterStartupScript(Page.GetType(), "CSP-focus", "<script language=\"JavaScript\">CSP_focus('" + c.ClientID + "');</script>");
 		}
 
-		protected void DisplayAlert(string message) {
-			Page.ClientScript.RegisterClientScriptBlock(Page.GetType(), Guid.NewGuid().ToString(), "<script language=\"JavaScript\">" +
-			                                                                                       GetAlertScript(message) +
-			                                                                                       "</script>");
+		protected void DisplayAlert(string message) 
+		{
+			Page.ClientScript.RegisterClientScriptBlock(Page.GetType(), Guid.NewGuid().ToString(), "<script language=\"JavaScript\">" + GetAlertScript(message) + "</script>");
 		}
 
-		protected string GetAlertScript(string message) {
+		protected string GetAlertScript(string message) 
+		{
 			message = message.Replace("'", "\'");
 			message = message.Replace("\n", "\\n");
 			return "alert('" + message + "');";
 		}
 
-		protected bool SortAscending {
-			get {
-				object o = ViewState["SortAscending"];
-				if (o == null)
-					return true;
-				else
-					return Convert.ToBoolean(o);
+		protected bool SortAscending 
+		{
+			get
+			{
+				var o = ViewState["SortAscending"];
+				return o == null || Convert.ToBoolean(o);
 			}
 			set { ViewState["SortAscending"] = value; }
 		}
 
-		protected Data.SqlProvider Provider {
+		protected Data.SqlProvider Provider 
+		{
 			get { return _provider; }
 		}
 
-		protected string SortExpression {
-			get {
-				object o = ViewState["SortExpression"];
-				if (o == null)
-					return string.Empty;
-				else
-					return o.ToString();
+		protected string SortExpression 
+		{
+			get
+			{
+				var o = ViewState["SortExpression"];
+				return o == null ? string.Empty : o.ToString();
 			}
 			set { ViewState["SortExpression"] = value; }
 		}
@@ -143,9 +179,9 @@ namespace Spinit.Wpc.Synologen.Presentation.Code {
 		}
 
 		protected bool IsInRole(MemberRoles.Roles role) {
-			string sRole = role.ToString();
-			bool access = true;
-			string memberComponentName = Member.Business.Globals.ComponentName;
+			var sRole = role.ToString();
+			var access = true;
+			var memberComponentName = Member.Business.Globals.ComponentName;
 			try {
 				return access = ((CxUser)Context.User).IsInRole(memberComponentName, sRole);
 			}
@@ -156,9 +192,9 @@ namespace Spinit.Wpc.Synologen.Presentation.Code {
 		}
 
 		protected bool IsInSynologenRole(SynologenRoles.Roles role) {
-			string sRole = role.ToString();
-			bool access = true;
-			string synologenComponentName = Synologen.Business.Globals.ComponentName;
+			var sRole = role.ToString();
+			var access = true;
+			var synologenComponentName = Business.Globals.ComponentName;
 			try {
 				return access = ((CxUser)Context.User).IsInRole(synologenComponentName, sRole);
 			}
@@ -166,38 +202,6 @@ namespace Spinit.Wpc.Synologen.Presentation.Code {
 				// access = Context.User.IsInRole(sRole);
 			}
 			return access;
-		}
-
-		protected override void OnInit(EventArgs e) {
-			Load += MemberPage_Load;
-			base.OnInit(e);
-			MemberPageInit();
-		}
-
-		private void MemberPageInit() {
-			_connectionString = ConfigurationManager.ConnectionStrings["WpcServer"].ConnectionString;
-			if (Cache["MemberSettings"] != null)
-				_settings = (ApplicationSettingsRow)Cache["MemberSettings"];
-			else {
-				//TODO Read from db
-				_settings = new ApplicationSettingsRow();
-				Cache["MemberSettings"] = _settings;
-			}
-			try {
-				CxUser wpcContext = CxUser.Current;
-				_location = wpcContext.Location;
-				_language = wpcContext.Language;
-				_locations = wpcContext.Locations;
-				_languages = wpcContext.Languages;
-				_userId = wpcContext.User.Id;
-				_defaultLanguage = wpcContext.DefaultLanguage;
-				_defaultLocation = wpcContext.DefaultLocation;
-				_user = wpcContext.User.UserName;
-			}
-			catch {
-				_user = Context.User.Identity.Name;
-			}
-			_provider = new Spinit.Wpc.Synologen.Data.SqlProvider(_connectionString);
 		}
 	}
 }

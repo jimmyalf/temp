9879f454ee64722029786288fb1f34bc90545cb1
Carl Berg <carl.berg@spinit.se>
2010-10-28 08:39:32 +0000

    Minor refactorings

--- Synologen Admin/Components/Synologen/ViewSettlementDetails.aspx.cs
+++ Synologen Admin/Components/Synologen/ViewSettlementDetails.aspx.cs
@@ -1,46 +1,38 @@
 using System;
-using System.Data;
 using Spinit.Wpc.Synologen.Business.Domain.Entities;
 using Spinit.Wpc.Synologen.Business.Utility;
 using Spinit.Wpc.Synologen.Presentation.Code;
 
-namespace Spinit.Wpc.Synologen.Presentation.Components.Synologen {
-	public partial class ViewSettlementDetails : SynologenPage {
+namespace Spinit.Wpc.Synologen.Presentation.Components.Synologen 
+{
+	public partial class ViewSettlementDetails : SynologenPage 
+	{
 		private int _settlementId;
-		private Settlement settlement;
 		private float _totalValueIncludingVAT;
 		private float _totalValueExcludingVAT;
 
 
-		protected void Page_Load(object sender, EventArgs e) {
-			if (Request.Params["settlementId"] != null){
+		protected void Page_Load(object sender, EventArgs e) 
+		{
+			if (Request.Params["settlementId"] != null)
+			{
 				_settlementId = Convert.ToInt32(Request.Params["settlementId"]);
-				settlement = Provider.GetSettlement(_settlementId);
+				Settlement = Provider.GetSettlement(_settlementId);
 			}
 			if (Page.IsPostBack) return;
 			PopulateContractCustomerArticles();
 		}
 
-		private void PopulateContractCustomerArticles() {
-			DataSet customerArticles = Provider.GetSettlementDetailsDataSet(_settlementId, out _totalValueIncludingVAT, out _totalValueExcludingVAT, null);
+		private void PopulateContractCustomerArticles() 
+		{
+			var customerArticles = Provider.GetSettlementDetailsDataSet(_settlementId, out _totalValueIncludingVAT, out _totalValueExcludingVAT, null);
 			gvSettlementDetails.DataSource = customerArticles;
 			gvSettlementDetails.DataBind();
 		}
 
-		public Settlement Settlement { 
-			get { return settlement; }
-		}
-
-		public string GetSettlementPeriodNumber(DateTime dateTimeValue) {
-			return General.GetSettlementPeriodNumber(dateTimeValue);
-		}
-
-		public float TotalValueExcludingVAT {
-			get { return _totalValueExcludingVAT; }
-		}
-
-		public float TotalValueIncludingVAT {
-			get { return _totalValueIncludingVAT; }
-		}
+		public Settlement Settlement { get; private set; }
+		public string GetSettlementPeriodNumber(DateTime dateTimeValue) { return General.GetSettlementPeriodNumber(dateTimeValue); }
+		public float TotalValueExcludingVAT { get { return _totalValueExcludingVAT; } }
+		public float TotalValueIncludingVAT { get { return _totalValueIncludingVAT; } }
 	}
 }

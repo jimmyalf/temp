806d2f40a07f8982ac8f883d0dbfcfb5db910074
Carl Berg <carl.berg@spinit.se>
2010-12-02 16:36:58 +0000

    Added new criteria for fetching all unhandled subscription errors for a shop.

--- Synologen IntegrationTests.Data/LensSubscriptionData/SubscriptionErrorRepositoryTester.cs.cs
+++ Synologen IntegrationTests.Data/LensSubscriptionData/SubscriptionErrorRepositoryTester.cs.cs
@@ -1,7 +1,12 @@
-﻿using NHibernate;
+﻿using System.Collections.Generic;
+using System.Linq;
+using NHibernate;
 using NUnit.Framework;
 using Shouldly;
+using Spinit.Extensions;
 using Spinit.Wpc.Synologen.Core.Domain.Model.LensSubscription;
+using Spinit.Wpc.Synologen.Core.Domain.Persistence.Criterias.LensSubscription;
+using Spinit.Wpc.Synologen.Core.Extensions;
 using Spinit.Wpc.Synologen.Data.Repositories.LensSubscriptionRepositories;
 using Spinit.Wpc.Synologen.Integration.Data.Test.LensSubscriptionData.Factories;
 
@@ -88,6 +93,54 @@ namespace Spinit.Wpc.Synologen.Integration.Data.Test.LensSubscriptionData
 				savedError.HandledDate.ShouldBe(_errorToEdit.HandledDate);
 				savedError.IsHandled.ShouldBe(_errorToEdit.IsHandled);
 				savedError.Subscription.ShouldBe(_errorToEdit.Subscription);
+			});
+		}
+	}
+
+	[TestFixture]
+	[Category("SubscriptionErrorRepositoryTester")]
+	public class When_fetching_subscription_errors_by_AllUnhandledSubscriptionErrorsForShopCriteria : BaseRepositoryTester<SubscriptionErrorRepository>
+	{
+		private IList<SubscriptionError> _expectedErrorsForShop1;
+		private IList<SubscriptionError> _expectedErrorsForShop2;
+
+		public When_fetching_subscription_errors_by_AllUnhandledSubscriptionErrorsForShopCriteria()
+		{
+
+			Context = session =>
+			{
+				var shop = new ShopRepository(session).Get(TestShopId);
+				var country = new CountryRepository(session).Get(TestCountryId);
+				var customer = CustomerFactory.Get(country, shop);
+				new CustomerRepository(session).Save(customer);
+				var subscription = SubscriptionFactory.Get(customer);
+				new SubscriptionRepository(session).Save(subscription);
+				_expectedErrorsForShop1 = SubscriptionErrorFactory.GetList(subscription);
+
+				var shop2 = new ShopRepository(session).Get(TestShop2Id);
+				var customer2 = CustomerFactory.Get(country, shop2);
+				new CustomerRepository(session).Save(customer2);
+				var subscription2 = SubscriptionFactory.Get(customer2);
+				new SubscriptionRepository(session).Save(subscription2);
+				_expectedErrorsForShop2 = SubscriptionErrorFactory.GetList(subscription2);
+			};
+			Because = repository =>
+			{
+				_expectedErrorsForShop1.Each(repository.Save);
+				_expectedErrorsForShop2.Each(repository.Save);
+			}; 
+		}
+
+		[Test]
+		public void Should_get_all_handled_subscription_errors_in_reversed_order()
+		{
+			var criteria = new AllUnhandledSubscriptionErrorsForShopCriteria(TestShopId);
+			var expectedErrors = _expectedErrorsForShop1.Where(x => Equals(x.HandledDate, null));
+			AssertUsing( session =>
+			{
+				var fetchedErrors = new SubscriptionErrorRepository(session).FindBy(criteria);
+				fetchedErrors.Count().ShouldBe(expectedErrors.Count());
+				fetchedErrors.First().CreatedDate.ShouldBeGreaterThan(fetchedErrors.Last().CreatedDate);
 			});
 		}
 	}

186e01f334d5e3a58642ca519f6497b2e2bf05dd
Carl Berg <carl.berg@spinit.se>
2010-12-03 11:18:14 +0000

    Renamed bootstrapper class

--- Synologen Site/WebBootstrapper.cs
+++ Synologen Site/WebBootstrapper.cs
@@ -2,11 +2,11 @@ using Spinit.Wpc.Core.UI;
 
 namespace Spinit.Wpc.Synologen.Presentation.Site
 {
-	public class Bootstrapper : IWpcComponentBootstrapper
+	public class WebBootstrapper : IWpcComponentBootstrapper
 	{
 		public void Bootstrap(WpcBootstrapperContext context)
 		{
-			Spinit.Wpc.Core.UI.Bootstrapper.Bootstrap(Dependency.NHibernate, context);
+			Bootstrapper.Bootstrap(Dependency.NHibernate, context);
 		}
 	}
 }

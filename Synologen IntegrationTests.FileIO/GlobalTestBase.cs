1506319ad4512ba5313ca78bb43a0fd159218dd4
Carl Berg <carl.berg@spinit.se>
2010-12-08 12:59:12 +0000

    Moved config files into Synologen.Config folder to enable different config files per user.

---
+++ Synologen IntegrationTests.FileIO/GlobalTestBase.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Synologen_IntegrationTests.FileIO
+{
+	[SetUpFixture]
+	public class GlobalTestBase
+	{
+
+		[SetUp]
+		public void RunBeforeAnyTests(){ }
+
+		[TearDown]
+		public void RunAfterAnyTests(){ }
+	}
+}

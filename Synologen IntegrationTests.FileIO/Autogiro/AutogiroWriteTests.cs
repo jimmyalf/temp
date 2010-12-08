8112a3d218339212a03f8c239957f347673aa256
Carl Berg <carl.berg@spinit.se>
2010-12-08 16:21:20 +0000

    Added read/write tests for autogiro files.

---
+++ Synologen IntegrationTests.FileIO/Autogiro/AutogiroWriteTests.cs
@@ -0,0 +1,71 @@
+using NUnit.Framework;
+using Shouldly;
+using Spinit.Wp.Synologen.Autogiro;
+using Spinit.Wp.Synologen.Autogiro.Writers;
+using Spinit.Wpc.Synologen.Core.Domain.Model.Autogiro.Send;
+using Spinit.Wpc.Synologen.Integration.FileIO.Test.Autogiro.Factories;
+using Spinit.Wpc.Synologen.Integration.FileIO.Test.Helpers;
+
+namespace Spinit.Wpc.Synologen.Integration.FileIO.Test.Autogiro
+{
+
+	[TestFixture]
+	[Category("AutogiroFileWriterTester")]
+	public class When_writing_payments_file : IOBase
+	{
+		private readonly string _filePath;
+		private readonly string _expectedFileContents;
+		private AutogiroFileWriter<PaymentsFile> _writer;
+
+		public When_writing_payments_file()
+		{
+			_expectedFileContents = FileContentFactory.GetLayoutB();
+			_filePath = GetWriteFilePath(@"\Autogiro_Payments.txt");
+			var paymentsToWrite = FileFactory.GetPaymentsFile();
+			Context = () =>
+			{
+				var fileWriter = new PaymentsFileWriter();
+				_writer = new AutogiroFileWriter<PaymentsFile>(fileWriter, _filePath);
+			};
+			Because = () => { _writer.Write(paymentsToWrite); };
+			
+		}
+
+		[Test]
+		public void Written_file_contains_expected_content()
+		{
+			var writtenFileContents = ReadFileContents(_filePath);
+			writtenFileContents.ShouldBe(_expectedFileContents);
+		}
+	}
+
+	[TestFixture]
+	[Category("AutogiroFileWriterTester")]
+	public class When_writing_consents_file : IOBase
+	{
+		private readonly string _expectedFileContents;
+		private readonly string _filePath;
+		private AutogiroFileWriter<ConsentsFile> _writer;
+
+		public When_writing_consents_file()
+		{
+			_expectedFileContents = FileContentFactory.GetLayoutA();
+			_filePath = GetWriteFilePath(@"\Autogiro_Consents.txt");
+			var consentsToWrite = FileFactory.GetConsentsFile();
+			Context = () =>
+			{
+				var fileWriter = new ConsentsFileWriter();
+				_writer = new AutogiroFileWriter<ConsentsFile>(fileWriter, _filePath);
+			};
+			Because = () => { _writer.Write(consentsToWrite); };
+		}
+
+		[Test]
+		public void Written_file_contains_expected_content()
+		{
+			var writtenFileContents = ReadFileContents(_filePath);
+			writtenFileContents.ShouldBe(_expectedFileContents);
+		}
+	}
+
+}
---
+++ Synologen IntegrationTests.FileIO/Helpers/StringExtensions.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Spinit.Wpc.Synologen.Integration.FileIO.Test.Helpers
+{
+	public static class StringExtensions
+	{
+		public static string AppendPaths(params string[] pathArray)
+		{
+			return String.Empty.AppendPaths(pathArray);
+		}
+
+		public static string AppendPaths(this string root, params string[] pathArray)
+		{
+			if(root == null) return null;
+			var returnString = root;
+			foreach (var pathItem in pathArray)
+			{
+				if(String.IsNullOrEmpty(pathItem)) continue;
+				returnString = String.Concat(returnString.TrimEnd('\\'), "\\", pathItem.TrimStart('\\'));
+			}
+			return returnString;
+		}
+	}
+}

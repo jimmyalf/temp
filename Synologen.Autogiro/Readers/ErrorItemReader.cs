f805d77e637e7709bc978f027c85c714f40cc092
Patrik Ivarsson <patrik.ivarsson@spinit.se>
2010-12-09 14:09:15 +0000

    Klasser i Synologen.Autogiro.Readers f?r att l?sa felfil fr?n BGC Tester f?r dessa klasser samt tester i Synologen.IntegrationsTests.FileIO

---
+++ Synologen.Autogiro/Readers/ErrorItemReader.cs
@@ -0,0 +1,26 @@
+﻿
+using Spinit.Wp.Synologen.Autogiro.Helpers;
+using Spinit.Wpc.Synologen.Core.Domain.Model.Autogiro.CommonTypes;
+using Spinit.Wpc.Synologen.Core.Domain.Model.Autogiro.Recieve;
+using Spinit.Wpc.Synologen.Core.Extensions;
+
+namespace Spinit.Wp.Synologen.Autogiro.Readers
+{
+	public class ErrorItemReader : IItemReader<Error> 
+	{
+		public Error Read(string line)
+		{
+			return new Error
+					{
+						Amount = line.ReadFrom(31).To(42).TrimStart('0').ParseAmount(),
+						CommentCode = line.ReadFrom(59).To(60).ParseEnum<ErrorCommentCode>(),
+						NumberOfReoccuringTransactionsLeft = line.ReadFrom(12).To(14).ParseNullable<int>(StringExtensions.ToInt),
+						PaymentDate = line.ReadFrom(3).To(10).ParseDate(),
+						PeriodCode = line.ReadFrom(11).To(11).ParseEnum<PeriodCode>(),
+						Reference = line.ReadFrom(43).To(58).TrimEnd(),
+						Transmitter = new Payer { CustomerNumber = line.ReadFrom(15).To(30).TrimStart('0') },
+						Type = line.ReadFrom(1).To(2).ParseEnum<PaymentType>()
+					};
+		}
+	}
+}

91516cdc1a5b3367c6b04b021f107cd8f2bd93f5
Carl Berg <carl.berg@spinit.se>
2011-01-18 09:52:29 +0000

    New update to safely (regarding null possibilities) create email

--- Synologen Site/Logic/Services/SynologenFrameOrderService.cs
+++ Synologen Site/Logic/Services/SynologenFrameOrderService.cs
@@ -1,5 +1,7 @@
+using System;
 using Spinit.Wpc.Synologen.Core.Domain.Model.FrameOrder;
 using Spinit.Wpc.Synologen.Core.Domain.Services;
+using Spinit.Wpc.Synologen.Core.Extensions;
 
 namespace Spinit.Wpc.Synologen.Presentation.Site.Logic.Services
 {
@@ -26,24 +28,24 @@ namespace Spinit.Wpc.Synologen.Presentation.Site.Logic.Services
 			return 
 				template
 				.Replace("{OrderId}",order.Id.ToString())
-				.Replace("{ShopName}", order.OrderingShop.Name)
-				.Replace("{ShopCity}",order.OrderingShop.Address.City)
-				.Replace("{FrameName}", order.Frame.Name)
-			    .Replace("{ArticleNumber}",order.Frame.ArticleNumber)
-			    .Replace("{PDLeft}", order.PupillaryDistance.Left.ToString())
-			    .Replace("{PDRight}", order.PupillaryDistance.Right.ToString())
-			    .Replace("{GlassTypeName}", order.GlassType.Name)
-			    .Replace("{SphereLeft}", order.Sphere.Left.ToString())
-			    .Replace("{SphereRight}", order.Sphere.Right.ToString())
-			    .Replace("{CylinderLeft}", order.Cylinder.Left.ToString())
-			    .Replace("{CylinderRight}", order.Cylinder.Right.ToString())
-			    .Replace("{AxisLeft}", order.Axis.Left.ToString())
-			    .Replace("{AxisRight}", order.Axis.Right.ToString())
-			    .Replace("{AdditionLeft}", (order.Addition !=null) ?  order.Addition.Left.ToString() : null)
-			    .Replace("{AdditionRight}", (order.Addition !=null) ?  order.Addition.Right.ToString() : null)
-			    .Replace("{HeightLeft}", (order.Height !=null) ?  order.Height.Left.ToString() : null)
-			    .Replace("{HeightRight}", (order.Height !=null) ?  order.Height.Right.ToString() : null)
-			    .Replace("{Reference}", order.Reference);
+				.Replace("{ShopName}", order.With(x => x.OrderingShop).Return(x => x.Name, string.Empty))
+				.Replace("{ShopCity}",order.With(x => x.OrderingShop).With(y => y.Address).Return(z => z.City, string.Empty))
+				.Replace("{FrameName}", order.With(x => x.Frame).Return(x => x.Name, string.Empty))
+			    .Replace("{ArticleNumber}", order.With(x => x.Frame).Return(x => x.ArticleNumber, string.Empty))
+			    .Replace("{PDLeft}", order.With(x => x.PupillaryDistance).Return(x => x.Left.SafeToString(), string.Empty))
+			    .Replace("{PDRight}", order.With(x => x.PupillaryDistance).Return(x=> x.Right.SafeToString(), string.Empty))
+			    .Replace("{GlassTypeName}", order.With(x => x.GlassType).Return(x => x.Name, string.Empty))
+			    .Replace("{SphereLeft}", order.With(x => x.Sphere).Return(x => x.Left.SafeToString(), string.Empty))
+			    .Replace("{SphereRight}", order.With(x => x.Sphere).Return(x => x.Right.SafeToString(), string.Empty))
+			    .Replace("{CylinderLeft}", order.With(x => x.Cylinder).Return(x => x.Left.SafeToString(), string.Empty))
+			    .Replace("{CylinderRight}", order.With(x => x.Cylinder).Return(x => x.Right.SafeToString(), string.Empty))
+			    .Replace("{AxisLeft}", order.With(x => x.Axis).Return(x => x.Left.SafeToString(), string.Empty))
+			    .Replace("{AxisRight}", order.With(x => x.Axis).Return(x => x.Right.SafeToString(), string.Empty))
+			    .Replace("{AdditionLeft}", order.With(x => x.Addition).Return(x => x.Left.SafeToString(), string.Empty))
+			    .Replace("{AdditionRight}", order.With(x => x.Addition).Return(x => x.Right.SafeToString(), string.Empty))
+			    .Replace("{HeightLeft}", order.With(x => x.Height).Return(x => x.Left.SafeToString(), string.Empty))
+			    .Replace("{HeightRight}", order.With(x => x.Height).Return(x => x.Right.SafeToString(), string.Empty))
+			    .Replace("{Reference}", order.Return(x => x.Reference, string.Empty));
 		}
 	}
 }

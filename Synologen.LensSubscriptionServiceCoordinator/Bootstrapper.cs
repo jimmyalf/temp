f36fb29f1bfc0bab221a57660a438c5a52d6eb02
Carl Berg <carl.berg@spinit.se>
2011-01-28 14:52:39 +0000

    Moved task base into core.

--- Synologen.LensSubscriptionServiceCoordinator/Bootstrapper.cs
+++ Synologen.LensSubscriptionServiceCoordinator/Bootstrapper.cs
@@ -1,3 +1,4 @@
+using Spinit.Data;
 using Spinit.Wpc.Synologen.LensSubscriptionServiceCoordinator.App.Ioc;
 using StructureMap;
 
@@ -8,6 +9,7 @@ namespace Spinit.Wpc.Synologen.LensSubscriptionServiceCoordinator
 		public static void Bootstrap()
 		{
 			ObjectFactory.Initialize(x => x.AddRegistry<TaskRunnerRegistry>());
+			ActionCriteriaExtensions.ConstructConvertersUsing(ObjectFactory.GetInstance);
 		}
 		
 	}

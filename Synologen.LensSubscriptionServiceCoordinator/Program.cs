d3ba00fcce5a3c432ce8aa4d77c052ddbbdfea86
Carl Berg <carl.berg@spinit.se>
2011-02-01 12:22:04 +0000

    Added better logging handling for task runner.

--- Synologen.LensSubscriptionServiceCoordinator/Program.cs
+++ Synologen.LensSubscriptionServiceCoordinator/Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Spinit.Extensions;
 using Spinit.Wpc.Synologen.Core.Domain.Services;
 using Spinit.Wpc.Synologen.Core.Domain.Services.Coordinator;
+using Spinit.Wpc.Synologen.LensSubscriptionServiceCoordinator.App.Logging;
 using StructureMap;
 
 namespace Spinit.Wpc.Synologen.LensSubscriptionServiceCoordinator
@@ -11,14 +12,30 @@ namespace Spinit.Wpc.Synologen.LensSubscriptionServiceCoordinator
 	{
 		public static void Main(string[] args)
 		{
+			var loggingService = LogFactory.CreateLoggingService();
+			try
+			{
+				loggingService.LogInfo("Taskrunner started execution");
+				RunTasks(loggingService);
+				loggingService.LogInfo("Taskrunner finished execution");
+			}
+			catch(Exception ex)
+			{
+				loggingService.LogError("Taskrunner caught an exception", ex);
+			}
+		}
+
+		public static void RunTasks(ILoggingService loggingService)
+		{
+			loggingService.LogDebug("Taskrunner bootstrapping starting...");
 			Bootstrapper.Bootstrap();
-			var loggingService = ObjectFactory.GetInstance<ILoggingService>();
-			loggingService.LogInfo("Taskrunner started execution");
+			loggingService.LogDebug("Taskrunner bootstrapping finished.");
+			loggingService.LogDebug("Taskrunner fetching tasks from container...");
 			var tasks = ObjectFactory.GetAllInstances<ITask>() ?? Enumerable.Empty<ITask>();
 			loggingService.LogInfo("Taskrunner scan found {0} tasks ({1})", tasks.Count(), tasks.Select(x => x.TaskName).Aggregate((taskA,taskB) => taskA + ", " + taskB));
 			tasks.Each(task => ExecuteLoggedTask(task, loggingService));
-			loggingService.LogInfo("Taskrunner finished execution");
 		}
+
 
 		public static void ExecuteLoggedTask(ITask task, ILoggingService loggingService)
 		{

71afed14b5f909b7460fcc82f3e9cd14caadc41f
Carl Berg <carl.berg@spinit.se>
2011-02-07 12:20:14 +0000

    Updated paths after project restructuring

--- Build/Post-Build-Invoice.bat
+++
@@ -1,5 +0,0 @@
-set rootPath=%~dp0..
-xcopy "%rootPath%\Dependancies\Tools\DeployLX.Licensing.v3.dll" 	"%rootPath%\Synologen Invoice\bin\Debug\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\Tools\DeployLX.Licensing.v3.dll" 	"%rootPath%\Synologen Invoice\bin\Release\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\Tools\SFTI-Helpers.dll"				"%rootPath%\Synologen Invoice\bin\Debug\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\Tools\SFTI-Helpers.dll"				"%rootPath%\Synologen Invoice\bin\Release\" /R /K /Y /Q
--- Build/Post-Build-LensSubscriptionServiceCoordinator.bat
+++ Build/Post-Build-LensSubscriptionServiceCoordinator.bat
@@ -1,8 +1,8 @@
-set rootPath=%~dp0..
-xcopy "%rootPath%\Dependancies\FluentNHibernate\NHibernate.ByteCode.Castle.dll" 	"%rootPath%\Synologen.LensSubscriptionServiceCoordinator\bin\Debug\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\FluentNHibernate\Castle.DynamicProxy2.dll" 			"%rootPath%\Synologen.LensSubscriptionServiceCoordinator\bin\Debug\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\FluentNHibernate\Castle.Core.dll" 					"%rootPath%\Synologen.LensSubscriptionServiceCoordinator\bin\Debug\" /R /K /Y /Q
+set rootPath=%~dp0..\..
+xcopy "%rootPath%\Dependancies\FluentNHibernate\NHibernate.ByteCode.Castle.dll" 	"%rootPath%\Synologen.LensSubscription\Synologen.LensSubscription.ServiceCoordinator\bin\Debug\" /R /K /Y /Q
+xcopy "%rootPath%\Dependancies\FluentNHibernate\Castle.DynamicProxy2.dll" 			"%rootPath%\Synologen.LensSubscription\Synologen.LensSubscription.ServiceCoordinator\bin\Debug\" /R /K /Y /Q
+xcopy "%rootPath%\Dependancies\FluentNHibernate\Castle.Core.dll" 					"%rootPath%\Synologen.LensSubscription\Synologen.LensSubscription.ServiceCoordinator\bin\Debug\" /R /K /Y /Q
 
-xcopy "%rootPath%\Dependancies\FluentNHibernate\NHibernate.ByteCode.Castle.dll" 	"%rootPath%\Synologen.LensSubscriptionServiceCoordinator\bin\Release\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\FluentNHibernate\Castle.DynamicProxy2.dll" 			"%rootPath%\Synologen.LensSubscriptionServiceCoordinator\bin\Release\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\FluentNHibernate\Castle.Core.dll" 					"%rootPath%\Synologen.LensSubscriptionServiceCoordinator\bin\Release\" /R /K /Y /Q
+xcopy "%rootPath%\Dependancies\FluentNHibernate\NHibernate.ByteCode.Castle.dll" 	"%rootPath%\Synologen.LensSubscription\Synologen.LensSubscription.ServiceCoordinator\bin\Release\" /R /K /Y /Q
+xcopy "%rootPath%\Dependancies\FluentNHibernate\Castle.DynamicProxy2.dll" 			"%rootPath%\Synologen.LensSubscription\Synologen.LensSubscription.ServiceCoordinator\bin\Release\" /R /K /Y /Q
+xcopy "%rootPath%\Dependancies\FluentNHibernate\Castle.Core.dll" 					"%rootPath%\Synologen.LensSubscription\Synologen.LensSubscription.ServiceCoordinator\bin\Release\" /R /K /Y /Q

de0ba4b9e50523c7179d86f485fb69491c210424
Carl Berg <carl.berg@spinit.se>
2011-02-11 15:19:04 +0000

    updated Bootstrapper

--- Synologen.LensSubscription/Synologen.LensSubscription.BGServiceCoordinator/Bootstrapper.cs
+++ Synologen.LensSubscription/Synologen.LensSubscription.BGServiceCoordinator/Bootstrapper.cs
@@ -1,10 +1,15 @@
+using Spinit.Data;
+using Spinit.Wpc.Synologen.LensSubscription.BGServiceCoordinator.IoC;
+using StructureMap;
+
 namespace Spinit.Wpc.Synologen.LensSubscription.BGServiceCoordinator
 {
 	public static class Bootstrapper
 	{
 		public static void Bootstrap()
 		{
-			
+			ObjectFactory.Initialize(x => x.AddRegistry<TaskRunnerRegistry>());
+			ActionCriteriaExtensions.ConstructConvertersUsing(ObjectFactory.GetInstance);
 		}
 	}
 }

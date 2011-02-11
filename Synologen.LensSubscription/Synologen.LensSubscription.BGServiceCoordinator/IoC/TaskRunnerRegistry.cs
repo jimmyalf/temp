a51c5509318e7e17cdcbde3b64b63ecc21e3d0e4
Carl Berg <carl.berg@spinit.se>
2011-02-11 15:18:24 +0000

    Added main functionality classes to project.

---
+++ Synologen.LensSubscription/Synologen.LensSubscription.BGServiceCoordinator/IoC/TaskRunnerRegistry.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using NHibernate;
+using Spinit.Data;
+using Spinit.Data.NHibernate;
+using Spinit.Synologen.LensSubscription.BGData;
+using Spinit.Wpc.Synologen.Core.Domain.Services;
+using Spinit.Wpc.Synologen.Core.Domain.Services.Coordinator;
+using Spinit.Wpc.Synologen.LensSubscription.BGServiceCoordinator.Logging;
+using StructureMap.Configuration.DSL;
+
+namespace Spinit.Wpc.Synologen.LensSubscription.BGServiceCoordinator.IoC
+{
+	public class TaskRunnerRegistry : Registry
+	{
+		public TaskRunnerRegistry()
+		{
+			// NHibernate
+			For<IUnitOfWork>().HybridHttpOrThreadLocalScoped().Use<NHibernateUnitOfWork>();
+			For<ISessionFactory>().Singleton().Use(NHibernateFactory.Instance.GetSessionFactory);
+			For<ISession>().Use(x => ((NHibernateUnitOfWork)x.GetInstance<IUnitOfWork>()).Session);
+
+			// Logging
+			For<ILoggingService>().Singleton().Use(LogFactory.CreateLoggingService());
+	
+			// Task scan
+			Scan(x =>
+			{
+				x.Assembly(Assembly.GetExecutingAssembly());
+				x.AddAllTypesOf<ITask>();
+			});
+
+			// Register criteria converters
+			//Scan(x =>
+			//{
+			//    x.AssemblyContainingType<PageOfFramesMatchingCriteriaConverter>();
+			//    x.Assembly(typeof(NHibernateActionCriteriaConverter<,>).Assembly.FullName);
+			//    x.ConnectImplementationsToTypesClosing(typeof(IActionCriteriaConverter<,>));
+			//});
+		}
+	}
+}

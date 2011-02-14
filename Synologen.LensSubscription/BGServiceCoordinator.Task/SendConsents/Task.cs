758ef03bfe0258f665629d9838856cbc016d92d6
Carl Berg <carl.berg@spinit.se>
2011-02-14 12:46:34 +0000

    Removed long path names

---
+++ Synologen.LensSubscription/BGServiceCoordinator.Task/SendConsents/Task.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Spinit.Wpc.Synologen.Core.Domain.Services;
+using Spinit.Wpc.Synologen.Core.Domain.Services.Coordinator;
+
+namespace Synologen.LensSubscription.BGServiceCoordinator.Task.SendConsents
+{
+	public class Task : TaskBase
+	{
+		public Task(ILoggingService loggingService) : base("SendConsents", loggingService) {}
+		public override void Execute() { throw new NotImplementedException(); }
+	}
+}

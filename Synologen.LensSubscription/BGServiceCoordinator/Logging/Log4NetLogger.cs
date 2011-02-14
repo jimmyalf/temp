c4fc6ce036d6fa0a6748e662b40332a497f64594
Carl Berg <carl.berg@spinit.se>
2011-02-14 13:49:44 +0000

    Updates after project restructuring

--- Synologen.LensSubscription/BGServiceCoordinator/Logging/Log4NetLogger.cs
+++ Synologen.LensSubscription/BGServiceCoordinator/Logging/Log4NetLogger.cs
@@ -2,7 +2,7 @@ using System;
 using log4net;
 using Spinit.Wpc.Synologen.Core.Domain.Services;
 
-namespace Spinit.Wpc.Synologen.LensSubscription.BGServiceCoordinator.Logging
+namespace Synologen.LensSubscription.BGServiceCoordinator.Logging
 {
 	public class Log4NetLogger : ILoggingService
 	{

5abe7fea47101373717559ce52d4c2ebae7d14b9
Carl Berg <carl.berg@spinit.se>
2011-02-14 13:14:25 +0000

    Updates after project restructuring

--- Synologen.LensSubscription/BGData.Test/CommonDataTestHelpers/NHibernateRepositoryTester.cs
+++ Synologen.LensSubscription/BGData.Test/CommonDataTestHelpers/NHibernateRepositoryTester.cs
@@ -2,127 +2,127 @@
 using NHibernate;
 using NUnit.Framework;
 
-namespace Spinit.Wpc.Synologen.Integration.Data.Test.CommonDataTestHelpers
+namespace Synologen.LensSubscription.BGData.Test.CommonDataTestHelpers
 {
-    public abstract class NHibernateRepositoryTester<TRepository>
-    {
-        protected NHibernateRepositoryTester()
-        {
-            Context = (ISession session) => { };
-            Because = (TRepository session) =>
-            {
-                throw new AssertionException("An action for Because has not been set!");
-            };
-        }
+	public abstract class NHibernateRepositoryTester<TRepository>
+	{
+		protected NHibernateRepositoryTester()
+		{
+			Context = (ISession session) => { };
+			Because = (TRepository session) =>
+			{
+				throw new AssertionException("An action for Because has not been set!");
+			};
+		}
 
-        [SetUp]
-        protected void SetUpTest()
-        {
-            SetUp().Invoke();
+		[SetUp]
+		protected void SetUpTest()
+		{
+			SetUp().Invoke();
 
-            ISession contextSession = null;
-            try
-            {
-                using (contextSession = GetSessionFactory().OpenSession())
-                {
-                    Context(contextSession);
-                }
-            }
-            catch
-            {
-                if (contextSession != null)
-                {
-                    contextSession.Dispose();
-                }
-                throw;
-            }
+			ISession contextSession = null;
+			try
+			{
+				using (contextSession = GetSessionFactory().OpenSession())
+				{
+					Context(contextSession);
+				}
+			}
+			catch
+			{
+				if (contextSession != null)
+				{
+					contextSession.Dispose();
+				}
+				throw;
+			}
 
-            ISession becauseSession = null;
-            try
-            {
-                using (becauseSession = GetSessionFactory().OpenSession())
-                {
-                    var repository = CreateRepository(becauseSession);
-                    Because(repository);
-                }
-            }
-            catch
-            {
-                if (becauseSession != null)
-                {
-                    becauseSession.Dispose();
-                }
-                throw;
-            }
-        }
+			ISession becauseSession = null;
+			try
+			{
+				using (becauseSession = GetSessionFactory().OpenSession())
+				{
+					var repository = CreateRepository(becauseSession);
+					Because(repository);
+				}
+			}
+			catch
+			{
+				if (becauseSession != null)
+				{
+					becauseSession.Dispose();
+				}
+				throw;
+			}
+		}
 
-        [TearDown]
-        public void TearDownTest()
-        {
-            TearDown().Invoke();
-        }
+		[TearDown]
+		public void TearDownTest()
+		{
+			TearDown().Invoke();
+		}
 
-        protected virtual Action SetUp()
-        {
-            return () => { };
-        }
+		protected virtual Action SetUp()
+		{
+			return () => { };
+		}
 
-        protected virtual Action TearDown()
-        {
-            return () => { };
-        }
+		protected virtual Action TearDown()
+		{
+			return () => { };
+		}
 
-        protected Action<ISession> Context;
-        protected Action<TRepository> Because;
+		protected Action<ISession> Context;
+		protected Action<TRepository> Because;
 
-        protected abstract ISessionFactory GetSessionFactory();
+		protected abstract ISessionFactory GetSessionFactory();
 
-        protected virtual TRepository CreateRepository(ISession session)
-        {
-            var args = new object[] { session };
-            return (TRepository)Activator.CreateInstance(typeof(TRepository), args);
-        }
+		protected virtual TRepository CreateRepository(ISession session)
+		{
+			var args = new object[] { session };
+			return (TRepository)Activator.CreateInstance(typeof(TRepository), args);
+		}
 
-        protected void AssertUsing(Action<ISession> action)
-        {
-            ISession verificationSession = null;
-            try
-            {
-                using (verificationSession = GetSessionFactory().OpenSession())
-                {
-                    action(verificationSession);
-                }
-            }
-            catch
-            {
-                if (verificationSession != null)
-                {
-                    verificationSession.Dispose();
-                }
-                throw;
-            }
-        }
+		protected void AssertUsing(Action<ISession> action)
+		{
+			ISession verificationSession = null;
+			try
+			{
+				using (verificationSession = GetSessionFactory().OpenSession())
+				{
+					action(verificationSession);
+				}
+			}
+			catch
+			{
+				if (verificationSession != null)
+				{
+					verificationSession.Dispose();
+				}
+				throw;
+			}
+		}
 
-        protected TResult GetResult<TResult>(Func<ISession, TResult> function)
-        {
-            ISession verificationSession = null;
-            TResult result;
-            try
-            {
-                using (verificationSession = GetSessionFactory().OpenSession())
-                {
-                    result = function(verificationSession);
-                }
-            }
-            catch
-            {
-                if (verificationSession != null)
-                {
-                    verificationSession.Dispose();
-                }
-                throw;
-            }
-            return result;
-        }
-    }
+		protected TResult GetResult<TResult>(Func<ISession, TResult> function)
+		{
+			ISession verificationSession = null;
+			TResult result;
+			try
+			{
+				using (verificationSession = GetSessionFactory().OpenSession())
+				{
+					result = function(verificationSession);
+				}
+			}
+			catch
+			{
+				if (verificationSession != null)
+				{
+					verificationSession.Dispose();
+				}
+				throw;
+			}
+			return result;
+		}
+	}
 }
--- Synologen.LensSubscription/BGData.Test/Factories/ReceivedFileSectionFactory.cs
+++ Synologen.LensSubscription/BGData.Test/Factories/ReceivedFileSectionFactory.cs
@@ -1,20 +1,20 @@
 ﻿using System;
 using Spinit.Wpc.Synologen.Core.Domain.Model.BGServer;
 
-namespace Spinit.Wpc.Synologen.LensSubscription.BGData.Test.Factories
+namespace Synologen.LensSubscription.BGData.Test.Factories
 {
-    public static class ReceivedFileSectionFactory
-    {
-        public static ReceivedFileSection Get()
-        {
-            return new ReceivedFileSection
-                       {
-                           CreatedDate = new DateTime(2011, 02, 02, 15, 30, 0),
-                           HandledDate = new DateTime(2011, 02, 02, 15, 30, 36),
-                           SectionData = new String('A', 100000),
-                           Type = SectionType.ReceivedConsents,
-                           TypeName = "ReceivedConsents"
-                       };
-        }
-    }
+	public static class ReceivedFileSectionFactory
+	{
+		public static ReceivedFileSection Get()
+		{
+			return new ReceivedFileSection
+			{
+				CreatedDate = new DateTime(2011, 02, 02, 15, 30, 0),
+				HandledDate = new DateTime(2011, 02, 02, 15, 30, 36),
+				SectionData = new String('A', 100000),
+				Type = SectionType.ReceivedConsents,
+				TypeName = "ReceivedConsents"
+			};
+		}
+	}
 }

ae915f02878c79942aa4aa1b819b434064fd7b4e
Carl Berg <carl.berg@spinit.se>
2011-02-16 08:18:38 +0000

    Updated service coordinator post build to fetch tasks to bin folders.

--- Build/Post-Build-Synologen.LensSubscription.ServiceCoordinator.bat
+++ Build/Post-Build-Synologen.LensSubscription.ServiceCoordinator.bat
@@ -5,3 +5,5 @@ xcopy "%1..\..\Dependancies\FluentNHibernate\Castle.Core.dll" 					"%1bin\Debug\
 xcopy "%1..\..\Dependancies\FluentNHibernate\NHibernate.ByteCode.Castle.dll" 	"%1bin\Release\" /R /K /Y /Q
 xcopy "%1..\..\Dependancies\FluentNHibernate\Castle.DynamicProxy2.dll" 			"%1bin\Release\" /R /K /Y /Q
 xcopy "%1..\..\Dependancies\FluentNHibernate\Castle.Core.dll" 					"%1bin\Release\" /R /K /Y /Q
+xcopy "%1..\ServiceCoordinator.Task\Output\Synologen.LensSubscription.ServiceCoordinator.Task.*.dll"	"%1bin\Release\" /R /K /Y /Q
+xcopy "%1..\ServiceCoordinator.Task\Output\Synologen.LensSubscription.ServiceCoordinator.Task.*.dll"	"%1bin\Debug\" /R /K /Y /Q

9ca66e9130083ca33e371a67104619edda409793
Carl Berg <carl.berg@spinit.se>
2011-02-17 13:56:24 +0000

    Added BGPaymentToSendRepository tests

--- Synologen.LensSubscription/BGData.Test/BGPaymentToSendRepositoryTester.cs
+++ Synologen.LensSubscription/BGData.Test/BGPaymentToSendRepositoryTester.cs
@@ -7,7 +7,7 @@ using Synologen.LensSubscription.BGData.Test.Factories;
 
 namespace Synologen.LensSubscription.BGData.Test
 {
-	[TestFixture]
+	[TestFixture, Category("PaymentToSendRepositoryTests")]
 	public class When_persisting_a_payment_to_send : BaseRepositoryTester<BGPaymentToSendRepository>
 	{
 		private BGPaymentToSend savedPaymentToSend;
@@ -42,5 +42,63 @@ namespace Synologen.LensSubscription.BGData.Test
 			});
 		}
 
+	}
+
+	[TestFixture, Category("PaymentToSendRepositoryTests")]
+	public class When_updating_a_payment_to_send : BaseRepositoryTester<BGPaymentToSendRepository>
+	{
+		private BGPaymentToSend paymentToSend;
+
+		public When_updating_a_payment_to_send()
+		{
+			Context = session =>
+			{
+				paymentToSend = PaymentToSendFactory.Get();
+				CreateRepository(session).Save(paymentToSend);
+				PaymentToSendFactory.Edit(paymentToSend);
+			};
+
+			Because = repository => repository.Save(paymentToSend);
+		}
+
+		[Test]
+		public void Payment_to_send_was_updated()
+		{
+			AssertUsing(session =>
+			{
+				var fetchedPayment = CreateRepository(session).Get(paymentToSend.Id);
+				fetchedPayment.Amount.ShouldBe(paymentToSend.Amount);
+				fetchedPayment.CustomerNumber.ShouldBe(paymentToSend.CustomerNumber);
+				fetchedPayment.Id.ShouldBe(paymentToSend.Id);
+				fetchedPayment.PaymentDate.ShouldBe(paymentToSend.PaymentDate);
+				fetchedPayment.PeriodCode.ShouldBe(paymentToSend.PeriodCode);
+				fetchedPayment.Reference.ShouldBe(paymentToSend.Reference);
+				fetchedPayment.SendDate.ShouldBe(paymentToSend.SendDate);
+				fetchedPayment.Type.ShouldBe(paymentToSend.Type);
+			});
+		}
+	}
+
+	[TestFixture, Category("PaymentToSendRepositoryTests")]
+	public class When_deleting_a_payment_to_send : BaseRepositoryTester<BGPaymentToSendRepository>
+	{
+		private BGPaymentToSend paymentToSend;
+
+		public When_deleting_a_payment_to_send()
+		{
+			Context = session =>
+			{
+				paymentToSend = PaymentToSendFactory.Get();
+				CreateRepository(session).Save(paymentToSend);
+			};
+
+			Because = repository => repository.Delete(paymentToSend);
+		}
+
+		[Test]
+		public void Payment_was_deleted_from_repository()
+		{
+			GetResult(session => CreateRepository(session).Get(paymentToSend.Id)).ShouldBe(null);
+		}
 	}
 }

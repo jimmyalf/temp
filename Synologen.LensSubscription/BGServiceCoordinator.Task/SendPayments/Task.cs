26095665e5a4920b370f2e345b5d22bd20a99950
Carl Berg <carl.berg@spinit.se>
2011-02-17 09:30:57 +0000

    Added new task (SendPayments)

---
+++ Synologen.LensSubscription/BGServiceCoordinator.Task/SendPayments/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Spinit.Wpc.Synologen.Core.Domain.Model.BGServer;
+using Spinit.Wpc.Synologen.Core.Domain.Services;
+using Spinit.Wpc.Synologen.Core.Domain.Services.Coordinator;
+
+namespace Synologen.LensSubscription.BGServiceCoordinator.Task.SendPayments
+{
+	public class Task : TaskBase
+	{
+		public Task(ILoggingService loggingService) 
+			: base("SendPayments", loggingService, BGTaskSequenceOrder.SendFiles) {}
+
+		public override void Execute() { throw new NotImplementedException(); }
+	}
+}

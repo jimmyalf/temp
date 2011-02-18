d0dff04e1a16dfc9f771635c8b9192bcb7ad122e
Patrik Ivarsson <patrik.ivarsson@spinit.se>
2011-02-18 14:27:05 +0000

    Started on ReceiveErrors BGServer task

---
+++ Synologen.Core/Domain/Model/BGServer/BGReceivedError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Spinit.Wpc.Synologen.Core.Domain.Model.Autogiro.Recieve;
+
+namespace Spinit.Wpc.Synologen.Core.Domain.Model.BGServer
+{
+    public class BGReceivedError : Entity
+    {
+        public int PayerNumber { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public decimal Amount { get; set; }
+        public string Reference { get; set; }
+        public ErrorCommentCode CommentCode { get; set; }
+    }
+}
---
+++ Synologen.LensSubscription/BGData/Repositories/BGRecievedErrorRepository.cs
@@ -0,0 +1,14 @@
+﻿using NHibernate;
+using Spinit.Data.NHibernate;
+using Spinit.Wpc.Synologen.Core.Domain.Model.BGServer;
+using Spinit.Wpc.Synologen.Core.Domain.Persistence.BGServer;
+
+namespace Synologen.LensSubscription.BGData.Repositories
+{
+    public class BGRecievedErrorRepository : NHibernateRepository<BGReceivedError>, IBGReceivedErrorRepository
+    {
+        public BGRecievedErrorRepository(ISession session) : base(session)
+        {
+        }
+    }
+}

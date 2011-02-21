786d00f0c275bb435412129ff788051e491e13ce
Patrik Ivarsson <patrik.ivarsson@spinit.se>
2011-02-21 14:35:38 +0000

    Added AllUnhandledReceivedErrorFileSectionsCriteria + converter Added ReceivedErrorsFactory Changed namesspace to ErrorsTask

--- Synologen.LensSubscription/BGServiceCoordinator.Task/Tasks.Test/Helpers/ReceiveErrorsTaskTestBase.cs
+++ Synologen.LensSubscription/BGServiceCoordinator.Task/Tasks.Test/Helpers/ReceiveErrorsTaskTestBase.cs
@@ -1,4 +1,6 @@
 using FakeItEasy;
+using Spinit.Wpc.Synologen.Core.Domain.Model.Autogiro.CommonTypes;
+using Spinit.Wpc.Synologen.Core.Domain.Model.Autogiro.Recieve;
 using Spinit.Wpc.Synologen.Core.Domain.Persistence.BGServer;
 using Spinit.Wpc.Synologen.Core.Domain.Services.Coordinator;
 
@@ -7,15 +9,21 @@ namespace Synologen.LensSubscription.BGServiceCoordinator.Task.Test.Helpers
 	public abstract class ReceiveErrorsTaskTestBase : TaskTestBase
 	{
 		protected IBGReceivedErrorRepository BGReceivedErrorRepository;
+        protected IAutogiroFileReader<ErrorsFile, Error> ErrorFileReader;
 
 		protected ReceiveErrorsTaskTestBase()
 		{
 			BGReceivedErrorRepository = A.Fake<IBGReceivedErrorRepository>();
+		    ErrorFileReader = A.Fake<IAutogiroFileReader<ErrorsFile, Error>>();
 		}
 
 		protected override ITask GetTask()
 		{
-			return new ReceiveConsents.ReceiveErrors.Task(Log4NetLogger);
+			return new ReceiveErrors.Task(
+                                        Log4NetLogger,
+                                        ReceivedFileRepository,
+                                        BGReceivedErrorRepository,
+                                        ErrorFileReader);
 		}
 	}
 }

65f96768cfe921387030119bbd4d331dbc9396c6
Carl Berg <carl.berg@spinit.se>
2011-02-24 08:22:43 +0000

    Added test classes to expand later.

---
+++ Synologen.LensSubscription/BGServiceCoordinator.Test/FtpServiceTest.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace Synologen.LensSubscription.BGService.Test
+{
+	[TestFixture]
+	public class FtpServiceTest
+	{
+		
+		[Test]
+		public void AddTest()
+		{
+			Assert.Inconclusive("Add tests");
+		}
+	}
+}

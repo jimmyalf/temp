73a064fec1467212ca30ff8ce3bc3b14a1056905
Carl Berg <carl.berg@spinit.se>
2011-02-24 08:22:43 +0000

    Added test classes to expand later.

---
+++ Synologen.LensSubscription/BGServiceCoordinator.Test/FileWriterServiceTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace Synologen.LensSubscription.BGService.Test
+{
+	[TestFixture]
+	public class FileWriterServiceTests
+	{
+		[Test]
+		public void AddTest()
+		{
+			Assert.Inconclusive("Add tests");
+		}
+	}
+}

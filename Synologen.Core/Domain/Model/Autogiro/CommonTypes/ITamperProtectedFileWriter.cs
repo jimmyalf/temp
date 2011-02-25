299e2276054894c5e0ec17a6dfc830af1d39f9dd
Carl Berg <carl.berg@spinit.se>
2011-02-25 09:31:18 +0000

    Added tests for TamperProtectedFileWriter

--- Synologen.Core/Domain/Model/Autogiro/CommonTypes/ITamperProtectedFileWriter.cs
+++ Synologen.Core/Domain/Model/Autogiro/CommonTypes/ITamperProtectedFileWriter.cs
@@ -3,6 +3,5 @@ namespace Spinit.Wpc.Synologen.Core.Domain.Model.Autogiro.CommonTypes
 	public interface ITamperProtectedFileWriter 
 	{
 		string Write(string fileContents);
-		string KeyVerificationToken { get; }
 	}
 }

c59024b3ef60ef097b6d6077f220610297239bd0
Carl Berg <carl.berg@spinit.se>
2011-02-28 09:18:44 +0000

    Updated test class.

--- Synologen.LensSubscription/BGServiceCoordinator.Test/TestCustomFtpService.cs
+++ Synologen.LensSubscription/BGServiceCoordinator.Test/TestCustomFtpService.cs
@@ -1,33 +1,33 @@
 using System;
 using NUnit.Framework;
-using Synologen.LensSubscription.BGServiceCoordinator.App.Services;
+using Spinit.Wpc.Synologen.Core.Domain.Services;
 
 namespace Synologen.LensSubscription.BGService.Test
 {
 	[TestFixture, Explicit("Test is for proof of concept only")]
 	public class TestCustomFtpService
 	{
-		protected CustomFtpService ftpService;
+		protected FtpCommandService FTPCommandService;
 
 		public TestCustomFtpService()
 		{
-			ftpService = new CustomFtpService("ftp.spinit.se");
-			ftpService.OnCommandExecuted += (sender, eventArgs) => Console.WriteLine("> " +eventArgs.Command);
-			ftpService.OnResponseReceived += (sender, eventArgs) => Console.WriteLine(eventArgs.Response);
+			FTPCommandService = new FtpCommandService("ftp.spinit.se");
+			FTPCommandService.OnCommandExecuted += (sender, eventArgs) => Console.WriteLine("> " +eventArgs.Command);
+			FTPCommandService.OnResponseReceived += (sender, eventArgs) => Console.WriteLine(eventArgs.Response);
 		}
 
 		[Test]
 		public void Test_login()
 		{
-			ftpService.Execute("USER spinitupload");
-			ftpService.Execute("PASS bradag");
-			ftpService.Execute("HELP");
+			FTPCommandService.Execute("USER spinitupload");
+			FTPCommandService.Execute("PASS bradag");
+			FTPCommandService.Execute("HELP");
 		}
 
 		[TearDown]
 		public void TearDown()
 		{
-			ftpService.Dispose();
+			FTPCommandService.Dispose();
 		}
 	}
 }

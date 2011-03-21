71bf0d9445dcddf174311dbaa154dceefb878cd8
Carl Berg <carl.berg@spinit.se>
2011-03-21 16:04:04 +0000

    Added Acceptance test project for Synologen.Presentation

---
+++ Synologen.Presentation.AcceptanceTest/LensSubscription/Add_Customer.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using FakeItEasy;
+using NUnit.Framework;
+using Shouldly;
+using Spinit.Wpc.Synologen.Data.Repositories.LensSubscriptionRepositories;
+using Spinit.Wpc.Synologen.Presentation.Site.Logic.EventArguments.LensSubscription;
+using Spinit.Wpc.Synologen.Presentation.Site.Logic.Presenters.LensSubscription;
+using Spinit.Wpc.Synologen.Presentation.Site.Logic.Views.LensSubscription;
+
+namespace Synologen.Presentation.AcceptanceTest.LensSubscription
+{
+	[TestFixture, Category("Feature: Adding customer")]
+	public class When_adding_a_new_customer : PresenterBase
+	{
+		protected CreateCustomerPresenter presenter;
+		private SaveCustomerEventArgs saveEventArgs;
+		protected ICreateCustomerView view;
+		public When_adding_a_new_customer()
+		{
+			Context = () =>
+			{
+				view = A.Fake<ICreateCustomerView>();
+				presenter = new CreateCustomerPresenter(view, customerRepository, shopRepository, countryRepository, synologenMemberService)
+				{
+					HttpContext = httpContext.Object
+				};
+				saveEventArgs = new SaveCustomerEventArgs
+				{
+					AddressLineOne = "AddressLineOne",
+					AddressLineTwo = "AddressLineTwo",
+					City = "City",
+					Email = "email@email.se",
+					FirstName = "FirstName",
+					LastName = "LastName",
+					MobilePhone = "MobilePhone",
+					Notes = "Notes",
+					PersonalIdNumber = "197910071111",
+					Phone = "031-123546",
+					PostalCode = "43632",
+				};
+			};
+			Because = () =>
+			{
+				presenter.View_Submit(this, saveEventArgs);
+			};
+		}
+
+		[Test]
+		public void A_customer_is_stored()
+		{
+			var lastCustomer = new CustomerRepository(GetSession()).GetAll().Last();
+
+			lastCustomer.Address.AddressLineOne.ShouldBe(saveEventArgs.AddressLineOne);
+			lastCustomer.Address.AddressLineTwo.ShouldBe(saveEventArgs.AddressLineTwo);
+			lastCustomer.Address.City.ShouldBe(saveEventArgs.City);
+			lastCustomer.Address.Country.Name.ShouldBe("Sverige");
+			lastCustomer.Address.PostalCode.ShouldBe(saveEventArgs.PostalCode);
+			lastCustomer.Contact.Email.ShouldBe(saveEventArgs.Email);
+			lastCustomer.Contact.MobilePhone.ShouldBe(saveEventArgs.MobilePhone);
+			lastCustomer.Contact.Phone.ShouldBe(saveEventArgs.Phone);
+			lastCustomer.FirstName.ShouldBe(saveEventArgs.FirstName);
+			lastCustomer.LastName.ShouldBe(saveEventArgs.LastName);
+			lastCustomer.Notes.ShouldBe(saveEventArgs.Notes);
+			lastCustomer.PersonalIdNumber.ShouldBe(saveEventArgs.PersonalIdNumber);
+			lastCustomer.Shop.Id.ShouldBe(testShopId);
+			lastCustomer.Subscriptions.Count().ShouldBe(0);
+		}
+
+	}
+}
---
+++ Synologen.Presentation.AcceptanceTest/LensSubscription/Add_Subscription_To_Customer.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Linq;
+using FakeItEasy;
+using NUnit.Framework;
+using Shouldly;
+using Spinit.Wpc.Synologen.Core.Domain.Model.LensSubscription;
+using Spinit.Wpc.Synologen.Data.Repositories.LensSubscriptionRepositories;
+using Spinit.Wpc.Synologen.Presentation.Site.Logic.EventArguments.LensSubscription;
+using Spinit.Wpc.Synologen.Presentation.Site.Logic.Presenters.LensSubscription;
+using Spinit.Wpc.Synologen.Presentation.Site.Logic.Views.LensSubscription;
+using Spinit.Wpc.Synologen.Core.Extensions;
+
+namespace Synologen.Presentation.AcceptanceTest.LensSubscription
+{
+	[TestFixture, Category("Feature: Adding subscription")]
+	public class When_adding_a_subscription_to_customer : PresenterBase
+	{
+		private ICreateLensSubscriptionView view;
+		private CreateLensSubscriptionPresenter presenter;
+		private SaveSubscriptionEventArgs saveSubscriptionEventArgs;
+		private Customer customer;
+		private const int SwedenCountryId = 1;
+
+		public When_adding_a_subscription_to_customer()
+		{
+			Context = () =>
+			{
+				var countryToUse = countryRepository.Get(SwedenCountryId);
+				var shopToUse = shopRepository.Get(testShopId);
+				customer = CreateCustomer(countryToUse, shopToUse);
+				customerRepository.Save(customer);
+				view = A.Fake<ICreateLensSubscriptionView>();
+				httpContext.SetupSingleQuery("customer", customer.Id.ToString());
+				presenter = new CreateLensSubscriptionPresenter(view, customerRepository, subscriptionRepository, synologenMemberService)
+				{
+					HttpContext = httpContext.Object
+				};
+				saveSubscriptionEventArgs = new SaveSubscriptionEventArgs
+				{
+					AccountNumber = "123456",
+                    ClearingNumber = "7894",
+                    MonthlyAmount = "345",
+                    Notes = "Notes..."
+				};
+			};
+			Because = () =>
+			{
+				presenter.View_Submit(this, saveSubscriptionEventArgs);
+			};
+		}
+
+		[Test]
+		public void A_subscription_is_added()
+		{
+			var lastSubscription = new SubscriptionRepository(GetSession()).GetAll().Last();
+			lastSubscription.ActivatedDate.ShouldBe(null);
+			lastSubscription.Active.ShouldBe(false);
+			lastSubscription.BankgiroPayerNumber.ShouldBe(null);
+			lastSubscription.ConsentStatus.ShouldBe(SubscriptionConsentStatus.NotSent);
+			lastSubscription.CreatedDate.Date.ShouldBe(DateTime.Now.Date);
+			lastSubscription.Customer.Id.ShouldBe(customer.Id);
+			lastSubscription.Errors.Count().ShouldBe(0);
+			lastSubscription.Notes.ShouldBe(saveSubscriptionEventArgs.Notes);
+			lastSubscription.PaymentInfo.AccountNumber.ShouldBe(saveSubscriptionEventArgs.AccountNumber);
+			lastSubscription.PaymentInfo.ClearingNumber.ShouldBe(saveSubscriptionEventArgs.ClearingNumber);
+			lastSubscription.PaymentInfo.MonthlyAmount.ShouldBe(saveSubscriptionEventArgs.MonthlyAmount.ToDecimal());
+			lastSubscription.PaymentInfo.PaymentSentDate.ShouldBe(null);
+			lastSubscription.Transactions.Count().ShouldBe(0);
+		}
+	}
+}

510518e4808f41a7803dd5c2d59632da6052772a
Andreas Jilvero <andreas.jilvero@spinit.se>
2011-09-06 07:27:30 +0000

    - More missing files...

---
+++ Synologen.Presentation.Site.Test/YammerTests/Factories/YammerFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace Spinit.Wpc.Synologen.Presentation.Site.Test.YammerTests.Factories
+{
+    public class YammerFactory
+    {
+        public static string GetJson()
+        {
+            var text = File.ReadAllText(@"C:\Develop\WPC\Customer Specific\Synologen\trunk\Synologen.Presentation.Site.Test\YammerTests\Factories\messages.json");
+            return text;
+        }
+    }
+}
---
+++ Synologen.Presentation.Site.Test/YammerTests/TestHelpers/YammerTestbase.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using Spinit.Wpc.Synologen.Business.Domain.Interfaces;
+using Spinit.Wpc.Synologen.Presentation.Site.Logic.Presenters.Yammer;
+using Spinit.Wpc.Synologen.Presentation.Site.Logic.Views.Yammer;
+using Spinit.Wpc.Synologen.Presentation.Site.Models.Yammer;
+using Spinit.Wpc.Synologen.Presentation.Site.Test.TestHelpers;
+
+namespace Spinit.Wpc.Synologen.Presentation.Site.Test.YammerTests.TestHelpers
+{
+    public abstract class YammerTestbase : PresenterTestbase<YammerPresenter, IYammerView, YammerListModel>
+    {
+        protected Mock<IYammerService> MockedService;
+
+        protected YammerTestbase()
+        {
+            SetUp = () =>
+            {
+                MockedService = new Mock<IYammerService>();
+            };
+
+            GetPresenter = () =>
+            {
+                return new YammerPresenter(MockedView.Object, MockedService.Object);
+            };
+        }
+    }
+}

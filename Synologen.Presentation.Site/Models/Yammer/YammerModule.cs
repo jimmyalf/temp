9f29e667d451461ca07ab082421a9cc663480a5e
Andreas Jilvero <andreas.jilvero@spinit.se>
2011-09-08 15:25:40 +0000

    - Adding missing yammer model files

---
+++ Synologen.Presentation.Site/Models/Yammer/YammerModule.cs
@@ -0,0 +1,9 @@
+﻿namespace Spinit.Wpc.Synologen.Presentation.Site.Models.Yammer
+{
+    public class YammerModule
+    {
+        public string Name { get; set; }
+        public string WebUrl { get; set; }
+        public string InlineHtml { get; set; }
+    }
+}

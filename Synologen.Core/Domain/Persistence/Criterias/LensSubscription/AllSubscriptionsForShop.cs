070e9ebb54045c50df4ac3b4d13ccab72004f2e2
Carl Berg <carl.berg@spinit.se>
2011-09-29 07:59:47 +0000

    Updated GetAllSubscriptionsForShop criteria to handle sorting and sorts by descending Id order by default.

--- Synologen.Core/Domain/Persistence/Criterias/LensSubscription/AllSubscriptionsForShop.cs
+++ Synologen.Core/Domain/Persistence/Criterias/LensSubscription/AllSubscriptionsForShop.cs
@@ -5,10 +5,16 @@ namespace Spinit.Wpc.Synologen.Core.Domain.Persistence.Criterias.LensSubscriptio
 	public class AllSubscriptionsForShopCriteria : IActionCriteria
 	{
 		public int ShopId { get; set; }
+		public bool SortAscending { get; set; }
 
 		public AllSubscriptionsForShopCriteria(int shopId)
 		{
 			ShopId = shopId;
+		}
+		public AllSubscriptionsForShopCriteria(int shopId, bool sortAscending)
+		{
+			ShopId = shopId;
+			SortAscending = sortAscending;
 		}
 	}
 }

33ee1cf7008804eab9e9ebb0752602d76e8d697d
Carl Berg <carl.berg@spinit.se>
2011-11-16 13:19:45 +0000

    Updated post build event.

--- Build/Post-Build-Ogonapoteket.bat
+++ Build/Post-Build-Ogonapoteket.bat
@@ -1,11 +1,10 @@
-set rootPath=%~dp0..
-xcopy "%rootPath%\Dependancies\Spinit.Wpc\Spinit.Wpc.Base.Presentation.Site.dll" 	"%rootPath%\Ogonapoteket Site\bin\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\Spinit.Wpc\Spinit.Wpc.Content.Presentation.Site.dll" 	"%rootPath%\Ogonapoteket Site\bin\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\Spinit.Wpc\Spinit.Wpc.Content.Data.dll" 	"%rootPath%\Ogonapoteket Site\bin\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\Spinit.Wpc\Spinit.Wpc.Content.Business.dll" 	"%rootPath%\Ogonapoteket Site\bin\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\Spinit.Wpc\Spinit.Wpc.Utility.Data.dll" 	"%rootPath%\Ogonapoteket Site\bin\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\Spinit.Wpc\Spinit.Wpc.Base.Data.dll" 	"%rootPath%\Ogonapoteket Site\bin\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\Spinit.Wpc\Spinit.Wpc.Core.UI.Mvp.Site.dll" 	"%rootPath%\Ogonapoteket Site\bin\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\Spinit.Wpc\Spinit.Wpc.QuickMail.Core.dll" 	"%rootPath%\Ogonapoteket Site\bin\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\Structuremap\Structuremap.dll" 	"%rootPath%\Ogonapoteket Site\bin\" /R /K /Y /Q
-xcopy "%rootPath%\Dependancies\WebFormsMvp\WebFormsMvp.dll" 	"%rootPath%\Ogonapoteket Site\bin\" /R /K /Y /Q
+xcopy "%1..\Dependancies\Spinit.Wpc\Spinit.Wpc.Base.Presentation.Site.dll" 			"%1\bin\" /R /K /Y /Q
+xcopy "%1..\Dependancies\Spinit.Wpc\Spinit.Wpc.Content.Presentation.Site.dll" 		"%1\bin\" /R /K /Y /Q
+xcopy "%1..\Dependancies\Spinit.Wpc\Spinit.Wpc.Content.Data.dll" 					"%1\bin\" /R /K /Y /Q
+xcopy "%1..\Dependancies\Spinit.Wpc\Spinit.Wpc.Content.Business.dll" 				"%1\bin\" /R /K /Y /Q
+xcopy "%1..\Dependancies\Spinit.Wpc\Spinit.Wpc.Utility.Data.dll" 					"%1\bin\" /R /K /Y /Q
+xcopy "%1..\Dependancies\Spinit.Wpc\Spinit.Wpc.Base.Data.dll" 						"%1\bin\" /R /K /Y /Q
+xcopy "%1..\Dependancies\Spinit.Wpc\Spinit.Wpc.Core.UI.Mvp.Site.dll" 				"%1\bin\" /R /K /Y /Q
+xcopy "%1..\Dependancies\Spinit.Wpc\Spinit.Wpc.QuickMail.Core.dll" 					"%1\bin\" /R /K /Y /Q
+xcopy "%1..\Dependancies\Structuremap\Structuremap.dll" 							"%1\bin\" /R /K /Y /Q
+xcopy "%1..\Dependancies\WebFormsMvp\WebFormsMvp.dll" 								"%1\bin\" /R /K /Y /Q

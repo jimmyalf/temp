1a0cd1325befe7a101037352205850d46f838dce
Carl Berg <carl.berg@spinit.se>
2011-11-16 14:02:59 +0000

    Updated visma project (moved).

--- Build/Post-Build-Spinit.Wpc.Synologen.Visma.bat
+++ Build/Post-Build-Spinit.Wpc.Synologen.Visma.bat
@@ -1,3 +1,2 @@
-
-xcopy "%1\Dependancies\Spinit ADK\Adk.dll" 	"%1\bin\Debug\" /R /K /Y /Q
-xcopy "%1\Dependancies\Spinit ADK\Adk.dll" 	"%1\bin\Release\" /R /K /Y /Q
+xcopy "%1..\..\Dependancies\Spinit ADK\Adk.dll" 	"%1bin\Debug\" /R /K /Y /Q
+xcopy "%1..\..\Dependancies\Spinit ADK\Adk.dll" 	"%1bin\Release\" /R /K /Y /Q

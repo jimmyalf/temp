90bdd17735c9b9079a3a09b1a418146570f0ea54
Carl Berg <carl.berg@spinit.se>
2011-11-17 16:43:50 +0000

    Added Site project.

---
+++ Synologen.Presentation.Site/App/Factory.cs
@@ -0,0 +1,13 @@
+﻿using Spinit.Wpc.Synologen.Business.Domain.Interfaces;
+using Spinit.Wpc.Synologen.Data;
+
+namespace Spinit.Wpc.Synologen.Presentation.Site.App
+{
+	public static class Factory
+	{
+		 public static ISqlProvider GetSqlProvider()
+		 {
+		 	return new SqlProvider(Base.Business.Globals.ConnectionString);
+		 }
+	}
+}

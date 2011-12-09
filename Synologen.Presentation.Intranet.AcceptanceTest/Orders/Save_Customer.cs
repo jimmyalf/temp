ccb26aa4af664997905442a85723b8cf37556f30
Carl Berg <carl.berg@spinit.se>
2011-12-09 09:38:10 +0000

    Updated presenter tests to expect customer id to be part of redirect url to next step

--- Synologen.Presentation.Intranet.AcceptanceTest/Orders/Save_Customer.cs
+++ Synologen.Presentation.Intranet.AcceptanceTest/Orders/Save_Customer.cs
@@ -18,8 +18,9 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.AcceptanceTest.Orders
         private SaveCustomerPresenter _saveCustomerPresenter;
     	private SaveCustomerEventArgs _form;
     	private string _testRedirectUrl;
-    	private OrderCustomer _previousCustomer;
+    	private OrderCustomer _customer;
     	private string _customerNotFoundWithPersonalIdNumber;
+    	private Func<int,string> _expectedRedirectUrl;
 
     	public When_picking_a_customer()
 		{
@@ -28,6 +29,7 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.AcceptanceTest.Orders
 			{
 				_testRedirectUrl = "/test/page";
 				View.NextPageId = 55;
+				_expectedRedirectUrl = createdCustomerId => String.Format("{0}?customer={1}", _testRedirectUrl, createdCustomerId);
 				A.CallTo(() => SynologenMemberService.GetPageUrl(View.NextPageId)).Returns(_testRedirectUrl);
 				_saveCustomerPresenter = GetPresenter();
 			};
@@ -51,18 +53,18 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.AcceptanceTest.Orders
 
     	private void KundinformationSparas()
     	{
-    		var customer = WithRepository<IOrderCustomerRepository>().GetAll().First();
-    		customer.AddressLineOne.ShouldBe(_form.AddressLineOne);
-			customer.AddressLineTwo.ShouldBe(_form.AddressLineTwo);
-			customer.City.ShouldBe(_form.City);
-			customer.Email.ShouldBe(_form.Email);
-			customer.FirstName.ShouldBe(_form.FirstName);
-			customer.LastName.ShouldBe(_form.LastName);
-			customer.MobilePhone.ShouldBe(_form.MobilePhone);
-			customer.Notes.ShouldBe(_form.Notes);
-			customer.PersonalIdNumber.ShouldBe(_form.PersonalIdNumber);
-			customer.Phone.ShouldBe(_form.Phone);
-			customer.PostalCode.ShouldBe(_form.PostalCode);
+    		_customer = WithRepository<IOrderCustomerRepository>().GetAll().First();
+    		_customer.AddressLineOne.ShouldBe(_form.AddressLineOne);
+			_customer.AddressLineTwo.ShouldBe(_form.AddressLineTwo);
+			_customer.City.ShouldBe(_form.City);
+			_customer.Email.ShouldBe(_form.Email);
+			_customer.FirstName.ShouldBe(_form.FirstName);
+			_customer.LastName.ShouldBe(_form.LastName);
+			_customer.MobilePhone.ShouldBe(_form.MobilePhone);
+			_customer.Notes.ShouldBe(_form.Notes);
+			_customer.PersonalIdNumber.ShouldBe(_form.PersonalIdNumber);
+			_customer.Phone.ShouldBe(_form.Phone);
+			_customer.PostalCode.ShouldBe(_form.PostalCode);
     	}
 
     	[Test]
@@ -110,9 +112,9 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.AcceptanceTest.Orders
 
     	private void AttEnKundHittatsIFöregåendeSteg()
     	{
-    		_previousCustomer = OrderFactory.GetCustomer();
-			WithRepository<IOrderCustomerRepository>().Save(_previousCustomer);
-    		HttpContext.SetupRequestParameter("customer", _previousCustomer.Id.ToString());
+    		_customer = OrderFactory.GetCustomer();
+			WithRepository<IOrderCustomerRepository>().Save(_customer);
+    		HttpContext.SetupRequestParameter("customer", _customer.Id.ToString());
     	}
 
         [Test]
@@ -153,12 +155,12 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.AcceptanceTest.Orders
 
     	private void AnvändarenUppdateratFormuläret()
     	{
-    		_form = OrderFactory.GetOrderCustomerForm(_previousCustomer.Id);
+    		_form = OrderFactory.GetOrderCustomerForm(_customer.Id);
     	}
 
     	private void UppdaterasBefintligKund()
     	{
-    		var customer = WithRepository<IOrderCustomerRepository>().Get(_previousCustomer.Id);
+    		var customer = WithRepository<IOrderCustomerRepository>().Get(_customer.Id);
     		customer.AddressLineOne.ShouldBe(_form.AddressLineOne);
 			customer.AddressLineTwo.ShouldBe(_form.AddressLineTwo);
 			customer.City.ShouldBe(_form.City);
@@ -174,23 +176,24 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.AcceptanceTest.Orders
 
     	private void FyllsFormuläretMedKunduppgifter()
     	{
-    		View.Model.FirstName.ShouldBe(_previousCustomer.FirstName);
-			View.Model.LastName.ShouldBe(_previousCustomer.LastName);
-			View.Model.PersonalIdNumber.ShouldBe(_previousCustomer.PersonalIdNumber);
-			View.Model.Email.ShouldBe(_previousCustomer.Email);
-			View.Model.MobilePhone.ShouldBe(_previousCustomer.MobilePhone);
-			View.Model.Phone.ShouldBe(_previousCustomer.Phone);
-			View.Model.AddressLineOne.ShouldBe(_previousCustomer.AddressLineOne);
-			View.Model.AddressLineTwo.ShouldBe(_previousCustomer.AddressLineTwo);
-			View.Model.City.ShouldBe(_previousCustomer.City);
-			View.Model.PostalCode.ShouldBe(_previousCustomer.PostalCode);
-			View.Model.Notes.ShouldBe(_previousCustomer.Notes);
+    		View.Model.FirstName.ShouldBe(_customer.FirstName);
+			View.Model.LastName.ShouldBe(_customer.LastName);
+			View.Model.PersonalIdNumber.ShouldBe(_customer.PersonalIdNumber);
+			View.Model.Email.ShouldBe(_customer.Email);
+			View.Model.MobilePhone.ShouldBe(_customer.MobilePhone);
+			View.Model.Phone.ShouldBe(_customer.Phone);
+			View.Model.AddressLineOne.ShouldBe(_customer.AddressLineOne);
+			View.Model.AddressLineTwo.ShouldBe(_customer.AddressLineTwo);
+			View.Model.City.ShouldBe(_customer.City);
+			View.Model.PostalCode.ShouldBe(_customer.PostalCode);
+			View.Model.Notes.ShouldBe(_customer.Notes);
     	}
 
         private void FörflyttasAnvändarenTillVynFörNästaSteg()
         {
             //Assert redirect
-			HttpContext.ResponseInstance.RedirectedUrl.ShouldBe(_testRedirectUrl);
+        	var url= _expectedRedirectUrl(_customer.Id);
+			HttpContext.ResponseInstance.RedirectedUrl.ShouldBe(url);
         }
 
         private void AnvändarenFörsökerFortsättaTillNästaSteg()

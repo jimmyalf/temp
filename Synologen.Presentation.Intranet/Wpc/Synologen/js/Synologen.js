bd785c283ac2a4757af026bb7deea2d25e269f43
Carl Berg <carl.berg@spinit.se>
2012-01-03 14:32:34 +0000

    Added jquery script to move custom number of transaction input to inside the proper list item.

--- Synologen.Presentation.Intranet/Wpc/Synologen/js/Synologen.js
+++ Synologen.Presentation.Intranet/Wpc/Synologen/js/Synologen.js
@@ -2,27 +2,34 @@
 	$.SynologenIntranet = $.SynologenIntranet || {};
 
 	$.extend($.SynologenIntranet, {
-		init: function() {
+		init: function () {
 			$("html").addClass("js-enabled");
 			$.SynologenIntranet.initYammer();
 			$.SynologenIntranet.initProgressBar(6);
+			$.SynologenIntranet.initAutogiroDetailView();
 		},
 
-		initYammer: function() {
+		initYammer: function () {
 			$(".fancybox").fancybox();
 		},
 
-		initProgressBar: function(numberOfSteps) {
+		initProgressBar: function (numberOfSteps) {
 			function getPercentage(currentStep) {
 				if (currentStep == numberOfSteps) return 100;
 				return (100 / numberOfSteps) * currentStep;
 			}
-			
+
 			for (var i = 1; i <= numberOfSteps; i++) {
 				$(".step" + i + " #progressbar").progressbar({
 					value: getPercentage(i)
 				});
 			}
+		},
+
+		initAutogiroDetailView: function () {
+			var input = $("input.custom-number-of-withdrawals");
+			var span = $("li .custom-number-of-withdrawals");
+			span.append(input);
 		}
 	});
 

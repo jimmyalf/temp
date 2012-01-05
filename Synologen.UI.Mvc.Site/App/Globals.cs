04a4beee3cfa1696866b787214340865904a3bba
Andreas Jilvero <andreas.jilvero@spinit.se>
2012-01-05 09:53:10 +0000

    - Completed GeocodingService

---
+++ Synologen.UI.Mvc.Site/App/Globals.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Configuration;
+
+namespace Spinit.Wpc.Synologen.UI.Mvc.Site.App
+{
+    public class Globals
+    {
+        private const string Section = "synologen";
+
+        public static string GoogleGeocode
+        {
+            get
+            {
+                var section = (AppSettingsSection)ConfigurationManager.GetSection(Section);
+                var setting = section.Settings["GoogleGeocode"];
+                if (setting != null)
+                {
+                    return setting.Value;
+                }
+                throw new ArgumentNullException("Parameter GoogleGeocode could not be found");
+            }
+        }
+    }
+}

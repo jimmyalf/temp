c68a5979de2e7dd5dd1054d284dab533420e5bc6
Andreas Jilvero <andreas.jilvero@spinit.se>
2012-01-05 10:03:25 +0000

    - Fixed config read

--- Synologen.UI.Mvc.Site/App/Globals.cs
+++ Synologen.UI.Mvc.Site/App/Globals.cs
@@ -1,24 +1,14 @@
-﻿using System;
-using System.Configuration;
+﻿using Spinit.Wpc.Utility.Business;
 
 namespace Spinit.Wpc.Synologen.UI.Mvc.Site.App
 {
-    public class Globals
+    public class Globals : ConfigurationFetch
     {
         private const string Section = "synologen";
 
         public static string GoogleGeocode
         {
-            get
-            {
-                var section = (AppSettingsSection)ConfigurationManager.GetSection(Section);
-                var setting = section.Settings["GoogleGeocode"];
-                if (setting != null)
-                {
-                    return setting.Value;
-                }
-                throw new ArgumentNullException("Parameter GoogleGeocode could not be found");
-            }
+            get { return SafeConfigString(Section, "GoogleGeocode", string.Empty); }
         }
     }
 }

abda2746c356a9cec992d4fadad2ae9ffee74269
Andreas Jilvero <andreas.jilvero@spinit.se>
2012-01-05 09:53:10 +0000

    - Completed GeocodingService

---
+++ Synologen.UI.Mvc.Site/App/Services/GeocodingService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+using Spinit.Wpc.Synologen.Core.Domain.Model.FrameOrder;
+using Spinit.Wpc.Synologen.Core.Domain.Model.ShopDetails;
+using Spinit.Wpc.Synologen.Core.Domain.Services;
+
+namespace Spinit.Wpc.Synologen.UI.Mvc.Site.App.Services
+{
+    public class GeocodingService : IGeocodingService
+    {
+        private class Json
+        {
+            [JsonProperty("results")]
+            public Result[] Results { get; set; }
+        }
+
+        private class Result
+        {
+            [JsonProperty("geometry")]
+            public Geometry Geometry { get; set; }
+        }
+
+        private class Geometry
+        {
+            [JsonProperty("location")]
+            public Location Location { get; set; }
+        }
+
+        private class Location
+        {
+            [JsonProperty("lat")]
+            public decimal Latitude;
+            [JsonProperty("lng")]
+            public decimal Longitude;
+        }
+
+        public Coordinates GetCoordinates(string address)
+        {
+            // TODO: Hämta data från config
+            var url = String.Format(Globals.GoogleGeocode, address);
+            var json = new WebClient().DownloadString(url);
+
+            var results = JsonConvert.DeserializeObject<Json>(json);
+            if (results != null && results.Results != null && results.Results.Length > 0)
+            {
+                var lat = results.Results[0].Geometry.Location.Latitude;
+                var lng = results.Results[0].Geometry.Location.Longitude;
+                return new Coordinates {Latitude = lat, Longitude = lng};
+            }
+            throw new ArgumentException(String.Format("Could not find location for given adress: {0}", address));
+        }
+    }
+}
---
+++ Synologen.UI.Mvc.Site/App/ViewModelParsers/ShopViewModelParserService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Spinit.Wpc.Synologen.Core.Domain.Model.ShopDetails;
+using Spinit.Wpc.Synologen.UI.Mvc.Site.Models;
+
+namespace Spinit.Wpc.Synologen.UI.Mvc.Site.App.ViewModelParsers
+{
+    public class ShopViewModelParserService
+    {
+        public IEnumerable<ShopListItem> ParseShops(IEnumerable<Shop> shops)
+        {
+            return shops.Select(ParseShop);
+        }
+
+        private static ShopListItem ParseShop(Shop shop)
+        {
+            return new ShopListItem
+            {
+                Description = shop.Description,
+                Email = shop.Email,
+                HomePage = shop.Url,
+                Id = shop.Id,
+                Latitude = shop.Coordinates.Latitude,
+                Longitude = shop.Coordinates.Longitude,
+                Map = shop.MapUrl,
+                Name = shop.Name,
+                StreetAddress = String.Format("{0} {1}", shop.Address.AddressLineOne, shop.Address.AddressLineTwo),
+                Telephone = shop.Phone
+            };
+        }
+    }
+}

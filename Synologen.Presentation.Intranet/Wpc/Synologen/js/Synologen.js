34e0ddfa1cb9804a69c1544a11fc40717d97e317
Erik Kinding <erik.kinding@spinit.se>
2012-01-09 14:20:17 +0000

    tab links disabled for now..

--- Synologen.Presentation.Intranet/Wpc/Synologen/js/Synologen.js
+++ Synologen.Presentation.Intranet/Wpc/Synologen/js/Synologen.js
@@ -1,46 +1,52 @@
 (function ($) {
-	$.SynologenIntranet = $.SynologenIntranet || {};
+    $.SynologenIntranet = $.SynologenIntranet || {};
 
-	$.extend($.SynologenIntranet, {
-		init: function () {
-			$("html").addClass("js-enabled");
-			$.SynologenIntranet.initYammer();
-			$.SynologenIntranet.initProgressBar(6);
-			$.SynologenIntranet.initAutogiroDetailView();
-			$.SynologenIntranet.initRadioButtonLists();
-		},
+    $.extend($.SynologenIntranet, {
+        init: function () {
+            $("html").addClass("js-enabled");
+            $.SynologenIntranet.initYammer();
+            $.SynologenIntranet.initProgressBar(6);
+            $.SynologenIntranet.initAutogiroDetailView();
+            $.SynologenIntranet.initRadioButtonLists();
+            $.SynologenIntranet.initDisableTabLinks();
+        },
 
-		initYammer: function () {
-			$(".fancybox").fancybox();
-		},
+        initDisableTabLinks: function () {
+            $('#tab-container a').attr('disabled', 'disabled');
+            //the underline hover thing is set to 'none' in the css...
+        },
 
-		initProgressBar: function (numberOfSteps) {
-			function getPercentage(currentStep) {
-				if (currentStep == numberOfSteps) return 100;
-				return (100 / numberOfSteps) * currentStep;
-			}
+        initYammer: function () {
+            //  $(".fancybox").fancybox();
+        },
 
-			for (var i = 1; i <= numberOfSteps; i++) {
-				$(".step" + i + " #progressbar").progressbar({
-					value: getPercentage(i)
-				});
-			}
-		},
+        initProgressBar: function (numberOfSteps) {
+            function getPercentage(currentStep) {
+                if (currentStep == numberOfSteps) return 100;
+                return (100 / numberOfSteps) * currentStep;
+            }
 
-		initAutogiroDetailView: function () {
-			var input = $("input.custom-number-of-withdrawals");
-			var span = $("li .custom-number-of-withdrawals");
-			span.append(input);
-		},
+            for (var i = 1; i <= numberOfSteps; i++) {
+                $(".step" + i + " #progressbar").progressbar({
+                    value: getPercentage(i)
+                });
+            }
+        },
 
-		initRadioButtonLists: function () {
-			$("ul.radioButtonItems").addClass("radio-list");
-			$(".radio-list li label").each(function () {
-				var text = $(this).text();
-				$(this).replaceWith(text);
-			});
-		}
-	});
+        initAutogiroDetailView: function () {
+            var input = $("input.custom-number-of-withdrawals");
+            var span = $("li .custom-number-of-withdrawals");
+            span.append(input);
+        },
+
+        initRadioButtonLists: function () {
+            $("ul.radioButtonItems").addClass("radio-list");
+            $(".radio-list li label").each(function () {
+                var text = $(this).text();
+                $(this).replaceWith(text);
+            });
+        }
+    });
 
 })(jQuery);
 

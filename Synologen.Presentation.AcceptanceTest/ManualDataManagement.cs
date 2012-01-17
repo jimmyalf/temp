d0efbce7e998141dac092ed3d9e1cc207ef107e4
Carl Berg <carl.berg@spinit.se>
2012-01-17 07:51:05 +0000

    Updated Synologen Client Installation project to use proper .Net framework prerequisites. Updated Order to use SystemTime for Created datetime. Updated PageOfOrdersMatchingCriteriaConverter to also filter on shop name.

--- Synologen.Presentation.AcceptanceTest/ManualDataManagement.cs
+++ Synologen.Presentation.AcceptanceTest/ManualDataManagement.cs
@@ -6,6 +6,7 @@ using NHibernate;
 using NUnit.Framework;
 using Spinit.Wpc.Synogen.Test.Data;
 using Spinit.Wpc.Synologen.Core.Domain.Model.Orders;
+using Spinit.Wpc.Synologen.Core.Domain.Services;
 using Spinit.Wpc.Synologen.Presentation.AcceptanceTest.Helpers;
 using StructureMap;
 
@@ -19,7 +20,7 @@ namespace Spinit.Wpc.Synologen.Presentation.AcceptanceTest
 		private OrderCustomer _customerOne;
 		private OrderCustomer _customerTwo;
 		private DataManager _dataManager;
-		private Shop _shop;
+		private Shop _shop1, _shop2;
 
 		[SetUp]
 		public void Context()
@@ -30,17 +31,20 @@ namespace Spinit.Wpc.Synologen.Presentation.AcceptanceTest
 			var category = OrderFactory.GetCategory().StoreWith(_session);
 			var articleType = OrderFactory.GetArticleType(category).StoreWith(_session);
 			var supplier = OrderFactory.GetSupplier().StoreWith(_session);
-			_shop = CreateShop(_session);
+			_shop1 = CreateShop(_session, "Testbutik A");
+			_shop2 = CreateShop(_session, "Testbutik B");
 			_article = OrderFactory.GetArticle(articleType, supplier).StoreWith(_session);
-			_customerOne = OrderFactory.GetCustomer(_shop).StoreWith(_session);
-			_customerTwo = OrderFactory.GetCustomer(_shop, firstName: "Börje", lastName: "Svensson").StoreWith(_session);
+			_customerOne = OrderFactory.GetCustomer(_shop1).StoreWith(_session);
+			_customerTwo = OrderFactory.GetCustomer(_shop2, firstName: "Börje", lastName: "Svensson").StoreWith(_session);
 		}
 
 		[Test]
 		public void Add_orders()
 		{
-			OrderFactory.GetOrders(_shop, _article, _customerOne).StoreItemsWith(_session);
-			OrderFactory.GetOrders(_shop, _article, _customerTwo).StoreItemsWith(_session);
+			SystemTime.InvokeWhileTimeIs(new DateTime(2012,01,10),() => 
+				OrderFactory.GetOrders(_shop1, _article, _customerOne).StoreItemsWith(_session));
+			SystemTime.InvokeWhileTimeIs(new DateTime(2012, 01, 15), () =>
+				OrderFactory.GetOrders(_shop2, _article, _customerTwo).StoreItemsWith(_session));
 		}
 
 		protected Shop CreateShop(ISession session, string shopName = "Testbutik")

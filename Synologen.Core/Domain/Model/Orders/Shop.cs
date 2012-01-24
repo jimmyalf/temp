d87eda6c555b249a912a8a3c00e8dd265f1eefe8
Erik Kinding <erik.kinding@spinit.se>
2012-01-24 15:30:17 +0000

    Email template for order email to suppliers.

--- Synologen.Core/Domain/Model/Orders/Shop.cs
+++ Synologen.Core/Domain/Model/Orders/Shop.cs
@@ -2,6 +2,10 @@ namespace Spinit.Wpc.Synologen.Core.Domain.Model.Orders
 {
 	public class Shop : Entity
 	{
-		public virtual string Name { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string AddressLineOne { get; set; }
+        public virtual string AddressLineTwo { get; set; }
+        public virtual string PostalCode { get; set; }
+        public virtual string City { get; set; }
 	}
 }

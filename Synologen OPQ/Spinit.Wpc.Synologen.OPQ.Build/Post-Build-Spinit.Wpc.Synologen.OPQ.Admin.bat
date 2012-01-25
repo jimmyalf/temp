1eec5978e3e06306ada56510fcc8c02186011a29
Andreas Jilvero <andreas.jilvero@spinit.se>
2012-01-25 14:59:24 +0000

    - (Hopefully) fixed build script

--- Synologen OPQ/Spinit.Wpc.Synologen.OPQ.Build/Post-Build-Spinit.Wpc.Synologen.OPQ.Admin.bat
+++ Synologen OPQ/Spinit.Wpc.Synologen.OPQ.Build/Post-Build-Spinit.Wpc.Synologen.OPQ.Admin.bat
@@ -1,5 +1,5 @@
-xcopy %1..\..\Dependancies\FluentNHibernate\Castle.DynamicProxy2.dll		 		%1\bin /R /K /Y /Q
-xcopy %1..\..\Dependancies\FluentNHibernate\Castle.Core.dll		 			%1\bin /R /K /Y /Q
-xcopy %1..\..\Dependancies\FluentNHibernate\NHibernate.ByteCode.Castle.dll			%1\bin /R /K /Y /Q
-xcopy %1..\..\Dependancies\Spinit.Wpc\Spinit.Wpc.Core.Dependencies.AutoMapper.dll		%1\bin /R /K /Y /Q
-xcopy %1..\..\Dependancies\Spinit.Wpc\Spinit.Wpc.Core.Dependencies.NHibernate.dll		%1\bin /R /K /Y /Q
+xcopy "%1..\..\Dependancies\FluentNHibernate\Castle.DynamicProxy2.dll"		 		"%1\bin" /R /K /Y /Q
+xcopy "%1..\..\Dependancies\FluentNHibernate\Castle.Core.dll"		 			"%1\bin" /R /K /Y /Q
+xcopy "%1..\..\Dependancies\FluentNHibernate\NHibernate.ByteCode.Castle.dll"			"%1\bin" /R /K /Y /Q
+xcopy "%1..\..\Dependancies\Spinit.Wpc\Spinit.Wpc.Core.Dependencies.AutoMapper.dll"		"%1\bin" /R /K /Y /Q
+xcopy "%1..\..\Dependancies\Spinit.Wpc\Spinit.Wpc.Core.Dependencies.NHibernate.dll"		"%1\bin" /R /K /Y /Q

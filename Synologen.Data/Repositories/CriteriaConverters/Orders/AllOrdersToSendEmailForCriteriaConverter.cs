fa3612fd710f4870033791499a039fc11acdaa60
Carl Berg <carl.berg@spinit.se>
2012-02-14 13:40:05 +0000

    Updated AllOrdersToSendEmailForCriteriaConverter to join tables being used in execution to avoid unnecessary select N+1 problem.

--- Synologen.Data/Repositories/CriteriaConverters/Orders/AllOrdersToSendEmailForCriteriaConverter.cs
+++ Synologen.Data/Repositories/CriteriaConverters/Orders/AllOrdersToSendEmailForCriteriaConverter.cs
@@ -1,5 +1,6 @@
 ﻿using NHibernate;
 using NHibernate.Criterion;
+using NHibernate.Transform;
 using Spinit.Data.NHibernate;
 using Spinit.Wpc.Synologen.Core.Domain.Model.Orders;
 using Spinit.Wpc.Synologen.Core.Domain.Persistence.Criterias.Orders;
@@ -16,7 +17,13 @@ namespace Spinit.Wpc.Synologen.Data.Repositories.CriteriaConverters.Orders
 				Restrictions.Eq(Property(x => x.ShippingType), OrderShippingOption.ToCustomer) || 
 				Restrictions.Eq(Property(x => x.ShippingType), OrderShippingOption.ToStore);
 			var isNotAlreadySent = Restrictions.IsNull(Property(x => x.SpinitServicesEmailId));
-			return Criteria.Add(Restrictions.And(isRightShippingType, isNotAlreadySent));
+			return Criteria.Add(Restrictions.And(isRightShippingType, isNotAlreadySent))
+			.SetFetchMode(Property(x => x.Shop), FetchMode.Join)
+			.SetFetchMode(Property(x => x.LensRecipe), FetchMode.Join)
+			.SetFetchMode(Property(x => x.Article), FetchMode.Join)
+			.SetFetchMode(Property(x => x.Article.ArticleSupplier), FetchMode.Join)
+			.SetFetchMode(Property(x => x.Customer), FetchMode.Join)
+			.SetResultTransformer(new DistinctRootEntityResultTransformer());
 		}
 	}
 }

e4b45b00d408d12bf3499442b57f42a8d12de4fd
Carl Berg <carl.berg@spinit.se>
2012-02-14 13:18:36 +0000

    Moved Synologen.OrderEmailSender into Service namespace. Added logging and removed some tightly coupled classes to enable future testability. Added criteria to fetch only orders to send email for (instead of fetching all orders).

---
+++ Synologen.Data/Repositories/CriteriaConverters/Orders/AllOrdersToSendEmailForCriteriaConverter.cs
@@ -0,0 +1,22 @@
+﻿using NHibernate;
+using NHibernate.Criterion;
+using Spinit.Data.NHibernate;
+using Spinit.Wpc.Synologen.Core.Domain.Model.Orders;
+using Spinit.Wpc.Synologen.Core.Domain.Persistence.Criterias.Orders;
+using Order = Spinit.Wpc.Synologen.Core.Domain.Model.Orders.Order;
+
+namespace Spinit.Wpc.Synologen.Data.Repositories.CriteriaConverters.Orders
+{
+	public class AllOrdersToSendEmailForCriteriaConverter : NHibernateActionCriteriaConverter<AllOrdersToSendEmailForCriteria,Order>
+	{
+		public AllOrdersToSendEmailForCriteriaConverter(ISession session) : base(session) {}
+		public override ICriteria Convert(AllOrdersToSendEmailForCriteria source)
+		{
+			var isRightShippingType = 
+				Restrictions.Eq(Property(x => x.ShippingType), OrderShippingOption.ToCustomer) || 
+				Restrictions.Eq(Property(x => x.ShippingType), OrderShippingOption.ToStore);
+			var isNotAlreadySent = Restrictions.IsNull(Property(x => x.SpinitServicesEmailId));
+			return Criteria.Add(Restrictions.And(isRightShippingType, isNotAlreadySent));
+		}
+	}
+}
---
+++ Synologen.Service/Client.OrderEmailSender/Services/SendOrderService.cs
@@ -0,0 +1,148 @@
+using Spinit.Extensions;
+using Spinit.Services.Client;
+using Spinit.Wpc.Synologen.Core.Domain.Model.Orders;
+using Spinit.Wpc.Synologen.Core.Domain.Services;
+using Synologen.Service.Client.OrderEmailSender.Application.Extensions;
+
+namespace Synologen.Service.Client.OrderEmailSender.Services
+{
+    public class SendOrderService : ISendOrderService
+    {
+        private readonly EmailClient2 _client;
+    	private readonly IConfigurationSettings _configurationSettings;
+
+    	public SendOrderService(EmailClient2 client, IConfigurationSettings configurationSettings)
+        {
+        	_client = client;
+        	_configurationSettings = configurationSettings;
+        	//var user = ConfigurationSettings.AppSettings["SpinitSendUser"];
+        	//var password = ConfigurationSettings.AppSettings["Password"];
+        	//var passwordEncoding = ConfigurationSettings.AppSettings["PasswordEncoding"];
+        	//var server = ConfigurationSettings.AppSettings["ServerAddress"];
+
+        	//ClientFactory.SetConfigurtion(ClientFactory.CreateConfiguration(
+        	//        server,
+        	//        user,
+        	//        password,
+        	//        PasswordEncryptionType.Sha1,
+        	//        passwordEncoding
+        	//        )
+        	//);
+        	//_client = ClientFactory.CreateEmail2Client();
+        }
+
+    	public int SendOrderByEmail(Order order)
+        {
+            return ParseAndSend(order);
+        }
+
+        private int ParseAndSend(Order order)
+        {
+            
+            var to = order.Article.ArticleSupplier.OrderEmailAddress;
+            //const string from = "order@synologen.nu";
+        	var from = _configurationSettings.OrderSenderEmailAddress;
+            var body = GetEmailBody(order);
+        	var subject = _configurationSettings.OrderEmailSubjectFormat.ReplaceWith(new
+        	{
+        		OrderArticleName = order.Article.Name
+        	});
+            //var subject = "Beställning, " + order.Article.Name;
+
+            return _client.SendMail(to, from, subject, body, EmailPriority.Medium);
+        }
+
+        private static string GetEmailBody(Order order)
+        {
+            var receiver = order.ShippingType.Equals(OrderShippingOption.ToCustomer) 
+				? order.Customer.ParseName(x => x.FirstName, x => x.LastName)
+				: order.Shop.Name;
+            var addressLineOne = order.ShippingType.Equals(OrderShippingOption.ToCustomer)
+                ? order.Customer.AddressLineOne
+                : order.Shop.AddressLineOne;
+			var addressLineTwo = order.ShippingType.Equals(OrderShippingOption.ToCustomer)
+				? order.Customer.AddressLineTwo
+				: order.Shop.AddressLineTwo;
+            var city = order.ShippingType.Equals(OrderShippingOption.ToCustomer)
+                ? order.Customer.City
+                : order.Shop.City;
+            var postalCode = order.ShippingType.Equals(OrderShippingOption.ToCustomer)
+                ? order.Customer.PostalCode
+                : order.Shop.PostalCode;
+
+
+            var data = new 
+            {
+                OrderId = order.Id,
+                Article = order.Article.Name,
+                ShopName = order.Shop.Name,
+
+                Receiver = receiver,
+                AddressLineOne = addressLineOne,
+                AddressLineTwo = addressLineTwo,
+                City = city,
+                PostalCode = postalCode,
+                               
+                LeftAddition = order.LensRecipe.Addition.Return(x => x.Left.GetStringValue() ,null),//.HasValue ? order.LensRecipe.Addition.Left.Value.ToString() : "",
+                LeftAxis = order.LensRecipe.Axis.Return(x => x.Left.GetStringValue() ,null),//.HasValue ? order.LensRecipe.Axis.Left.Value.ToString() : "",
+                LeftPower = order.LensRecipe.Power.Return(x => x.Left.GetStringValue() ,null),//.HasValue ? order.LensRecipe.Power.Left.Value.ToString() : "",
+                LeftCylinder = order.LensRecipe.Cylinder.Return(x => x.Left.GetStringValue() ,null),//.HasValue ? order.LensRecipe.Cylinder.Left.Value.ToString() : "",
+                LeftDiameter = order.LensRecipe.Diameter.Return(x => x.Left.GetStringValue() ,null),//.HasValue ? order.LensRecipe.Diameter.Left.Value.ToString() : "",
+                LeftBaseCurve = order.LensRecipe.BaseCurve.Return(x => x.Left.GetStringValue() ,null),//.HasValue ? order.LensRecipe.BaseCurve.Left.Value.ToString() : "",
+                RightAddition = order.LensRecipe.Addition.Return(x => x.Left.GetStringValue() ,null),//.HasValue ? order.LensRecipe.Addition.Right.Value.ToString() : "",
+                RightAxis = order.LensRecipe.Axis.Return(x => x.Right.GetStringValue() ,null),//.HasValue ? order.LensRecipe.Axis.Right.Value.ToString() : "",
+                RightPower = order.LensRecipe.Power.Return(x => x.Right.GetStringValue() ,null),//.HasValue ? order.LensRecipe.Power.Right.Value.ToString() : "",
+                RightCylinder = order.LensRecipe.Cylinder.Return(x => x.Right.GetStringValue() ,null),//.HasValue ? order.LensRecipe.Cylinder.Right.Value.ToString() : "",
+                RightDiameter = order.LensRecipe.Diameter.Return(x => x.Right.GetStringValue() ,null),//.HasValue ? order.LensRecipe.Diameter.Right.Value.ToString() : "",
+                RightBaseCurve = order.LensRecipe.BaseCurve.Return(x => x.Right.GetStringValue() ,null),//.HasValue ? order.LensRecipe.BaseCurve.Right.Value.ToString() : "",
+            };
+            return HtmlTemplate().ReplaceWith(data);
+            
+        }
+
+        private static string HtmlTemplate()
+        {
+            return @"<html>
+              <head>
+                <style type=""text/css"">
+		            td { padding-right: 1em; }
+		            body { padding:0; margin:0; font: 0.7em Verdana, Arial, Helvetica, sans-serif; } 
+		            table {font-size: 1.0em;}
+	            </style>
+              </head>
+	            <body>
+	            <table>
+		            <tr><td colspan=""6"">Beställnings-id: {OrderId}</td></tr>
+		            <tr><td colspan=""6"">Från butik: {ShopName}</td></tr>
+		            <tr><td colspan=""6"">Artikel: {Article}</td></tr>
+		            <tr class=""spacer-row""><td colspan=""6""/></tr>
+		            <tr>
+			            <td>Styrka Höger: {RightPower}</td>
+			            <td>Addition Höger: {RightAddition}</td>
+			            <td>Baskurva Höger: {RightBaseCurve}</td>
+			            <td>Diameter Höger: {RightDiameter}</td>
+			            <td>Cylinder Höger: {RightCylinder}</td>
+			            <td>Axel Höger: {RightAxis}</td>
+		            </tr>
+		            <tr>
+			            <td>Styrka Vänster: {LeftPower}</td>
+			            <td>Addition Vänster: {LeftAddition}</td>
+			            <td>Baskurva Vänster: {LeftBaseCurve}</td>
+			            <td>Diameter Vänster: {LeftDiameter}</td>
+			            <td>Cylinder Vänster: {LeftCylinder}</td>
+			            <td>Axel Vänster: {LeftAxis}</td>
+		            </tr>
+		            <tr class=""spacer-row""><td colspan=""6""/></tr>
+		            <tr><td colspan=""6"">Leveransadress:</td></tr>
+		            <tr><td colspan=""6"">{Receiver}</td></tr>
+		            <tr><td colspan=""6"">{AddressLineOne}</td></tr>
+		            <tr><td colspan=""6"">{AddressLineTwo}</td></tr>
+		            <tr><td colspan=""6"">{PostalCode}</td></tr>
+		            <tr><td colspan=""6"">{City}</td></tr>
+	            </table>
+	            </body>
+            </html>";
+        }
+
+    }
+}

bf6ed65d55268a7e78c12c1855c09edf841f62ec
Carl Berg <carl.berg@spinit.se>
2012-03-06 13:27:04 +0000

    Updated OrderEmailSender to batch orders by shop and supplier.

--- Synologen.Core/Domain/Services/ISendOrderService.cs
+++ Synologen.Core/Domain/Services/ISendOrderService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Spinit.Wpc.Synologen.Core.Domain.Model.Orders;
 
 namespace Spinit.Wpc.Synologen.Core.Domain.Services
@@ -5,5 +6,6 @@ namespace Spinit.Wpc.Synologen.Core.Domain.Services
     public interface ISendOrderService
     {
         int SendOrderByEmail(Order order);
+    	int SendOrdersBatchedByShopAndSupplier(IEnumerable<Order> orderWithShopAndSupplierInCommon, ArticleSupplier supplier);
     }
 }
--- Synologen.Service/Client.OrderEmailSender/Services/SendOrderService.cs
+++ Synologen.Service/Client.OrderEmailSender/Services/SendOrderService.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
+using RazorEngine;
 using Spinit.Extensions;
 using Spinit.Services.Client;
 using Spinit.Wpc.Synologen.Core.Domain.Model.Orders;
 using Spinit.Wpc.Synologen.Core.Domain.Services;
 using Synologen.Service.Client.OrderEmailSender.Application.Extensions;
+using Synologen.Service.Client.OrderEmailSender.Model;
 
 namespace Synologen.Service.Client.OrderEmailSender.Services
 {
@@ -22,18 +25,22 @@ namespace Synologen.Service.Client.OrderEmailSender.Services
 			return ParseAndSend(order);
 		}
 
+		public int SendOrdersBatchedByShopAndSupplier(IEnumerable<Order> orderWithShopAndSupplierInCommon, ArticleSupplier supplier)
+		{
+			var to = supplier.OrderEmailAddress;
+			var from = _configurationSettings.OrderSenderEmailAddress;
+			var subject = _configurationSettings.OrderEmailSubjectFormat;
+			var viewModel = new OrderEmailListModel(orderWithShopAndSupplierInCommon);
+			var body = Razor.Parse(RazorTemplate(), viewModel);
+			return _client.SendMail(to, from, subject, body, EmailPriority.Medium);
+		}
+
 		private int ParseAndSend(Order order)
 		{
-			
 			var to = order.LensRecipe.Article.Left.ArticleSupplier.OrderEmailAddress;
 			var from = _configurationSettings.OrderSenderEmailAddress;
 			var body = GetEmailBody(order);
 			var subject = _configurationSettings.OrderEmailSubjectFormat;
-			//var subject = _configurationSettings.OrderEmailSubjectFormat.ReplaceWith(new
-			//{
-			//    OrderArticleName = order.Article.Name
-			//});
-
 			return _client.SendMail(to, from, subject, body, EmailPriority.Medium);
 		}
 
@@ -135,6 +142,101 @@ namespace Synologen.Service.Client.OrderEmailSender.Services
 					</table>
 				</body>
 			</html>";
+		}
+
+		private static string RazorTemplate()
+		{
+			return @"
+<!DOCTYPE html>
+<html lang=""sv"">
+	<head>
+		<style type=""text/css"">
+			html {margin: 0; padding: 0; height: 100%;}
+			body {font: 0.875em/1.5em sans-serif; line-height: 1.5em; color: black; margin: 0; padding: 0 5%;}
+			p { margin: 0; padding: 0; border: 0; vertical-align: baseline; }
+			table {border-collapse: collapse; border-spacing: 0;}
+			table.parameters, .parameters th, .parameters td { border: solid 1px #A0A0A0; }
+			tbody th { text-align:left; margin: 0px; vertical-align: text-top; }
+			tr { margin: 0px; padding 0px; }
+			td, th{ padding: 2px 10px; }
+		</style>
+		<meta charset=""utf-8"">
+		<title>Beställning från Synologen</title>
+	</head>
+	<body>
+		<hr/>
+	@foreach(var order in Model.Orders){
+		<div class=""order"">
+			<table class=""order-information"">
+				<tbody>
+					<tr class=""order-id""><th>Beställnings-id</th><td>@order.OrderId</td></tr>
+					<tr class=""shop""><th>Från butik</th><td>@order.ShopName, @order.ShopCity</td></tr>
+					<tr class=""customer""><th>Kund</th><td>@order.Customer</td></tr>
+					<tr class=""reference""><th>Referens</th><td>@order.Reference</td></tr>
+					<tr class=""delivery-address"">
+						<th>Leveransadress</th>
+						<td>
+							<p>@order.DeliveryAddress.Receiver</p>
+							<p>@order.DeliveryAddress.AddressLineOne</p>
+							<p>@order.DeliveryAddress.AddressLineTwo</p>
+							<p>@order.DeliveryAddress.PostalCode @order.DeliveryAddress.City</p>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<br/>
+			<table class=""parameters"">
+				<thead>
+					<tr><th>Parameter</th><th>Höger</th><th>Vänster</th></tr>
+				</thead>
+				<tbody>
+					<tr class=""article"">
+						<th>Artikel</th>
+						<td class=""right"">@order.Article.Right</td>
+						<td class=""left"">@order.Article.Left</td>
+					</tr>
+					<tr class=""quantity"">
+						<th>Antal</th>
+						<td class=""right"">@order.Quantity.Right</td>
+						<td class=""left"">@order.Quantity.Left</td>
+					</tr>
+					<tr class=""power"">
+						<th>Styrka</th>
+						<td class=""right"">@order.Power.Right</td>
+						<td class=""left"">@order.Power.Left</td>
+					</tr>
+					<tr class=""addition"">
+						<th>Addition</th>
+						<td class=""right"">@order.Addition.Right</td>
+						<td class=""left"">@order.Addition.Left</td>
+					</tr>
+					<tr class=""basecurve"">
+						<th>Baskurva</th>
+						<td class=""right"">@order.BaseCurve.Right</td>
+						<td class=""left"">@order.BaseCurve.Left</td>
+					</tr>
+					<tr class=""diameter"">
+						<th>Diameter</th>
+						<td class=""right"">@order.Diameter.Right</td>
+						<td class=""left"">@order.Diameter.Left</td>
+					</tr>
+					<tr class=""diameter"">
+						<th>Cylinder</th>
+						<td class=""right"">@order.Cylinder.Right</td>
+						<td class=""left"">@order.Cylinder.Left</td>
+					</tr>
+					<tr class=""axis"">
+						<th>Axel</th>
+						<td class=""right"">@order.Axis.Right</td>
+						<td class=""left"">@order.Axis.Left</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+		<hr/>
+		}
+	</body>
+</html>";
 		}
 
 	}

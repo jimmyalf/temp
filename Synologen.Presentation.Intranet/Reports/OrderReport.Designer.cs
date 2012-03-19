9e14eac481625542a8d982f5b9f3ce6f72b4964b
Carl Berg <carl.berg@spinit.se>
2012-03-19 13:35:31 +0000

    Updated Report view layout, added a third page with static legal information.

--- Synologen.Presentation.Intranet/Reports/OrderReport.Designer.cs
+++ Synologen.Presentation.Intranet/Reports/OrderReport.Designer.cs
@@ -30,6 +30,8 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Reports
 			this.reportPageOne = new DataDynamics.ActiveReports.SubReport();
 			this.reportPageTwo = new DataDynamics.ActiveReports.SubReport();
 			this.pageBreak1 = new DataDynamics.ActiveReports.PageBreak();
+			this.reportPageThree = new DataDynamics.ActiveReports.SubReport();
+			this.pageBreak2 = new DataDynamics.ActiveReports.PageBreak();
 			((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
 			// 
 			// detail
@@ -38,47 +40,75 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Reports
 			this.detail.Controls.AddRange(new DataDynamics.ActiveReports.ARControl[] {
             this.reportPageOne,
             this.reportPageTwo,
-            this.pageBreak1});
-			this.detail.Height = 0.4062502F;
+            this.pageBreak1,
+            this.reportPageThree,
+            this.pageBreak2});
+			this.detail.Height = 1.095226F;
 			this.detail.Name = "detail";
 			// 
 			// reportPageOne
 			// 
 			this.reportPageOne.CloseBorder = false;
-			this.reportPageOne.Height = 0.198F;
+			this.reportPageOne.Height = 0.2952756F;
 			this.reportPageOne.Left = 0F;
 			this.reportPageOne.Name = "reportPageOne";
 			this.reportPageOne.Report = null;
 			this.reportPageOne.ReportName = "reportPageOne";
 			this.reportPageOne.Top = 0F;
-			this.reportPageOne.Width = 6.562F;
+			this.reportPageOne.Width = 6.299212F;
 			// 
 			// reportPageTwo
 			// 
 			this.reportPageTwo.CloseBorder = false;
-			this.reportPageTwo.Height = 0.198F;
+			this.reportPageTwo.Height = 0.3937008F;
 			this.reportPageTwo.Left = 0F;
 			this.reportPageTwo.Name = "reportPageTwo";
 			this.reportPageTwo.Report = null;
 			this.reportPageTwo.ReportName = "reportPageTwo";
-			this.reportPageTwo.Top = 0.198F;
-			this.reportPageTwo.Width = 6.562F;
+			this.reportPageTwo.Top = 0.2952756F;
+			this.reportPageTwo.Width = 6.299212F;
 			// 
 			// pageBreak1
 			// 
-			this.pageBreak1.Height = 0.2F;
+			this.pageBreak1.Height = 0.01F;
 			this.pageBreak1.Left = 0F;
 			this.pageBreak1.Name = "pageBreak1";
-			this.pageBreak1.Size = new System.Drawing.SizeF(6.5F, 0.2F);
-			this.pageBreak1.Top = 0.198F;
-			this.pageBreak1.Width = 6.5F;
+			this.pageBreak1.Size = new System.Drawing.SizeF(6.299212F, 0.01F);
+			this.pageBreak1.Top = 0.2952756F;
+			this.pageBreak1.Width = 6.299212F;
+			// 
+			// reportPageThree
+			// 
+			this.reportPageThree.CloseBorder = false;
+			this.reportPageThree.Height = 0.3937008F;
+			this.reportPageThree.Left = 0F;
+			this.reportPageThree.Name = "reportPageThree";
+			this.reportPageThree.Report = null;
+			this.reportPageThree.ReportName = "reportPageThree";
+			this.reportPageThree.Top = 0.6889764F;
+			this.reportPageThree.Width = 6.299212F;
+			// 
+			// pageBreak2
+			// 
+			this.pageBreak2.Height = 0.01F;
+			this.pageBreak2.Left = 0F;
+			this.pageBreak2.Name = "pageBreak2";
+			this.pageBreak2.Size = new System.Drawing.SizeF(6.299212F, 0.01F);
+			this.pageBreak2.Top = 0.6889764F;
+			this.pageBreak2.Width = 6.299212F;
 			// 
 			// OrderReport
 			// 
 			this.MasterReport = false;
-			this.PageSettings.PaperHeight = 11F;
-			this.PageSettings.PaperWidth = 8.5F;
-			this.PrintWidth = 6.562F;
+			this.PageSettings.DefaultPaperSize = false;
+			this.PageSettings.Margins.Bottom = 0.984252F;
+			this.PageSettings.Margins.Left = 0.984252F;
+			this.PageSettings.Margins.Right = 0.984252F;
+			this.PageSettings.Margins.Top = 0.984252F;
+			this.PageSettings.PaperHeight = 11.69291F;
+			this.PageSettings.PaperKind = System.Drawing.Printing.PaperKind.A4;
+			this.PageSettings.PaperWidth = 8.267716F;
+			this.PrintWidth = 6.299212F;
 			this.Sections.Add(this.detail);
 			this.StyleSheet.Add(new DDCssLib.StyleSheetRule("font-family: Arial; font-style: normal; text-decoration: none; font-weight: norma" +
             "l; font-size: 10pt; color: Black", "Normal"));
@@ -94,5 +124,7 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Reports
 		private DataDynamics.ActiveReports.SubReport reportPageOne;
 		private DataDynamics.ActiveReports.SubReport reportPageTwo;
 		private DataDynamics.ActiveReports.PageBreak pageBreak1;
+		private DataDynamics.ActiveReports.SubReport reportPageThree;
+		private DataDynamics.ActiveReports.PageBreak pageBreak2;
 	}
 }

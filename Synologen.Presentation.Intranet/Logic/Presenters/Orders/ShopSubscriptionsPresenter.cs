70bc3a3716eff6c57b9f14062f27ed1ba3f62a4b
Carl Berg <carl.berg@spinit.se>
2012-03-19 14:49:57 +0000

    Updated consent status display names. Updated ReturnSwitch functionality.

--- Synologen.Presentation.Intranet/Logic/Presenters/Orders/ShopSubscriptionsPresenter.cs
+++ Synologen.Presentation.Intranet/Logic/Presenters/Orders/ShopSubscriptionsPresenter.cs
@@ -9,6 +9,7 @@ using Spinit.Wpc.Synologen.Core.Domain.Model.Orders.SubscriptionTypes;
 using Spinit.Wpc.Synologen.Core.Domain.Persistence.Criterias.Orders;
 using Spinit.Wpc.Synologen.Core.Domain.Persistence.Orders;
 using Spinit.Wpc.Synologen.Core.Domain.Services;
+using Spinit.Wpc.Synologen.Core.Extensions;
 using Spinit.Wpc.Synologen.Core.Utility;
 using Spinit.Wpc.Synologen.Presentation.Intranet.Logic.Views.Orders;
 using Spinit.Wpc.Synologen.Presentation.Intranet.Models.Orders;
@@ -41,7 +42,7 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Logic.Presenters.Orders
 			Func<int, string> getPageUrl = pageId => _routingService.GetPageUrl(pageId) ?? "#";
 			var viewModelSubscriptions = subscriptions.Select(x => ParseSubscription(x, () => getPageUrl(View.CustomerDetailsPageId), () => getPageUrl(View.SubscriptionDetailsPageId)));
 			View.Model = new ShopSubscriptionsModel(viewModelSubscriptions);
-			_routingService.GetPageUrl(55, new {customer = 1, subscription = 55, query = "hej"});
+			_routingService.GetPageUrl(55, new {customer = 1, subscription = 55});
 		}
 
 		protected SubscriptionListItem ParseSubscription(Subscription subscription, Func<string> getCustomerDetailsUrl, Func<string> getSubscriptionDetailsUrl)
@@ -67,12 +68,9 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Logic.Presenters.Orders
 		protected string GetStatusMessage(Subscription subscription)
 		{
 		    return Switch.On<Subscription,string>(subscription)
-		        .Case(s => !s.Active, "Inaktivt")
-		        .Case(s => s.Errors != null && s.Errors.Any(e => !e.IsHandled), "Har ohanterade fel")
-		        .Case(s => s.ConsentStatus == SubscriptionConsentStatus.Accepted, "Aktivt")
-		        .Case(s => s.ConsentStatus == SubscriptionConsentStatus.Denied, "Ej medgivet")
-		        .Case(s => s.ConsentStatus == SubscriptionConsentStatus.NotSent, "Medgivande ej skickat")
-		        .Case(s => s.ConsentStatus == SubscriptionConsentStatus.Sent, "Skickat för medgivande")
+		        .Case(s => !s.Active, "Vilande autogiro")
+		        .Case(s => s.Errors != null && s.Errors.Any(e => !e.IsHandled), "Transaktion ej genomförd")
+		        .Default(s => s.ConsentStatus.GetEnumDisplayName())
 		        .Evaluate();
 		}
 	}

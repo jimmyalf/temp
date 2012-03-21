24d63b88a54294fde380853644aa1dea734a0fd0
Carl Berg <carl.berg@spinit.se>
2012-03-21 11:56:16 +0000

    Updated fonts and sizes.

--- Synologen.Presentation.Intranet/Reports/OrderAGDescriptionReport.Designer.cs
+++ Synologen.Presentation.Intranet/Reports/OrderAGDescriptionReport.Designer.cs
@@ -82,18 +82,18 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Reports
 			this.label63.Left = 0.0984252F;
 			this.label63.MultiLine = false;
 			this.label63.Name = "label63";
-			this.label63.Style = "font-size: 8pt; font-weight: bold; vertical-align: bottom; ddo-char-set: 0";
+			this.label63.Style = "font-size: 7pt; font-weight: bold; vertical-align: bottom; ddo-char-set: 1";
 			this.label63.Text = "Allmänt";
 			this.label63.Top = 0F;
 			this.label63.Width = 2.952756F;
 			// 
 			// label64
 			// 
-			this.label64.Height = 2.559056F;
+			this.label64.Height = 1.968505F;
 			this.label64.HyperLink = null;
 			this.label64.Left = 0.0984252F;
 			this.label64.Name = "label64";
-			this.label64.Style = "font-size: 8pt; ddo-char-set: 1";
+			this.label64.Style = "font-size: 7pt; ddo-char-set: 1";
 			this.label64.Text = resources.GetString("label64.Text");
 			this.label64.Top = 0.1968499F;
 			this.label64.Width = 2.952756F;
@@ -105,18 +105,18 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Reports
 			this.label1.Left = 3.248032F;
 			this.label1.MultiLine = false;
 			this.label1.Name = "label1";
-			this.label1.Style = "font-size: 8pt; font-weight: bold; vertical-align: bottom; ddo-char-set: 0";
+			this.label1.Style = "font-size: 7pt; font-weight: bold; vertical-align: bottom; ddo-char-set: 1";
 			this.label1.Text = "Stoppa betalning (återkallelse av betalningsorder)";
 			this.label1.Top = 5.736947E-07F;
 			this.label1.Width = 2.952756F;
 			// 
 			// label2
 			// 
-			this.label2.Height = 1.574803F;
+			this.label2.Height = 1.181102F;
 			this.label2.HyperLink = null;
 			this.label2.Left = 3.248032F;
 			this.label2.Name = "label2";
-			this.label2.Style = "font-size: 8pt; ddo-char-set: 1";
+			this.label2.Style = "font-size: 7pt; ddo-char-set: 1";
 			this.label2.Text = resources.GetString("label2.Text");
 			this.label2.Top = 0.1968504F;
 			this.label2.Width = 2.952756F;
@@ -128,21 +128,21 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Reports
 			this.label3.Left = 0.0984252F;
 			this.label3.MultiLine = false;
 			this.label3.Name = "label3";
-			this.label3.Style = "font-size: 8pt; font-weight: bold; vertical-align: bottom; ddo-char-set: 0";
+			this.label3.Style = "font-size: 7pt; font-weight: bold; vertical-align: bottom; ddo-char-set: 0";
 			this.label3.Text = "Definition av bankdag";
-			this.label3.Top = 2.854332F;
+			this.label3.Top = 2.362206F;
 			this.label3.Width = 2.952756F;
 			// 
 			// label4
 			// 
-			this.label4.Height = 0.4921264F;
+			this.label4.Height = 0.3937008F;
 			this.label4.HyperLink = null;
 			this.label4.Left = 0.0984252F;
 			this.label4.Name = "label4";
-			this.label4.Style = "font-size: 8pt; ddo-char-set: 1";
+			this.label4.Style = "font-size: 7pt; ddo-char-set: 1";
 			this.label4.Text = "Med bankdag avses alla dagar utom lördag, söndag, midsommarafton, julafton eller " +
     "nyårsafton eller annan \r\nallmän helgdag.";
-			this.label4.Top = 3.051181F;
+			this.label4.Top = 2.559057F;
 			this.label4.Width = 2.952756F;
 			// 
 			// label5
@@ -152,20 +152,20 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Reports
 			this.label5.Left = 0.0984252F;
 			this.label5.MultiLine = false;
 			this.label5.Name = "label5";
-			this.label5.Style = "font-size: 8pt; font-weight: bold; vertical-align: bottom; ddo-char-set: 0";
+			this.label5.Style = "font-size: 7pt; font-weight: bold; vertical-align: bottom; ddo-char-set: 1";
 			this.label5.Text = "Information om betalning";
-			this.label5.Top = 3.641733F;
+			this.label5.Top = 3.149608F;
 			this.label5.Width = 2.952756F;
 			// 
 			// label6
 			// 
-			this.label6.Height = 2.06693F;
+			this.label6.Height = 1.574805F;
 			this.label6.HyperLink = null;
 			this.label6.Left = 0.0984252F;
 			this.label6.Name = "label6";
-			this.label6.Style = "font-size: 8pt; ddo-char-set: 1";
+			this.label6.Style = "font-size: 7pt; ddo-char-set: 1";
 			this.label6.Text = resources.GetString("label6.Text");
-			this.label6.Top = 3.838582F;
+			this.label6.Top = 3.346457F;
 			this.label6.Width = 2.952756F;
 			// 
 			// label7
@@ -175,20 +175,20 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Reports
 			this.label7.Left = 0.0984252F;
 			this.label7.MultiLine = false;
 			this.label7.Name = "label7";
-			this.label7.Style = "font-size: 8pt; font-weight: bold; vertical-align: bottom; ddo-char-set: 0";
+			this.label7.Style = "font-size: 7pt; font-weight: bold; vertical-align: bottom; ddo-char-set: 1";
 			this.label7.Text = "Täckning måste finnas på konto";
-			this.label7.Top = 6.003938F;
+			this.label7.Top = 5.118111F;
 			this.label7.Width = 2.952756F;
 			// 
 			// label8
 			// 
-			this.label8.Height = 1.181103F;
+			this.label8.Height = 0.8858263F;
 			this.label8.HyperLink = null;
 			this.label8.Left = 0.0984252F;
 			this.label8.Name = "label8";
-			this.label8.Style = "font-size: 8pt; ddo-char-set: 1";
+			this.label8.Style = "font-size: 7pt; ddo-char-set: 1";
 			this.label8.Text = resources.GetString("label8.Text");
-			this.label8.Top = 6.200788F;
+			this.label8.Top = 5.31496F;
 			this.label8.Width = 2.952756F;
 			// 
 			// label9
@@ -198,43 +198,43 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Reports
 			this.label9.Left = 3.248032F;
 			this.label9.MultiLine = false;
 			this.label9.Name = "label9";
-			this.label9.Style = "font-size: 8pt; font-weight: bold; vertical-align: bottom; ddo-char-set: 0";
+			this.label9.Style = "font-size: 7pt; font-weight: bold; vertical-align: bottom; ddo-char-set: 1";
 			this.label9.Text = "Medgivandets giltighetstid, återkallelse";
-			this.label9.Top = 1.870079F;
+			this.label9.Top = 1.574804F;
 			this.label9.Width = 2.952756F;
 			// 
 			// label10
 			// 
-			this.label10.Height = 1.476378F;
+			this.label10.Height = 0.9842522F;
 			this.label10.HyperLink = null;
 			this.label10.Left = 3.248032F;
 			this.label10.Name = "label10";
-			this.label10.Style = "font-size: 8pt; ddo-char-set: 1";
+			this.label10.Style = "font-size: 7pt; ddo-char-set: 1";
 			this.label10.Text = resources.GetString("label10.Text");
-			this.label10.Top = 2.066929F;
+			this.label10.Top = 1.771654F;
 			this.label10.Width = 2.952756F;
 			// 
 			// label11
 			// 
-			this.label11.Height = 0.3937008F;
+			this.label11.Height = 0.2952769F;
 			this.label11.HyperLink = null;
 			this.label11.Left = 3.248032F;
 			this.label11.Name = "label11";
-			this.label11.Style = "font-size: 8pt; font-weight: bold; vertical-align: bottom; ddo-char-set: 0";
+			this.label11.Style = "font-size: 7pt; font-weight: bold; vertical-align: bottom; ddo-char-set: 1";
 			this.label11.Text = "Rätten för betalningsmottagaren och betalarens betaltjänstleverantör att avsluta " +
     "anslutningen till Autogiro";
-			this.label11.Top = 3.641734F;
+			this.label11.Top = 3.051181F;
 			this.label11.Width = 2.952756F;
 			// 
 			// label12
 			// 
-			this.label12.Height = 1.870078F;
+			this.label12.Height = 1.279527F;
 			this.label12.HyperLink = null;
 			this.label12.Left = 3.248032F;
 			this.label12.Name = "label12";
-			this.label12.Style = "font-size: 8pt; ddo-char-set: 1";
+			this.label12.Style = "font-size: 7pt; ddo-char-set: 1";
 			this.label12.Text = resources.GetString("label12.Text");
-			this.label12.Top = 4.035434F;
+			this.label12.Top = 3.346457F;
 			this.label12.Width = 2.952756F;
 			// 
 			// OrderAGDescriptionReport

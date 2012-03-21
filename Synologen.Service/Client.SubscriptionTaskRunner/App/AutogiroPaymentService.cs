c8799cbc35c4d671da4a1194ddf60bd1a5bdcdc4
Carl Berg <carl.berg@spinit.se>
2012-03-21 16:09:46 +0000

    Updated AllSubscriptionsToSendPaymentsForCriteria to include a cutoff date after which subscriptions created won't be sent "this" month.

--- Synologen.Service/Client.SubscriptionTaskRunner/App/AutogiroPaymentService.cs
+++ Synologen.Service/Client.SubscriptionTaskRunner/App/AutogiroPaymentService.cs
@@ -15,9 +15,21 @@ namespace Synologen.Service.Client.SubscriptionTaskRunner.App
 		public DateTime GetPaymentDate()
 		{
 			var paymentDayInMonth = _bgServiceCoordinatorSettingsService.GetPaymentDayInMonth();
+			return GetSelectedOrLastDateInMonth(paymentDayInMonth);
+		}
+
+		public DateTime GetCutOffDateTime()
+		{
 			var paymentCutOffDayInMonth = _bgServiceCoordinatorSettingsService.GetPaymentCutOffDayInMonth();
-			var returnDate = new DateTime(SystemTime.Now.Year, SystemTime.Now.Month, paymentDayInMonth);
-			return SystemTime.Now.Day >= paymentCutOffDayInMonth ? returnDate.AddMonths(1) : returnDate;
+			return GetSelectedOrLastDateInMonth(paymentCutOffDayInMonth, 23, 59, 59);
+		}
+
+		private DateTime GetSelectedOrLastDateInMonth(int day, int hour = 0, int minute = 0, int second = 0)
+		{
+			var lastDayInThisMonth = DateTime.DaysInMonth(SystemTime.Now.Year, SystemTime.Now.Month);
+			return lastDayInThisMonth < day 
+				? new DateTime(SystemTime.Now.Year, SystemTime.Now.Month, lastDayInThisMonth, hour, minute, second)
+				: new DateTime(SystemTime.Now.Year, SystemTime.Now.Month, day, hour, minute, second);
 		}
 	}
 }

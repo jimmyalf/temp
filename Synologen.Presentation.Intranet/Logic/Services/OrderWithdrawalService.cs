ff47827932199ed8d41edaddc4bc7a0d346e9cba
Carl Berg <carl.berg@spinit.se>
2012-04-19 13:05:23 +0000

    Updated OrderWithdrawalService to handle subscriptions that are already active and consented.

--- Synologen.Presentation.Intranet/Logic/Services/OrderWithdrawalService.cs
+++ Synologen.Presentation.Intranet/Logic/Services/OrderWithdrawalService.cs
@@ -12,20 +12,34 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Logic.Services
 			_synologenSettingsService = synologenSettingsService;
 		}
 		
-		public DateTime GetExpectedFirstWithdrawalDate(DateTime subscriptionCreatedDate)
+		public DateTime GetExpectedFirstWithdrawalDate(DateTime subscriptionCreatedDate, bool isAlreadyConsentedSubscription)
 		{
-			if(subscriptionCreatedDate.Day <= _synologenSettingsService.SubscriptionCutoffDate)
+			//Subscription is already consented and withdrawal date not yet passed
+			if(isAlreadyConsentedSubscription && subscriptionCreatedDate.Day <= _synologenSettingsService.SubscriptionWithdrawalDate)
+			{
+				GetWithdrawalDateInGivenMonth(subscriptionCreatedDate);
+			}
+			//Subscription is already consented but withdrawal date has passed
+			if(isAlreadyConsentedSubscription && subscriptionCreatedDate.Day > _synologenSettingsService.SubscriptionWithdrawalDate)
+			{
+				GetWithdrawalDateInNextMonth(subscriptionCreatedDate);
+			}
+			//Subscription is not yet consented and cut off date has not yet passed
+			if(!isAlreadyConsentedSubscription && subscriptionCreatedDate.Day <= _synologenSettingsService.SubscriptionCutoffDate)
 		 	{
-		 		return new DateTime(
-					subscriptionCreatedDate.Year, 
-					subscriptionCreatedDate.Month, 
-					_synologenSettingsService.SubscriptionWithdrawalDate);
+		 		return GetWithdrawalDateInGivenMonth(subscriptionCreatedDate);
 		 	}
-			return new DateTime(
-				subscriptionCreatedDate.Year, 
-				subscriptionCreatedDate.Month, 
-				_synologenSettingsService.SubscriptionWithdrawalDate)
-				.AddMonths(1);
+			//Subscription is not yet consented but cut off date has passed
+			return GetWithdrawalDateInNextMonth(subscriptionCreatedDate);
+		}
+
+		private DateTime GetWithdrawalDateInGivenMonth(DateTime value)
+		{
+		 	return new DateTime(value.Year, value.Month, _synologenSettingsService.SubscriptionWithdrawalDate);
+		}
+		private DateTime GetWithdrawalDateInNextMonth(DateTime value)
+		{
+			return new DateTime(value.Year, value.Month, _synologenSettingsService.SubscriptionWithdrawalDate).AddMonths(1);
 		}
 	}
 }

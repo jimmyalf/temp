3fc183545d0ce10025de1a937019c1080776054a
Anders Axelsson <anders.axelsson@spinit.se>
2012-06-08 05:52:42 +0000

    Fixes in test-data.

--- Synologen OPQ/Spinit.Wpc.Synologen.OPQ.Database/Scripts/Test Data/Data Layer Tests/TestData.sql
+++ Synologen OPQ/Spinit.Wpc.Synologen.OPQ.Database/Scripts/Test Data/Data Layer Tests/TestData.sql
@@ -116,6 +116,29 @@ INSERT INTO dbo.SynologenOpqNodeSupplierConnections (
 VALUES ( 
 	1, 1) 
 
+-- =============================================
+-- Create default file-categories
+-- =============================================
+
+SET IDENTITY_INSERT dbo.SynologenOpqFileCategories ON
+
+INSERT INTO dbo.SynologenOpqFileCategories (
+	[Id], [Name], IsActive, CreatedById, CreatedByName, CreatedDate) 
+VALUES ( 
+	1, 'Centrala dokument', 1, 1, 'Admin', GETDATE ())
+	
+INSERT INTO dbo.SynologenOpqFileCategories (
+	[Id], [Name], IsActive, CreatedById, CreatedByName, CreatedDate) 
+VALUES ( 
+	2, 'Egna dokument', 1, 1, 'Admin', GETDATE ())
+
+INSERT INTO dbo.SynologenOpqFileCategories (
+	[Id], [Name], IsActive, CreatedById, CreatedByName, CreatedDate) 
+VALUES ( 
+	3, 'Ifyllda dokument', 1, 1, 'Admin', GETDATE ())
+
+SET IDENTITY_INSERT dbo.SynologenOpqFileCategories OFF
+
 ------------------------------------------------
 -- Inserts into files
 ------------------------------------------------

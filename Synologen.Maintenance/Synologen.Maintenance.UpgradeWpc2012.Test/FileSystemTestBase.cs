7d083d1109ec38598260e797b79f769c5d48b997
Carl Berg <carl.berg@spinit.se>
2012-09-05 14:20:51 +0000

    Updated tests.

---
+++ Synologen.Maintenance/Synologen.Maintenance.UpgradeWpc2012.Test/FileSystemTestBase.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using NUnit.Framework;
+using Synologen.Maintenance.UpgradeWpc2012.Domain.Settings;
+using Synologen.Maintenance.UpgradeWpc2012.Test.Persistence;
+
+namespace Synologen.Maintenance.UpgradeWpc2012.Test
+{
+	public class FileSystemTestBase
+	{
+		protected Migrator Migrator;
+
+		[SetUp]
+		protected void Setup()
+		{
+			FileSystem.CleanCommonFilesFolder();
+			Migrator = new Migrator();
+		}
+
+		protected string CreateFile(string relativePath)
+		{
+			var root = Settings.CommonFilesDirectory;
+			var path = Path.Combine(root.FullName, relativePath);
+			File.AppendAllText(path, "Testfile", Encoding.UTF8);
+			return path;
+		}
+
+		protected string CreateDirectory(string relativePath)
+		{
+			var root = Settings.CommonFilesDirectory;
+			var path = Path.Combine(root.FullName, relativePath);
+			Directory.CreateDirectory(path);
+			return path;
+		}
+
+		protected IEnumerable<FileInfo> GetAllFiles(string searchPattern = null, SearchOption searchOption = SearchOption.TopDirectoryOnly)
+		{
+			return searchPattern != null 
+				? Settings.CommonFilesDirectory.GetFiles(searchPattern, searchOption) 
+				: Settings.CommonFilesDirectory.GetFiles();
+		}
+
+		protected IEnumerable<DirectoryInfo> GetAllDirectories(string searchPattern = null, SearchOption searchOption = SearchOption.TopDirectoryOnly)
+		{
+			return searchPattern != null 
+				? Settings.CommonFilesDirectory.GetDirectories(searchPattern, searchOption) 
+				: Settings.CommonFilesDirectory.GetDirectories();
+		}
+
+		protected DirectoryInfo GetDirectory(string relativePath)
+		{
+			var root = Settings.CommonFilesDirectory;
+			var path = Path.Combine(root.FullName, relativePath);
+			return new DirectoryInfo(path);
+		}
+
+		[TearDown]
+		protected void TearDown()
+		{
+			
+		}	
+	}
+}
---
+++ Synologen.Maintenance/Synologen.Maintenance.UpgradeWpc2012.Test/Persistence/FileSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Linq;
+using Synologen.Maintenance.UpgradeWpc2012.Domain.Settings;
+
+namespace Synologen.Maintenance.UpgradeWpc2012.Test.Persistence
+{
+	public static class FileSystem
+	{
+		public static void CleanCommonFilesFolder()
+		{
+			
+			var files = Settings.CommonFilesDirectory.GetFiles("*.*", SearchOption.AllDirectories);
+			foreach (var file in files)
+			{
+				file.Delete();
+			}
+			var directories = Settings.CommonFilesDirectory.GetDirectories("*");
+			foreach (var directory in directories)
+			{
+				directory.Delete(true);
+			}
+		}
+	}
+}

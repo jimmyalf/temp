812ba730a848663cada36bdc4eff05a9ec7b3141
Carl Berg <carl.berg@spinit.se>
2012-09-10 10:49:16 +0000

    Cleaned out test project to test only additional components (OPQ).

--- Synologen.Maintenance/Synologen.Maintenance.UpgradeWpc2012.Test/Base/FileSystemTestBase.cs
+++ Synologen.Maintenance/Synologen.Maintenance.UpgradeWpc2012.Test/Base/FileSystemTestBase.cs
@@ -1,59 +1,16 @@
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
 using NUnit.Framework;
-using Synologen.Maintenance.UpgradeWpc2012.Domain.Settings;
-using Synologen.Maintenance.UpgradeWpc2012.Test.Persistence;
+using Spinit.Wpc.Maintenance.FileAndContentMigration.Test;
 
 namespace Synologen.Maintenance.UpgradeWpc2012.Test.Base
 {
-	public class FileSystemTestBase
+	public abstract class FileSystemTestBase : FileSystemTestBase<FileSystem>
 	{
-		protected Migrator Migrator;
+		protected FileSystemTestBase() : base(new FileSystem()) {}
 
 		[SetUp]
-		protected void Setup()
+		protected override void Setup()
 		{
-			FileSystem.CleanCommonFilesFolder();
-			Migrator = new Migrator();
-			Migrator.Initialize();
-		}
-
-		protected string CreateFile(string relativePath)
-		{
-			var root = Settings.CommonFilesDirectory;
-			var path = Path.Combine(root.FullName, relativePath);
-			File.AppendAllText(path, "Testfile", Encoding.UTF8);
-			return path;
-		}
-
-		protected string CreateDirectory(string relativePath)
-		{
-			var root = Settings.CommonFilesDirectory;
-			var path = Path.Combine(root.FullName, relativePath);
-			Directory.CreateDirectory(path);
-			return path;
-		}
-
-		protected IEnumerable<FileInfo> GetAllFiles(string searchPattern = null, SearchOption searchOption = SearchOption.TopDirectoryOnly)
-		{
-			return searchPattern != null 
-				? Settings.CommonFilesDirectory.GetFiles(searchPattern, searchOption) 
-				: Settings.CommonFilesDirectory.GetFiles();
-		}
-
-		protected IEnumerable<DirectoryInfo> GetAllDirectories(string searchPattern = null, SearchOption searchOption = SearchOption.TopDirectoryOnly)
-		{
-			return searchPattern != null 
-				? Settings.CommonFilesDirectory.GetDirectories(searchPattern, searchOption) 
-				: Settings.CommonFilesDirectory.GetDirectories();
-		}
-
-		protected DirectoryInfo GetDirectory(string relativePath)
-		{
-			var root = Settings.CommonFilesDirectory;
-			var path = Path.Combine(root.FullName, relativePath);
-			return new DirectoryInfo(path);
+			base.Setup();
 		}
 
 		[TearDown]
---
+++ Synologen.Maintenance/Synologen.Maintenance.UpgradeWpc2012.Test/Persistence/Queries/AllOPQDocumentHistoryEntitiesQuery.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Spinit.Data.SqlClient.SqlBuilder;
+using Spinit.Wpc.Maintenance.FileAndContentMigration.Persistence;
+using Synologen.Maintenance.UpgradeWpc2012.Domain.Model.Entities;
+
+namespace Synologen.Maintenance.UpgradeWpc2012.Test.Persistence.Queries
+{
+	public class AllOPQDocumentHistoryEntitiesQuery : PersistenceBase
+	{
+		public IEnumerable<OPQDocumentHistoryEntity> Execute()
+		{
+			var query = QueryBuilder.Build(@"SELECT Id, DocumentContent FROM SynologenOpqDocumentHistories");
+			return Query(query, OPQDocumentHistoryEntity.Parse).ToList();
+		}		 
+	}
+}

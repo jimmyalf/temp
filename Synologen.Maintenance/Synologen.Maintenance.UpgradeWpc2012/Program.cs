b5978eb0edcfeaac630c3199043266893eccb568
Carl Berg <carl.berg@spinit.se>
2012-09-18 14:40:33 +0000

    Updated config for live migration.

--- Synologen.Maintenance/Synologen.Maintenance.UpgradeWpc2012/Program.cs
+++ Synologen.Maintenance/Synologen.Maintenance.UpgradeWpc2012/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Xml.Serialization;
 using Spinit.Wpc.Maintenance.FileAndContentMigration;
 using Spinit.Wpc.Maintenance.FileAndContentMigration.Domain.Config;
 using Spinit.Wpc.Maintenance.FileAndContentMigration.Domain.Extensions;
 using Spinit.Wpc.Maintenance.FileAndContentMigration.Domain.Model.ComponentMigrators;
+using Spinit.Wpc.Maintenance.FileAndContentMigration.Domain.Model.Results;
 using Synologen.Maintenance.UpgradeWpc2012.Domain.Model.ComponentMigrators;
 
 namespace Synologen.Maintenance.UpgradeWpc2012
@@ -19,16 +21,25 @@ namespace Synologen.Maintenance.UpgradeWpc2012
 			migrator.FileRenamed += (s, e) => Console.WriteLine(e);
 			migrator.ComponentEntityUpdated += (s, e) => Console.WriteLine(e);
 			migrator.Initialize();
-			migrator.MigrateBaseFiles();
-			var renamedDirectories = migrator.MigrateDirectories();
-			var renamedFiles = migrator.MigrateFiles();
-			var renamedContent = migrator.MigrateEntity(new ContentMigrator());
-			var renamedNews = migrator.MigrateEntity(new NewsMigrator());
-			var renamedCourses = migrator.MigrateEntity(new CourseMigrator());
-			var renamedMembers = migrator.MigrateEntity(new MemberContentMigrator());
-			var renamedOPQDocumentEntries = migrator.MigrateEntity(new OPQDocumentMigrator());
-			var renamedOPQDocumentHistoryEntries = migrator.MigrateEntity(new OPQDocumentHistoryMigrator());
+			migrator.MigrateBaseFiles().SaveTo("BaseFiles.xml");
+			migrator.MigrateDirectories().SaveTo("Directories.xml");
+			migrator.MigrateFiles().SaveTo("Files.xml");
+			migrator.MigrateEntity(new ContentMigrator()).SaveTo("Content.xml");
+			migrator.MigrateEntity(new NewsMigrator()).SaveTo("News.xml");
+			migrator.MigrateEntity(new CourseMigrator()).SaveTo("Course.xml");
+			migrator.MigrateEntity(new MemberContentMigrator()).SaveTo("MemberContent.xml");
+			migrator.MigrateEntity(new OPQDocumentMigrator()).SaveTo("OPQDocument.xml");
+			migrator.MigrateEntity(new OPQDocumentHistoryMigrator()).SaveTo("OPQDocumentHistory.xml");
 			Console.ReadKey();
+		}
+	}
+
+	public static class ResultExtensions
+	{
+		public static void SaveTo<TEntity>(this IOperationResultCollection<TEntity> resultCollection,string fileName)
+		{
+			var path = Path.Combine(@"C:\Users\cber\Desktop\MigrationResults\", fileName);
+			resultCollection.Save(path);
 		}
 	}
 }

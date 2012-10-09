dd7267c0455219807cc1f3484f1590823c7bb47f
Carl Berg <carl.berg@spinit.se>
2012-10-09 13:35:38 +0000

    Added MigrateSubscriptionAmounts project.

---
+++ Synologen.Maintenance/Synologen.Maintenance.MigrateSubscriptionAmounts/Persistence/PersistenceBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Configuration;
+
+namespace Synologen.Maintenance.MigrateSubscriptionAmounts.Persistence
+{
+	public class PersistenceBase : Spinit.Data.SqlClient.PersistenceBase
+	{
+		public PersistenceBase() : base(ConfigurationManager.ConnectionStrings["WpcServer"].ConnectionString) {}
+	}
+}
---
+++ Synologen.Maintenance/Synologen.Maintenance.MigrateSubscriptionAmounts/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Synologen.Maintenance.MigrateSubscriptionAmounts
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}

2439b7cd4a57b7e5e9a517aa1d6000c284cf8cec
Carl Berg <carl.berg@spinit.se>
2012-10-10 12:43:48 +0000

    Added order domain object, created static helper class Data for data access

---
+++ Synologen.Maintenance/Synologen.Maintenance.MigrateSubscriptionAmounts/Domain/Model/Order.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using Synologen.Maintenance.MigrateSubscriptionAmounts.Persistence;
+
+namespace Synologen.Maintenance.MigrateSubscriptionAmounts.Domain.Model
+{
+	public class Order
+	{
+		public int Id { get; set; }
+		public int PaymentOptionSubscripitonId { get; set; }
+		public int SubscriptionItemId { get; set; }
+		public decimal? OrderTotalWithdrawalAmount { get; set; }
+		public decimal? TaxedWithdrawalAmount { get; set; }
+		public decimal? TaxFreeWithdrawalAmount { get; set; }
+
+        public static Order Parse(IDataRecord record)
+        {
+            return Data.CreateParser<Order>(record)
+                .Parse(x => x.Id)
+				.Parse(x => x.PaymentOptionSubscripitonId)
+				.Parse(x => x.SubscriptionItemId)
+				.Parse(x => x.OrderTotalWithdrawalAmount)
+				.Parse(x => x.TaxedWithdrawalAmount)
+				.Parse(x => x.TaxFreeWithdrawalAmount)
+                .GetValue();
+        }
+	}
+}
---
+++ Synologen.Maintenance/Synologen.Maintenance.MigrateSubscriptionAmounts/Persistence/Data.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using Spinit.Data.FluentParameters;
+
+namespace Synologen.Maintenance.MigrateSubscriptionAmounts.Persistence
+{
+	public static class Data
+	{
+		 public static FluentDataParser<TType> CreateParser<TType>(IDataRecord record) where TType : class
+		 {
+		 	return new FluentDataParser<TType>(record) {ColumnPrefix = string.Empty};
+		 }
+	}
+}

904a8de7936da3a9981c5f48d2f9fbd96a0707bf
Richard Brodie <richard.brodie@spinit.se>
2012-10-11 14:40:20 +0000

    tidying up Program.cs and adding initial HandleWithdrawal() code

--- Synologen.Maintenance/Synologen.Maintenance.MigrateSubscriptionAmounts/Program.cs
+++ Synologen.Maintenance/Synologen.Maintenance.MigrateSubscriptionAmounts/Program.cs
@@ -10,6 +10,7 @@ namespace Synologen.Maintenance.MigrateSubscriptionAmounts
 	{
 		static void Main(string[] args)
 		{
+<<<<<<< HEAD
 			var migrator = new Migrator();
 			var results = migrator.MigrateOrders();
 
@@ -17,14 +18,24 @@ namespace Synologen.Maintenance.MigrateSubscriptionAmounts
 			var OrderTransactions = new FetchOrderTransactions().Execute();
 		    var OrderSubscriptionItems = new FetchOrderSubscriptionItems().Execute();
 		    List<PendingPayment> PendingPayments = new FetchPendingPayments().Execute().ToList();
+=======
+		    var migrator = new Migrator();
+			migrator.MigrateOrders();
+
+            var OrderTransactions = new FetchOrderTransactions().Execute().ToList();
+            var OrderSubscriptionItems = new FetchOrderSubscriptionItems().Execute().ToList();
+		    var PendingPayments = new FetchPendingPayments().Execute().ToList();
+		    var UpdatedTransactions = new List<Transaction>();
+            
+>>>>>>> tidying up Program.cs and adding initial HandleWithdrawal() code
 		    foreach (var orderTransaction in OrderTransactions)
 		    {
-                if (orderTransaction.PendingPaymentId != null)
-                {
-                	//orderTransaction = HandlePendingPayment(orderTransaction, PendingPayments);
-                }
-                else if (orderTransaction.Reason == 3) HandleCorrection(orderTransaction);
-                else if (orderTransaction.Reason == 2) HandleWithdrawal(orderTransaction);
+                if (orderTransaction.PendingPaymentId != null) 
+                	UpdatedTransactions.Add(HandlePendingPayment(orderTransaction, PendingPayments)); 
+                else if (orderTransaction.Reason == 3) 
+                    UpdatedTransactions.Add(HandleCorrection(orderTransaction)); 
+                else if (orderTransaction.Reason == 2) 
+                    UpdatedTransactions.Add(HandleWithdrawal(orderTransaction, OrderSubscriptionItems)); 
 		    }
 			
 		}
@@ -40,12 +51,24 @@ namespace Synologen.Maintenance.MigrateSubscriptionAmounts
         static Transaction HandleCorrection(Transaction transaction)
         {
             transaction.NewAmount.Taxed = transaction.OldAmount;
+            transaction.NewAmount.TaxFree = 0; // unneccessary?
             return transaction;
         }
 
-        static void HandleWithdrawal(Transaction transaction)
+        static Transaction HandleWithdrawal(Transaction transaction, IEnumerable<SubscriptionItem> orderSubscriptionItems)
         {
-            
+            foreach (var OrderSubscriptionItem in orderSubscriptionItems)
+            {
+                if(
+                    (OrderSubscriptionItem.SubscriptionId == transaction.SubscriptionId)
+                    && (OrderSubscriptionItem.Amount.TaxFree + OrderSubscriptionItem.Amount.Taxed == transaction.OldAmount))
+                {
+                    transaction.NewAmount.TaxFree += OrderSubscriptionItem.Amount.TaxFree;
+                    transaction.NewAmount.Taxed += OrderSubscriptionItem.Amount.Taxed;
+                    break;
+                }
+            } // this can all be done much more prettily with Linq but it's probably not worth the time
+            return transaction;
         }
 	}
 }

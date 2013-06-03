2d3b93ba319647af8d1010fe7337d7bfabe19cf1
Carl Berg <carl.berg@spinit.se>
2013-06-03 17:51:25 +0200

    Initiated restructuring of svefaktura generation to favour a more decoupled approach

---
+++ Synologen.ContractSale/Utility/Svefaktura/ISvefakturaHydrator.cs
@@ -0,0 +1,10 @@
+﻿using Spinit.Wpc.Synologen.Business.Domain.Interfaces;
+using Spinit.Wpc.Synologen.Svefaktura.Svefakt2.SFTI.Documents.BasicInvoice;
+
+namespace Spinit.Wpc.Synologen.Invoicing.Svefaktura
+{
+    public interface ISvefakturaHydrator
+    {
+        void Fill(IOrder order, SFTIInvoiceType invoice);
+    }
+}

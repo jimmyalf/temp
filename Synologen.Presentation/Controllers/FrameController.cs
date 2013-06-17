679e84f74583130bf1f82eeb029d698d64a294bd
Carl Berg <carl.berg@spinit.se>
2013-06-17 14:19:55 +0200

    Included supplier as a column in glass types list

--- Synologen.Presentation/Controllers/FrameController.cs
+++ Synologen.Presentation/Controllers/FrameController.cs
@@ -42,7 +42,6 @@ namespace Spinit.Wpc.Synologen.Presentation.Controllers
 				OrderBy = gridPageSortParameters.Column,
 				SortAscending = gridPageSortParameters.Direction == SortDirection.Ascending
 			};
-			//HttpContext.Items.Add("MVC", "Use flag to determine MVC or Webform masterpage");
 			var list = _frameRepository.FindBy(criteria);
 			var viewList = list
 				.ToFrameViewList();

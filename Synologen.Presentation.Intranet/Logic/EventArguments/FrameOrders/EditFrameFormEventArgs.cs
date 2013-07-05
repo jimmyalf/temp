a412394b5eeb4a4ed254114c289f6660c528c676
Carl Berg <carl.berg@spinit.se>
2013-07-05 17:14:23 +0200

    Updated form

--- Synologen.Presentation.Intranet/Logic/EventArguments/FrameOrders/EditFrameFormEventArgs.cs
+++ Synologen.Presentation.Intranet/Logic/EventArguments/FrameOrders/EditFrameFormEventArgs.cs
@@ -3,20 +3,56 @@ using Spinit.Wpc.Synologen.Core.Domain.Model.FrameOrder;
 
 namespace Spinit.Wpc.Synologen.Presentation.Intranet.Logic.EventArguments.FrameOrders
 {
-	public class EditFrameFormEventArgs : EventArgs
+    public class EditFrameFormEventArgs : EventArgs, 
+        IFrameSelectedEventArgs, 
+        IGlassTypeSelectedEventArgs
 	{
 		public EyeParameter SelectedPupillaryDistance { get; set; }
 		public EyeParameter SelectedSphere { get; set; }
 		public EyeParameter SelectedCylinder { get; set; }
-		//public int? SelectedAxisLeft { get; set; }
-		//public int? SelectedAxisRight { get; set; }
 		public EyeParameter<int> SelectedAxis { get; set; }
 		public EyeParameter SelectedAddition { get; set; }
 		public EyeParameter SelectedHeight { get; set; }
         public int SelectedSupplierId { get; set; }
-		public int SelectedFrameId { get; set; }
-		public int SelectedGlassTypeId { get; set; }
+        public int SelectedFrameId { get; set; }
+        public int SelectedGlassTypeId { get; set; }
 		public string Reference { get; set; }
 		public bool PageIsValid { get; set; }
 	}
+
+    public class FrameSelectedEventArgs : SupplierSelectedEventArgs, IFrameSelectedEventArgs
+    {
+        public int SelectedFrameId { get; set; }
+        public EyeParameter SelectedPupillaryDistance { get; set; }
+    }
+
+    public interface IFrameSelectedEventArgs : ISupplierSelectedEventArgs
+    {
+        int SelectedFrameId { get; set; }
+        EyeParameter SelectedPupillaryDistance { get; set; }
+    }
+
+    public class GlassTypeSelectedEventArgs : SupplierSelectedEventArgs, IGlassTypeSelectedEventArgs
+    {
+        public int SelectedGlassTypeId { get; set; }
+        public EyeParameter SelectedSphere { get; set; }
+        public EyeParameter SelectedCylinder { get; set; }
+    }
+
+    public interface IGlassTypeSelectedEventArgs : ISupplierSelectedEventArgs
+    {
+        int SelectedGlassTypeId { get; set; }
+        EyeParameter SelectedSphere { get; set; }
+        EyeParameter SelectedCylinder { get; set; }
+    }
+
+    public class SupplierSelectedEventArgs : EventArgs, ISupplierSelectedEventArgs
+    {
+        public int SelectedSupplierId { get; set; }
+    }
+
+    public interface ISupplierSelectedEventArgs
+    {
+        int SelectedSupplierId { get; set; }
+    }
 }

4995b970f22526dd59d6af5cd355dded3205e0fd
Carl Berg <carl.berg@spinit.se>
2013-07-29 16:42:35 +0200

    Markup cleanup

--- Synologen.Presentation.Intranet/Models/Orders/PaymentOptionsModel.cs
+++ Synologen.Presentation.Intranet/Models/Orders/PaymentOptionsModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Spinit.Extensions;
@@ -16,22 +17,30 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Models.Orders
         public IList<SubscriptionListItemModel> Subscriptions { get; set; }
     	public string CustomerName { get; set; }
         public int SelectedOption { get; set; }
+
+        public string RenderChecked(Func<PaymentOptionsModel, bool> predicate)
+        {
+            return RenderChecked(predicate(this));
+        }
+
+        public string RenderChecked(bool predicate)
+        {
+            return predicate ? "checked" : string.Empty;
+        }
     }
 
     public class SubscriptionListItemModel
     {
         public SubscriptionListItemModel() { }
-        public SubscriptionListItemModel(Subscription subscription, int selectedItem)
+        public SubscriptionListItemModel(Subscription subscription)
         {
             SubscriptionItems = subscription.SubscriptionItems
-                .Select(x => new SubscriptionItemListItemModel(x))
+                .Select((x, index) => new SubscriptionItemListItemModel(x, index))
                 .ToList();
             Title = "{AccountNumber} ({ConsentStatus})".ReplaceWith(new { AccountNumber = subscription.BankAccountNumber, ConsentStatus = subscription.ConsentStatus.GetEnumDisplayName() });
             Id = subscription.Id;
-            CheckedStatement = subscription.Id == selectedItem ? "checked=\"checked\"" : null;
         }
 
-        public string CheckedStatement { get; protected set; }
         public int Id { get; set; }
         public string Title { get; set; }
         public IList<SubscriptionItemListItemModel> SubscriptionItems { get; set; }
@@ -40,17 +49,19 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Models.Orders
     public class SubscriptionItemListItemModel
     {
         public SubscriptionItemListItemModel() { }
-        public SubscriptionItemListItemModel(SubscriptionItem subscriptionItem)
+        public SubscriptionItemListItemModel(SubscriptionItem subscriptionItem, int index)
         {
             Name = subscriptionItem.Title ?? "Namnlös";
             Created = subscriptionItem.CreatedDate.ToShortDateString();
             Withdrawals = subscriptionItem.IsOngoing 
                 ? subscriptionItem.PerformedWithdrawals.ToString()
                 : string.Format("{0}/{1}", subscriptionItem.PerformedWithdrawals, subscriptionItem.WithdrawalsLimit);
+            Index = index;
         }
 
         public string Name { get; set; }
         public string Created { get; set; }
         public string Withdrawals { get; set; }
+        public int Index { get; protected set; }
     }
 }

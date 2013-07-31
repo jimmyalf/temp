f83fd5af662032466ca22ed1a44acbc5801fdf15
pgra <pgra@localhost>
2013-07-31 09:44:55 +0200

    SY-56) Skapa kreditfaktura

--- Synologen.Presentation/Controllers/ReportController.cs
+++ Synologen.Presentation/Controllers/ReportController.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Mvc;
+using Microsoft.Reporting.WebForms;
+using Spinit.Wpc.Synologen.Business.Domain.Entities;
 using Spinit.Wpc.Synologen.Business.Domain.Interfaces;
 using Spinit.Wpc.Synologen.Presentation.Application.Services;
 using Spinit.Wpc.Synologen.Presentation.Application.Web;
@@ -6,23 +8,55 @@ using Spinit.Wpc.Synologen.Reports.Models;
 
 namespace Spinit.Wpc.Synologen.Presentation.Controllers
 {
-	public class ReportController : ReportBaseController
-	{
-		private readonly ISqlProvider _sqlProvider;
-		private readonly IInvoiceReportViewService _invoiceReportViewService;
+    public class ReportController : ReportBaseController
+    {
+        #region Fields
 
-		public ReportController(ISqlProvider sqlProvider, IInvoiceReportViewService invoiceReportViewService)
-		{
-			_sqlProvider = sqlProvider;
-			_invoiceReportViewService = invoiceReportViewService;
-		}
+        private readonly IInvoiceReportViewService _invoiceReportViewService;
+        private readonly ISqlProvider _sqlProvider;
 
-		public ActionResult InvoiceCopy(int id)
-		{
-			var invoice = _sqlProvider.GetOrder(id);
-			var dataSources = _invoiceReportViewService.GetInvoiceReportDataSources(invoice);
-			const string embeddedReportFullName = "Spinit.Wpc.Synologen.Reports.Invoicing.InvoiceCopy.rdlc";
-			return PDFReportInAssemblyOf<InvoiceCopyReport>(embeddedReportFullName, dataSources);
-		}
-	}
+        #endregion
+
+        #region Constructors and Destructors
+
+        public ReportController(ISqlProvider sqlProvider, IInvoiceReportViewService invoiceReportViewService)
+        {
+            _sqlProvider = sqlProvider;
+            _invoiceReportViewService = invoiceReportViewService;
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        public ActionResult InvoiceCopy(int id)
+        {
+            Order invoice = _sqlProvider.GetOrder(id);
+            ReportDataSource[] dataSources = _invoiceReportViewService.GetInvoiceReportDataSources(invoice);
+            const string EmbeddedReportFullName = "Spinit.Wpc.Synologen.Reports.Invoicing.InvoiceCopy.rdlc";
+            return PDFReportInAssemblyOf<InvoiceCopyReport>(EmbeddedReportFullName, dataSources);
+        }
+
+        // Add fkn
+        public ActionResult InvoiceCredit(int id)
+        {
+            Order invoice = _sqlProvider.GetOrder(id);
+
+            // Invertera kostnader för att kunna kreditera fakturan
+            foreach (var orderItem in invoice.OrderItems)
+            {
+                orderItem.DisplayTotalPrice *= -1;
+                orderItem.SinglePrice *= -1;
+            }
+
+            invoice.InvoiceSumExcludingVAT *= -1;
+            invoice.InvoiceSumIncludingVAT *= -1;
+
+            ReportDataSource[] dataSources = _invoiceReportViewService.GetInvoiceReportDataSources(invoice);
+            const string EmbeddedReportFullName = "Spinit.Wpc.Synologen.Reports.Invoicing.InvoiceCredit.rdlc";
+            return PDFReportInAssemblyOf<InvoiceCopyReport>(EmbeddedReportFullName, dataSources);
+        }
+
+        #endregion
+    }
 }

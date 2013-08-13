3a96f9433f23b17095d5904ed80d4e4f5925ba0f
pgra <pgra@localhost>
2013-08-13 09:35:25 +0200

    Indentering och byte av s?kv?gar etc.

--- Synologen.ContractSale/ServiceLibrary.Test/TestInvoiceParsingAndValidation.cs
+++ Synologen.ContractSale/ServiceLibrary.Test/TestInvoiceParsingAndValidation.cs
@@ -18,110 +18,117 @@ using Spinit.Wpc.Synologen.Svefaktura.Svefakt2.UBL.CommonBasicComponents;
 
 namespace Spinit.Wpc.Synologen.Integration.Services.Test
 {
-	[TestFixture, Explicit]
-	public class TestInvoiceParsingAndValidation
-	{
-		private SqlProvider _provider;
-		private SvefakturaConversionSettings _settings;
+    [TestFixture, Explicit]
+    public class TestInvoiceParsingAndValidation
+    {
+        private SqlProvider _provider;
+        private SvefakturaConversionSettings _settings;
 
-		[SetUp]
-		public void Setup()
-		{
-			const string connectionString = @"Initial Catalog=dbWpcSynologen;Data Source=TEAL;uid=sa;pwd=RICE17A;Pooling=true;Connect Timeout=15;";
-			_provider = new SqlProvider(connectionString);
-			_settings = GetSettings();
-		}
+        [SetUp]
+        public void Setup()
+        {
+            // const string connectionString = @"Initial Catalog=dbWpcSynologen;Data Source=TEAL.hotel.se;uid=sa;pwd=RICE17A;Pooling=true;Connect Timeout=15;";
+            //    <add name="WpcServer" connectionString="Data Source=.\sqlexpress;Initial Catalog=dbWpcSynologen;Integrated Security=SSPI;" providerName="System.Data.SqlClient" />
+            // const string connectionString = @"Data Source=.\sqlexpress;Initial Catalog=dbWpcSynologen;Integrated Security=SSPI;";
+            // const string connectionString = @"Initial Catalog=dbWpcSynologen;Data Source=TEAL.hotel.se;uid=sa;pwd=RICE17A;Pooling=true;Connect Timeout=15;";
+            //                 connectionString="Initial Catalog=dbWpcSynologen;Data Source=BLACK\SQL2008;uid=sa;pwd=RICE17A;Pooling=true;Connect Timeout=15;" providerName="System.Data.SqlClient" />
+            const string connectionString = @"Data Source=BLACK.hotel.se\SQL2008;Initial Catalog=dbWpcSynologen;uid=sa;pwd=RICE17A;Pooling=true;Connect Timeout=15;";
+            
 
-		[Test]
-		public void Debugging()
-		{
-			var invoiceList = new List<SFTIInvoiceType>();
-			var orderList = new List<Order>();
-			for(var i = 4039; i <= 4045; i++)
-			{
-				var order = _provider.GetOrder(i);
-				orderList.Add(order);
-				if(order.StatusId == 3) continue;
-				var invoice = General.CreateInvoiceSvefaktura(order, _settings);
-				invoiceList.Add(invoice);
-			}
-			foreach (var invoice in invoiceList)
-			{
-				//if(invoice.ID.Value != "4599") continue;
-				var ruleViolations = SvefakturaValidator.ValidateObject(invoice);
-				if(ruleViolations.Any())
-				{
-					throw new AssertionException("Ruleviolations were found for invoice " + invoice.ID.Value);
-				}
-			}
-			var encoding = Encoding.GetEncoding("ISO-8859-1");
-			var postOfficeHeader = "<?POSTNET SND=\"STREAMS000345\" REC=\"SE00087815000\" MSGTYPE=\"SYNOLOG\"?>";
-			var output = SvefakturaSerializer.Serialize(new SFTIInvoiceList {Invoices = invoiceList}, encoding, "\r\n", Formatting.Indented, postOfficeHeader);
-			Debug.Write(output);
-			var filePath = @"C:\Users\cber\Desktop\Synologen Faktura\Exempelfaktura_" + DateTime.Now.ToString("yyyy-MM-dd_HH.mm") + ".xml";
-			File.WriteAllText(filePath, output, encoding);
-		}
+            _provider = new SqlProvider(connectionString);
+            _settings = GetSettings();
+        }
 
-		[Test]
-		public void Debugging2()
-		{
-			var order = _provider.GetOrder(4061);
-			var invoice = General.CreateInvoiceSvefaktura(order, _settings);
-			var ruleViolations = SvefakturaValidator.ValidateObject(invoice).ToList();
-			var output = SvefakturaSerializer.Serialize(new SFTIInvoiceList {Invoices = new List<SFTIInvoiceType>(new[] {invoice})}, Encoding.UTF8, "\r\n", Formatting.Indented, null);
-			Debug.Write(output);
-		}
+        [Test]
+        public void Debugging()
+        {
+            var invoiceList = new List<SFTIInvoiceType>();
+            var orderList = new List<Order>();
+            for (var i = 3886; i <= 3886; i++)
+            {
+                var order = _provider.GetOrder(i);
+                orderList.Add(order);
+                if (order.StatusId == 3) continue;
+                var invoice = General.CreateInvoiceSvefaktura(order, _settings);
+                invoiceList.Add(invoice);
+            }
+            //foreach (var invoice in invoiceList)
+            //{
+            //    //if(invoice.ID.Value != "4599") continue;
+            //    var ruleViolations = SvefakturaValidator.ValidateObject(invoice);
+            //    if (ruleViolations.Any())
+            //    {
+            //        throw new AssertionException("Ruleviolations were found for invoice " + invoice.ID.Value);
+            //    }
+            //}
+            var encoding = Encoding.GetEncoding("ISO-8859-1");
+            var postOfficeHeader = "<?POSTNET SND=\"STREAMS000345\" REC=\"SE00087815000\" MSGTYPE=\"SYNOLOG\"?>";
+            var output = SvefakturaSerializer.Serialize(new SFTIInvoiceList { Invoices = invoiceList }, encoding, "\r\n", Formatting.Indented, postOfficeHeader);
+            Debug.Write(output);
+            var filePath = @"C:\Temp\Exempelfaktura_" + DateTime.Now.ToString("yyyy-MM-dd_HH.mm") + ".xml";
+            File.WriteAllText(filePath, output, encoding);
+        }
 
-		public static SvefakturaConversionSettings GetSettings()
-		{
-			return new SvefakturaConversionSettings
-			{
-				SellingOrganizationName = "Synologen Service AB",
-				Adress = new SFTIAddressType
-				{
-					StreetName = new StreetNameType { Value = "Strandbergsgatan 61" },
-					CityName = new CityNameType { Value = "Stockholm" },
-					Country = GetSwedishSFTICountryType(),
-					PostalZone = new ZoneType { Value = "112 51" }
+        [Test]
+        public void Debugging2()
+        {
+            var order = _provider.GetOrder(4061);
+            var invoice = General.CreateInvoiceSvefaktura(order, _settings);
+            var ruleViolations = SvefakturaValidator.ValidateObject(invoice).ToList();
+            var output = SvefakturaSerializer.Serialize(new SFTIInvoiceList { Invoices = new List<SFTIInvoiceType>(new[] { invoice }) }, Encoding.UTF8, "\r\n", Formatting.Indented, null);
+            Debug.Write(output);
+        }
 
-				},
-				RegistrationAdress = new SFTIAddressType
-				{
-					CityName = new CityNameType { Value = "Klippan" },
-					Country = GetSwedishSFTICountryType(),
-				},
-				Contact = new SFTIContactType
-				{
-					ElectronicMail = new MailType { Value = "info@synologen.se" },
-					Name = new NameType { Value = "Violetta Nordlöf" },
-					Telefax = new TelefaxType { Value = "084407359" },
-					Telephone = new TelephoneType { Value = "084407350" }
-				},
-				SellingOrganizationNumber = "5562626100",
-				ExemptionReason = "Innehar F-skattebevis",
-				TaxAccountingCode = "SE556262610001",
-				InvoiceIssueDate = DateTime.Now.Date,//new DateTime(2009, 10, 30),
-				InvoiceTypeCode = "380",
-				InvoiceCurrencyCode = CurrencyCodeContentType.SEK,
-				VATAmount = 0.25m,
-				BankGiro = "56936677",
-				BankgiroBankIdentificationCode = "BGABSESS",
-				InvoicePaymentTermsTextFormat = "{InvoiceNumberOfDueDays} dagar netto",
-				InvoiceExpieryPenaltySurchargePercent = 8m,
-				VATFreeReasonMessage = "Momsfri"
-			};
-		}
+        public static SvefakturaConversionSettings GetSettings()
+        {
+            return new SvefakturaConversionSettings
+            {
+                SellingOrganizationName = "Synologen Service AB",
+                Adress = new SFTIAddressType
+                {
+                    StreetName = new StreetNameType { Value = "Strandbergsgatan 61" },
+                    CityName = new CityNameType { Value = "Stockholm" },
+                    Country = GetSwedishSFTICountryType(),
+                    PostalZone = new ZoneType { Value = "112 51" }
 
-		public static SFTICountryType GetSwedishSFTICountryType()
-		{
-			return new SFTICountryType
-			{
-				IdentificationCode = new CountryIdentificationCodeType
-				{
-					name = "Sverige", 
-					Value = CountryIdentificationCodeContentType.SE
-				}
-			};
-		}
-	}
+                },
+                RegistrationAdress = new SFTIAddressType
+                {
+                    CityName = new CityNameType { Value = "Klippan" },
+                    Country = GetSwedishSFTICountryType(),
+                },
+                Contact = new SFTIContactType
+                {
+                    ElectronicMail = new MailType { Value = "info@synologen.se" },
+                    Name = new NameType { Value = "Violetta Nordlöf" },
+                    Telefax = new TelefaxType { Value = "084407359" },
+                    Telephone = new TelephoneType { Value = "084407350" }
+                },
+                SellingOrganizationNumber = "5562626100",
+                ExemptionReason = "Innehar F-skattebevis",
+                TaxAccountingCode = "SE556262610001",
+                InvoiceIssueDate = DateTime.Now.Date,//new DateTime(2009, 10, 30),
+                InvoiceTypeCode = "380",
+                InvoiceCurrencyCode = CurrencyCodeContentType.SEK,
+                VATAmount = 0.25m,
+                BankGiro = "56936677",
+                BankgiroBankIdentificationCode = "BGABSESS",
+                InvoicePaymentTermsTextFormat = "{InvoiceNumberOfDueDays} dagar netto",
+                InvoiceExpieryPenaltySurchargePercent = 8m,
+                VATFreeReasonMessage = "Momsfri"
+            };
+        }
+
+        public static SFTICountryType GetSwedishSFTICountryType()
+        {
+            return new SFTICountryType
+            {
+                IdentificationCode = new CountryIdentificationCodeType
+                {
+                    name = "Sverige",
+                    Value = CountryIdentificationCodeContentType.SE
+                }
+            };
+        }
+    }
 }

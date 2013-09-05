c5f6961e148ac0dfb54ecf0d7dbe8a2efe5b7b2c
pgra <pgra@localhost>
2013-09-05 09:38:51 +0200

    SY-77) Ta fram de "dolda" funktionerna i DEMO f?r Synologen

--- Synologen.Presentation.Intranet/Wpc/Synologen/Orders/AutogiroDetails.ascx.designer.cs
+++ Synologen.Presentation.Intranet/Wpc/Synologen/Orders/AutogiroDetails.ascx.designer.cs
@@ -13,6 +13,15 @@ namespace Spinit.Wpc.Synologen.Presentation.Intranet.Wpc.Synologen.Orders {
     public partial class AutogiroDetails {
         
         /// <summary>
+        /// txtName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtName;
+        
+        /// <summary>
         /// txtBankAccountNumber control.
         /// </summary>
         /// <remarks>

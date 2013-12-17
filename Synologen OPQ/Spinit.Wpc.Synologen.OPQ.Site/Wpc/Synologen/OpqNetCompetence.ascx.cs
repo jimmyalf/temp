09759f884e270de2ff172a1376f71c1c76899a00
Andreas Jilvero <Andreas Jilvero@localhost>
2013-12-17 10:46:05 +0100

    Added nodes for netCompetence

--- Synologen OPQ/Spinit.Wpc.Synologen.OPQ.Site/Wpc/Synologen/OpqNetCompetence.ascx.cs
+++ Synologen OPQ/Spinit.Wpc.Synologen.OPQ.Site/Wpc/Synologen/OpqNetCompetence.ascx.cs
@@ -1,122 +1,159 @@
 ﻿using System;
 using System.Data;
+using System.Linq;
 using System.Web.UI.WebControls;
-
 using Netcompetence.Netcampus.CommonDataObject;
-
 using Spinit.Wpc.Synologen.OPQ.Core;
-using Spinit.Wpc.Utility.Business;
 using Spinit.Wpc.Synologen.OPQ.Site.Code;
+using Spinit.Wpc.Synologen.OPQ.Site.Code.Config;
+using Spinit.Wpc.Utility.Business;
 
 namespace Spinit.Wpc.Synologen.OPQ.Site.Wpc.Synologen
 {
-	public partial class OpqNetCompetence : OpqControlPage
-	{
-		protected void Page_Load (object sender, EventArgs e)
-		{
-			if (!Page.IsPostBack) {
-				InitResources ();
-			}
+    public partial class OpqNetCompetence : OpqControlPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                InitResources();
+            }
 
-			if ((MemberShopId == null) || (MemberShopId <= 0)) {
-				ShowNegativeFeedBack (userMessageManager, "NoShopException");
-			}
-			else {
-				if (!Page.IsPostBack) {
-					FillUsers ();
-				}
-			}
-		}
-
-		#region Control Events
-
-		protected void BtnNavigateToNetCompetenceClick (object sender, EventArgs e)
-		{
-			SendToNetCompetence ();
-		}		
-		
-		#endregion
-
-		#region Internal Methods
-
-		private void FillUsers ()
-		{
-			if ((MemberShopId != null) && (MemberShopId > 0)) {
-				DataSet ds = Provider.GetSynologenMembers (0, (int) MemberShopId, 0, null);
-
-				if ((ds != null) && (ds.Tables.Count > 0) && (ds.Tables [0].Rows != null) && (ds.Tables [0].Rows.Count > 0)) {
-					foreach (DataRow dataRow in ds.Tables [0].Rows) {
-						drpUsers.Items.Add (
-							new ListItem (
-								string.Format ("{0} {1}", Util.CheckNullString (dataRow, "cContactFirst"), Util.CheckNullString (dataRow, "cContactLast")),
-								Util.CheckNullString (dataRow, "cEmail")));
-					}
-				}
-			}
-		}
-
-		private void InitResources ()
-		{
-			btnNavigateToNetCompetence.Text = GetLocalResourceObject ("GoToNetCompetence") as string;
-		}
-
-		private void SendToNetCompetence ()
-		{
-			try {
-				string userName = null;
-			    if (Configuration.NetCompetenceDebug)
-			    {
-                    userName = "andreas.jilvero@spinit.se";
-			    }
-			    else if (drpUsers.SelectedItem != null)
+            if ((MemberShopId == null) || (MemberShopId <= 0))
+            {
+                ShowNegativeFeedBack(userMessageManager, "NoShopException");
+            }
+            else
+            {
+                if (!Page.IsPostBack)
                 {
-					userName = drpUsers.SelectedItem.Value;
-				}
+                    FillNodes();
+                    FillUsers();
+                }
+            }
+        }
 
-				if (string.IsNullOrEmpty (userName)) {
-					ShowNegativeFeedBack (userMessageManager, Configuration.NetCompetenceDebug ? "ErrorNoUserName" : "ErrorConnect");
-					return;
-				}
+        protected void BtnLoad(object sender, EventArgs e)
+        {
+            Page.Form.Target = "_blank";
+        }
 
-				if (string.IsNullOrEmpty (Configuration.NetCompetenceEncryptionKey)) {
-					ShowNegativeFeedBack (userMessageManager, Configuration.NetCompetenceDebug ? "ErrorNoEncryption" : "ErrorConnect");
-					return;
-				}
+        #region Control Events
 
-				if (string.IsNullOrEmpty (Configuration.NetCompetenceNtsLoginUrl)) {
-					ShowNegativeFeedBack (userMessageManager, Configuration.NetCompetenceDebug ? "ErrorNoUrl" : "ErrorConnect");
-					return;
-				}
+        protected void BtnNavigateToNetCompetenceClick(object sender, EventArgs e)
+        {
+            SendToNetCompetence();
+        }
 
-				string plainData = string.Format ("{0}|{1}", userName, DateTime.UtcNow);
+        #endregion
 
-				if (!string.IsNullOrEmpty (Configuration.NetCompetenceReturnPage)) {
-					//plainData += string.Format ("|{0}", Configuration.NetCompetenceReturnPage);
-				}
+        #region Internal Methods
 
-				CryptoUtil cryptoUtil = new CryptoUtil ();
-				string encryptedData = cryptoUtil.EncryptData (Configuration.NetCompetenceEncryptionKey, plainData);
-				encryptedData = Server.UrlEncode (encryptedData);
+        private void FillNodes()
+        {
+            var config = OPQConfigurationSection.GetInstance();
+            foreach (var node in config.Nodes)
+            {
+                var listItem = new ListItem(node.Name, node.Id.ToString());
+                rblNodes.Items.Add(listItem);
+            }
 
-				string url = string.Format ("{0}?login=true&AuthenticationType=SingleSignOn&EncryptedData={1}", Configuration.NetCompetenceNtsLoginUrl, encryptedData);
+            if (config.Nodes.Any())
+            {
+                rblNodes.SelectedIndex = 0;
+            }
+        }
 
-				/*if (Configuration.NetCompetenceDebug) {
-					ShowPositiveFeedBack (userMessageManager, "ShowLink", string.Format("{0}, User: {1}", url, userName));
-					return;
-				}*/
+        private void FillUsers()
+        {
+            if ((MemberShopId != null) && (MemberShopId > 0))
+            {
+                DataSet ds = Provider.GetSynologenMembers(0, (int)MemberShopId, 0, null);
 
-				Response.Redirect (url);
-			}
-			catch (Exception ex) {
-				if (Configuration.NetCompetenceDebug) {
-					ShowNegativeFeedBack (userMessageManager, "Exception", ex.Message);
-				}
-				else {
-					ShowNegativeFeedBack (userMessageManager, "ErrorConnect");
-				}
-			}
-		}
+                if ((ds != null) && (ds.Tables.Count > 0) && (ds.Tables[0].Rows != null) && (ds.Tables[0].Rows.Count > 0))
+                {
+                    foreach (DataRow dataRow in ds.Tables[0].Rows)
+                    {
+                        drpUsers.Items.Add(
+                            new ListItem(
+                                string.Format("{0} {1}", Util.CheckNullString(dataRow, "cContactFirst"), Util.CheckNullString(dataRow, "cContactLast")),
+                                Util.CheckNullString(dataRow, "cEmail")));
+                    }
+                }
+            }
+        }
 
-		#endregion
-	}
+        private void InitResources()
+        {
+            btnNavigateToNetCompetence.Text = GetLocalResourceObject("GoToNetCompetence") as string;
+        }
+
+        private void SendToNetCompetence()
+        {
+            try
+            {
+                string userName = null;
+                if (Configuration.NetCompetenceDebug)
+                {
+                    userName = "andreas.jilvero@spinit.se";
+                }
+                else if (drpUsers.SelectedItem != null)
+                {
+                    userName = drpUsers.SelectedItem.Value;
+                }
+
+                if (string.IsNullOrEmpty(userName))
+                {
+                    ShowNegativeFeedBack(userMessageManager, Configuration.NetCompetenceDebug ? "ErrorNoUserName" : "ErrorConnect");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(Configuration.NetCompetenceEncryptionKey))
+                {
+                    ShowNegativeFeedBack(userMessageManager, Configuration.NetCompetenceDebug ? "ErrorNoEncryption" : "ErrorConnect");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(Configuration.NetCompetenceNtsLoginUrl))
+                {
+                    ShowNegativeFeedBack(userMessageManager, Configuration.NetCompetenceDebug ? "ErrorNoUrl" : "ErrorConnect");
+                    return;
+                }
+
+                string plainData = string.Format("{0}|{1}", userName, DateTime.UtcNow);
+
+                if (!string.IsNullOrEmpty(Configuration.NetCompetenceReturnPage))
+                {
+                    //plainData += string.Format ("|{0}", Configuration.NetCompetenceReturnPage);
+                }
+
+                var cryptoUtil = new CryptoUtil();
+                var selectedNode = !string.IsNullOrEmpty(rblNodes.SelectedValue) ? string.Format("&nodeID={0}", rblNodes.SelectedValue) : string.Empty;
+                var encryptedData = cryptoUtil.EncryptData(Configuration.NetCompetenceEncryptionKey, plainData);
+                encryptedData = Server.UrlEncode(encryptedData);
+
+                string url = string.Format("{0}?login=true&AuthenticationType=SingleSignOn&EncryptedData={1}{2}", Configuration.NetCompetenceNtsLoginUrl, encryptedData, selectedNode);
+
+                /*if (Configuration.NetCompetenceDebug) {
+                    ShowPositiveFeedBack (userMessageManager, "ShowLink", string.Format("{0}, User: {1}", url, userName));
+                    return;
+                }*/
+
+                Response.Redirect(url);
+            }
+            catch (Exception ex)
+            {
+                if (Configuration.NetCompetenceDebug)
+                {
+                    ShowNegativeFeedBack(userMessageManager, "Exception", ex.Message);
+                }
+                else
+                {
+                    ShowNegativeFeedBack(userMessageManager, "ErrorConnect");
+                }
+            }
+        }
+
+        #endregion
+    }
 }
--- Synologen OPQ/Spinit.Wpc.Synologen.OPQ.Site/Wpc/Synologen/OpqNetCompetence.ascx.designer.cs
+++ Synologen OPQ/Spinit.Wpc.Synologen.OPQ.Site/Wpc/Synologen/OpqNetCompetence.ascx.designer.cs
@@ -22,6 +22,15 @@ namespace Spinit.Wpc.Synologen.OPQ.Site.Wpc.Synologen {
         protected global::Spinit.Wpc.Synologen.OPQ.Site.Code.UserMessageManager userMessageManager;
         
         /// <summary>
+        /// rblNodes control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList rblNodes;
+        
+        /// <summary>
         /// drpUsers control.
         /// </summary>
         /// <remarks>

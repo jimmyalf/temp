b15a30689174ec55ab767648d4e0a49c01e60261
Sebastian Appler Olsson <sebastian.applerolsson@spinit.se>
2014-10-15 16:35:10 +0200

    SY-820, SY-821 Created NoOp invoice type and service

--- Synologen.Data/SqlProviderContract.cs
+++ Synologen.Data/SqlProviderContract.cs
@@ -35,6 +35,23 @@ namespace Spinit.Wpc.Synologen.Data {
 
 		}
 
+		public void SetInvoiceMethodForContractCompanies(int contractId, int invoiceMethod) {
+            SqlParameter[] parameters = {
+						new SqlParameter ("@invoiceMethod", SqlDbType.Int, 4),
+						new SqlParameter ("@contractId", SqlDbType.Int, 4)
+					};
+		    parameters[0].Value = invoiceMethod;
+		    parameters[1].Value = contractId;
+            try
+            {
+
+                RunProcedure("spSynologenSetInvoiceMethodForContractCompanies", parameters);
+        	}
+			catch (SqlException e) {
+				throw new GeneralData.DatabaseInterface.DataException("SqlException while updating invoice methods for contract companies.", e);
+			}
+		}
+
 		public Contract GetContract(int contractCustomerId) {
 			try {
 				var contractCustomerDataSet = GetContracts(FetchCustomerContract.Specific, contractCustomerId, 0, null);
---
+++ Synologen.Service/Web.Invoicing/OrderProcessing/OrderProcessors/NoOpProcessor.cs
@@ -0,0 +1,58 @@
+﻿using Spinit.Wpc.Synologen.Business.Domain.Enumerations;
+using Spinit.Wpc.Synologen.Business.Domain.Interfaces;
+using Spinit.Wpc.Synologen.Invoicing.Svefaktura.Formatters;
+using Spinit.Wpc.Synologen.Invoicing.Svefaktura.SvefakturaBuilders;
+using Spinit.Wpc.Synologen.Invoicing.Svefaktura.Validators;
+using Spinit.Wpc.Synologen.Invoicing.Types;
+using Synologen.Service.Web.Invoicing.Services;
+
+namespace Synologen.Service.Web.Invoicing.OrderProcessing.OrderProcessors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class NoOpProcessor : OrderProcessorBase
+    {
+        public NoOpProcessor(ISqlProvider provider, IFtpService ftpService, IMailService mailService, IFileService fileService, IOrderProcessConfiguration orderProcessConfiguration)
+            : base(provider, ftpService, mailService, fileService, orderProcessConfiguration)
+        {
+        }
+
+        public override OrderProcessResult Process(IList<IOrder> ordersToProcess)
+        {
+            var result = new OrderProcessResult();
+            if (!ordersToProcess.Any())
+            {
+                return result;
+            }
+
+            foreach (var order in ordersToProcess)
+            {
+                try
+                {
+                    ProcessOrder(order);
+                    result.AddSentOrderId(order.Id);
+                }
+                catch (Exception ex)
+                {
+                    result.AddFailedOrderId(order.Id, ex);
+                }
+            }
+
+            return result;
+        }
+
+        private void ProcessOrder(IOrder order)
+        {
+
+            UpdateOrderStatus(order.Id);
+            AddOrderHistory(order.Id, order.InvoiceNumber, "Not sent because this order is of type 'No Operational'");
+        }
+
+        public override bool IHandle(InvoicingMethod method)
+        {
+            return method == InvoicingMethod.NoOp;
+        }
+    }
+}

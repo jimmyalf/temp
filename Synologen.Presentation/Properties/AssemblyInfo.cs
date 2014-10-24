a48c2793aaa7e95d0b9b0ca7c39429dc55c621f8
Carl Berg <carl.berg@spinit.se>
2014-10-24 14:14:08 +0200

    Bumped involved assembly versions to 4.5.0

--- Synologen.Presentation/Properties/AssemblyInfo.cs
+++ Synologen.Presentation/Properties/AssemblyInfo.cs
@@ -31,5 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.2.1")]
-[assembly: AssemblyFileVersion("4.2.1")]
+[assembly: AssemblyVersion("4.5.0")]
+[assembly: AssemblyFileVersion("4.5.0")]
+[assembly: AssemblyInformationalVersion("4.5.0")]

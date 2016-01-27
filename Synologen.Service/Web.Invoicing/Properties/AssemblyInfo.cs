12204336da9aeeaa33da4bbbf1c2f88a5f8341e1
Carl Berg <carl.berg@spinit.se>
2016-01-27 09:46:41 +0100

    Bumped assmebly versions

--- Synologen.Service/Web.Invoicing/Properties/AssemblyInfo.cs
+++ Synologen.Service/Web.Invoicing/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.7.3")]
-[assembly: AssemblyFileVersion("4.7.3")]
-[assembly: AssemblyInformationalVersion("4.7.3")]
+[assembly: AssemblyVersion("4.8.0")]
+[assembly: AssemblyFileVersion("4.8.0")]
+[assembly: AssemblyInformationalVersion("4.8.0")]
